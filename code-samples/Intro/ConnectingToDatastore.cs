52b0b7801bb7e0257e99d4bc0d11a0e5f543f2dc
synhershko <itamar@code972.com>
2011-04-26 17:57:54 +0300

    Documentation work

---
+++ code-samples/Intro/ConnectingToDatastore.cs
@@ -0,0 +1,13 @@
+﻿using Raven.Client.Document;
+
+namespace RavenCodeSamples.Intro
+{
+	public class ConnectingToDatastore : CodeSampleBase
+	{
+		public void RunInServerMode()
+		{
+			var documentStore = new DocumentStore { Url = "http://myravendb.mydomain.com/" };
+			documentStore.Initialize();
+		}
+	}
+}
