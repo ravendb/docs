1ee0037b260b5425d49cd877102b6b06b5c26430
synhershko <itamar@code972.com>
2011-05-08 20:32:52 +0300

    Documentation work (spatial search)

--- code-samples/Consumer/Spatial.cs
+++ code-samples/Consumer/Spatial.cs
@@ -1,9 +1,12 @@
-﻿namespace RavenCodeSamples.Consumer
+﻿using System.Linq;
+using Raven.Client.Indexes;
+
+namespace RavenCodeSamples.Consumer
 {
 	public class Spatial : CodeSampleBase
 	{
 		#region spatial0
-		class Restaurant
+		public class Restaurant
 		{
 			public string Name { get; set; }
 			public double Longitude { get; set; }
@@ -12,13 +15,30 @@
 		}
 		#endregion
 
+		public class Restaurants_ByRatingAndLocation : AbstractIndexCreationTask<Restaurant>
+		{
+			public Restaurants_ByRatingAndLocation()
+			{
+				#region spatial1
+				Map = restaurants => from r in restaurants
+				                     select new {r.Rating, _ = SpatialIndex.Generate(r.Latitude, r.Longitude)};
+				#endregion
+			}
+		}
+
 		public void BasicSpatialSearch()
 		{
 			using (var store = NewDocumentStore())
 			{
 				using (var session = store.OpenSession())
 				{
-					
+					#region spatial2
+					var matchingResturants =
+						session.Advanced.LuceneQuery<Restaurant>("Restaurants/ByRatingAndLocaton")
+							.WhereGreaterThanOrEqual("Rating", 4)
+							.WithinRadiusOf(radius: 5, latitude: 38.9103000, longitude: -77.3942)
+							.ToList();
+					#endregion
 				}
 			}
 		}
