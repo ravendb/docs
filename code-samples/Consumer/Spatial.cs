a928daadac9971db80f0d0a3f142d0e0b18b0633
synhershko <itamar@code972.com>
2011-05-08 14:18:52 +0300

    Spatial code sekeleton

---
+++ code-samples/Consumer/Spatial.cs
@@ -0,0 +1,26 @@
+﻿namespace RavenCodeSamples.Consumer
+{
+	public class Spatial : CodeSampleBase
+	{
+		#region spatial0
+		class Restaurant
+		{
+			public string Name { get; set; }
+			public double Longitude { get; set; }
+			public double Latitude { get; set; }
+			public short Rating { get; set; }
+		}
+		#endregion
+
+		public void BasicSpatialSearch()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					
+				}
+			}
+		}
+	}
+}
