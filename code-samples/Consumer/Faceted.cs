47862e04b7d2ad115b88e4c6d9a9e7d867f490dd
synhershko <itamar@code972.com>
2011-05-18 00:09:10 +0300

    Documentation work (querying for terms)

---
+++ code-samples/Consumer/Faceted.cs
@@ -0,0 +1,18 @@
+﻿namespace RavenCodeSamples.Consumer
+{
+	public class Faceted : CodeSampleBase
+	{
+		public void GetTermsSample()
+		{
+			using (var documentStore = NewDocumentStore())
+			{
+				using (var session = documentStore.OpenSession())
+				{
+					#region getterms1
+					session.Advanced.DatabaseCommands.GetTerms("indexName", "MyProperty", null, 128);
+					#endregion
+				}
+			}
+		}
+	}
+}
