a75236ebd2e12afed710f79ea165eb3aa64660bc
synhershko <itamar@code972.com>
2011-05-17 23:39:47 +0300

    Documentation work (Doc metadata)

---
+++ code-samples/Consumer/Metadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raven.Json.Linq;
+
+namespace RavenCodeSamples.Consumer
+{
+	public class Metadata : CodeSampleBase
+	{
+		public void BasicMetadataUsage()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					#region getting_metadata
+					var product = session.Load<Product>(1);
+					RavenJObject metadata = session.Advanced.GetMetadataFor(product);
+
+					// Get the last modified time stamp, which is known to be of type DateTime
+					DateTime collectionName = metadata["Last-Modified"].Value<DateTime>();
+					#endregion
+				}
+			}
+		}
+	}
+}
