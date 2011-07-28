76a8d76ac14ebb0a00e5f727d915c6fe8eda8bfd
synhershko <itamar@ayende.com>
2011-07-28 14:27:12 +0300

    Documentation work (deployment)

---
+++ code-samples/Server/Deployment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Embedded;
+
+namespace RavenCodeSamples.Server
+{
+	public class Deployment
+	{
+		public void InitEmbeddedSample()
+		{
+#region embedded1
+			var documentStore = new EmbeddableDocumentStore
+			{
+				DataDirectory = "Data"
+			};
+#endregion
+		}
+
+		public void InitEmbeddedHttpSample()
+		{
+#region embedded2
+			var documentStore = new EmbeddableDocumentStore
+			{
+				DataDirectory = "Data",
+				UseEmbeddedHttpServer = true
+			};
+#endregion
+		}
+	}
+}
