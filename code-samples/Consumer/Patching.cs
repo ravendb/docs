90042f0f9b4ebc4ad666a55af8e8dfcbdefd9828
Itamar Syn-Hershko <itamar@hibernatingrhinos.com>
2011-09-11 01:16:05 +0300

    Formatting samples so they fit nicely in ouput

--- code-samples/Consumer/Patching.cs
+++ code-samples/Consumer/Patching.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Raven.Abstractions.Data;
-using Raven.Client.Document;
+﻿using Raven.Abstractions.Data;
 using Raven.Json.Linq;
 
 namespace RavenCodeSamples.Consumer
@@ -16,105 +11,125 @@ namespace RavenCodeSamples.Consumer
 			{
 				#region patching1
 
-				var comment = new BlogComment {Title = "Foo", Content = "Bar"};
+				var comment = new BlogComment
+				              	{
+				              		Title = "Foo",
+				              		Content = "Bar"
+				              	};
 
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Add,
-				                                                       				Name = "Comments",
-				                                                       				Value = RavenJObject.FromObject(comment)
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Add,
+									Name = "Comments",
+									Value = RavenJObject.FromObject(comment)
+								}
+						});
 
 				#endregion
 
 				#region patching2
 
 				// Setting a native type value
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Set,
-				                                                       				Name = "Title",
-				                                                       				Value = RavenJObject.FromObject("New title")
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Set,
+									Name = "Title",
+									Value = RavenJObject.FromObject("New title")
+								}
+						});
 
 				// Setting an object as a property value
-				documentStore.DatabaseCommands.Patch("blogposts/4321", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Set,
-				                                                       				Name = "Author",
-				                                                       				Value = RavenJObject.FromObject(
-				                                                       					new BlogAuthor
-				                                                       						{
-				                                                       							Name = "Itamar",
-				                                                       							ImageUrl = "/author_images/itamar.jpg"
-				                                                       						})
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/4321",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Set,
+									Name = "Author",
+									Value = RavenJObject.FromObject(
+										new BlogAuthor
+											{
+												Name = "Itamar",
+												ImageUrl = "/author_images/itamar.jpg"
+											})
+								}
+						});
 
 				#endregion
 
 				#region patching3
 
 				// This is how you rename a property; copying works exactly the same, but with Type = PatchCommandType.Copy
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Rename,
-				                                                       				Name = "Comments",
-				                                                       				Value = new RavenJValue("cmts")
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Rename,
+									Name = "Comments",
+									Value = new RavenJValue("cmts")
+								}
+						});
+
 				#endregion
 
 				#region patching4
 
 				// Assuming we have a Views counter in our entity
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Inc,
-				                                                       				Name = "Views",
-				                                                       				Value = new RavenJValue(1)
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Inc,
+									Name = "Views",
+									Value = new RavenJValue(1)
+								}
+						});
+
 				#endregion
 
 				#region patching_arrays1
 
 				// Append a new comment; Insert operation is supported as well, by
 				// using PatchCommandType.Add and specifying a Position to insert at
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Add,
-				                                                       				Name = "Comments",
-				                                                       				Value =
-				                                                       					RavenJObject.FromObject(new BlogComment
-				                                                       					                        	{Content = "FooBar"})
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Add,
+									Name = "Comments",
+									Value =
+										RavenJObject.FromObject(new BlogComment
+										                        	{Content = "FooBar"})
+								}
+						});
 
 				// Remove the first comment
-				documentStore.DatabaseCommands.Patch("blogposts/1234", new[]
-				                                                       	{
-				                                                       		new PatchRequest
-				                                                       			{
-				                                                       				Type = PatchCommandType.Remove,
-				                                                       				Name = "Comments",
-				                                                       				Position = 0
-				                                                       			}
-				                                                       	});
+				documentStore.DatabaseCommands.Patch(
+					"blogposts/1234",
+					new[]
+						{
+							new PatchRequest
+								{
+									Type = PatchCommandType.Remove,
+									Name = "Comments",
+									Position = 0
+								}
+						});
 
 				#endregion
 			}
