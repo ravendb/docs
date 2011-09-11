25be7928b20db026311087075e75b503588be5b4
Itamar Syn-Hershko <itamar@hibernatingrhinos.com>
2011-09-11 19:42:00 +0300

    DocsCompiler helper project

---
+++ Tools/RavenDB.DocsCompiler/RavenDB.DocsCompiler/Compiler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using RavenDB.DocsCompiler.MagicWorkers;
+using RavenDB.DocsCompiler.Model;
+
+namespace RavenDB.DocsCompiler
+{
+	public static class Compiler
+	{
+		private const string DocsListFileName = ".docslist";
+
+		public static void CompileFile(string title, string fullPath, string outputFullPath)
+		{
+			var template = File.ReadAllText(@"z:\Projects\RavenDB\RavenDB-docs\Tools\html-template.html");
+
+			var contents = string.Format(template, title, DocumentationParser.Parse(fullPath));
+			File.WriteAllText(outputFullPath, contents);
+		}
+
+		public static void CompileFolder(string title, string fullPath, string outputPath)
+		{
+			if (!File.Exists(Path.Combine(fullPath, DocsListFileName)))
+				return;
+
+			if (!Directory.Exists(outputPath))
+			{
+				Directory.CreateDirectory(outputPath);
+			}
+
+			CompileFile(title, Path.Combine(fullPath, "index.markdown"), Path.Combine(outputPath, "index.html"));
+
+			// Copy images
+			// TODO: Thumbs, lightbox
+			var imagesPath = Path.Combine(fullPath, "images");
+			if (Directory.Exists(imagesPath))
+			{
+				if (!Directory.Exists(Path.Combine(outputPath, @"images")))
+					Directory.CreateDirectory(Path.Combine(outputPath, @"images"));
+
+				var images = Directory.GetFiles(imagesPath);
+				foreach (var image in images)
+				{
+					File.Copy(image, Path.Combine(outputPath, @"images", Path.GetFileName(image)), true);
+				}
+			}
+
+			var foldersToProcess = new List<Folder>();
+
+			var docs = DocsListParser.Parse(Path.Combine(fullPath, DocsListFileName));
+			foreach (var item in docs)
+			{
+				var document = item as Document;
+				if (document != null)
+				{
+					CompileFile(document.Title, Path.Combine(fullPath, document.Slug), Path.Combine(outputPath, document.Slug.Replace(".markdown", ".html")));
+					continue;
+				}
+
+				var folder = item as Folder;
+				if (folder != null)
+				{
+					foldersToProcess.Add(folder);
+					continue;
+				}
+			}
+
+			foreach (var folder in foldersToProcess)
+			{
+				CompileFolder(folder.Name, Path.Combine(fullPath, folder.Slug.TrimStart('\\', '/')), Path.Combine(outputPath, folder.Slug.TrimStart('\\', '/')));
+			}
+		}
+	}
+}
---
+++ Tools/RavenDB.DocsCompiler/RavenDB.DocsCompiler/MagicWorkers/DocsListParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using RavenDB.DocsCompiler.Model;
+
+namespace RavenDB.DocsCompiler.MagicWorkers
+{
+	public static class DocsListParser
+	{
+		//static readonly Regex DocsListLine = new Regex(@"\^[FILES-LIST(-RECURSIVE)?\]", RegexOptions.Compiled);
+		static readonly Regex DocsListLine = new Regex(@"^([\w\-/\.]{2,})\t(.+)$", RegexOptions.Compiled | RegexOptions.Multiline);
+
+		public static IEnumerable<IDocumentationItem> Parse(string filePath)
+		{
+			if (!File.Exists(filePath))
+				yield break;
+
+			var contents = File.ReadAllText(filePath);
+
+			var matches = DocsListLine.Matches(contents);
+			foreach (Match match in matches)
+			{
+				var path = match.Groups[1].Value.Trim();
+				if (path[0] == '\\' || path[0] == '/')
+				{
+					yield return new Folder
+					             	{
+					             		Name = match.Groups[2].Value.Trim(),
+					             		Slug = path
+					             	};
+				}
+				else
+				{
+					yield return new Document
+					             	{
+					             		Title = match.Groups[2].Value.Trim(),
+					             		Slug = path,
+					             	};
+				}
+				
+			}
+		}
+
+		/*
+		public static IEnumerable<MenuItem> ParseAll(string content)
+		{
+			var lines = content.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+			return lines.Select(ParseLine);
+		}
+
+		public static string ParseDocsList(this string content, string path, string slug)
+		{
+			return DocsListFinder.Replace(content, match => GenerateDocsListMenu(path, slug, !string.IsNullOrEmpty(match.Groups[1].Value)));
+		}
+
+		private static string GenerateDocsListMenu(string path, string slug, bool isRecursive)
+		{
+			var filePath = Path.Combine(Directory.GetParent(path).FullName, ".docslist");
+			if (!File.Exists(filePath))
+				return string.Empty;
+
+			// TODO: consider isRecursive
+			var menu = File.ReadAllText(filePath);
+			var result = ParseAll(menu)
+				.Select(item => string.Format("- [{0}]({1})", item.Title, slug + "/" + item.Slug));
+			return string.Join(Environment.NewLine, result);
+		}*/
+	}
+}
