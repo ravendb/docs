8d8b1ee877ee64b903d01f59a2f8ea146ec96e4c
Pawel Pekrol <ppekrol@ais.pl>
2012-12-13 11:59:23 +0100

    RDoc-71. Document Encryption Bundle

---
+++ version_2/code-samples/Server/Bundles/Encryption.cs
@@ -0,0 +1,31 @@
+﻿using Raven.Abstractions.Data;
+
+namespace RavenCodeSamples.Server.Bundles
+{
+    using Raven.Client.Extensions;
+
+    public class Encryption : CodeSampleBase
+    {
+        public void Sample()
+        {
+            using (var store = NewDocumentStore())
+            {
+                #region encryption_1
+                store.DatabaseCommands.CreateDatabase(new DatabaseDocument
+                    {
+                        Id = "EncryptedDB",
+                        // Other configuration options ommited for simplicity
+                        Settings =
+                            {
+                                // ...
+                                {"Raven/ActiveBundles", "Encryption"},
+                                {"Raven/Encryption/Algorithm", "System.Security.Cryptography.DESCryptoServiceProvider, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"},
+                                {"Raven/Encryption/Key", "<key_here>"}
+                            }
+                    });
+
+                #endregion
+            }
+        }
+    }
+}
