112ea8f7acc34c6f4874587f4f236b569997b76c
Pawel Pekrol <ppekrol@ais.pl>
2012-12-13 13:56:39 +0100

    RDoc-72 Server/Bundles/Compression

---
+++ version_2/code-samples/Server/Bundles/Compression.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Abstractions.Data;
+using Raven.Client.Extensions;
+
+namespace RavenCodeSamples.Server.Bundles
+{
+    public class Compression : CodeSampleBase
+    {
+        public void Sample()
+        {
+            using (var store = NewDocumentStore())
+            {
+                #region compression_1
+                store.DatabaseCommands.CreateDatabase(new DatabaseDocument
+                {
+                    Id = "CompressedDB",
+                    // Other configuration options ommited for simplicity
+                    Settings =
+                            {
+                                // ...
+                                {"Raven/ActiveBundles", "Compression"}
+                            }
+                });
+
+                #endregion
+            }
+        }
+    }
+}
