f34ef86dc3297394907f0dd0f9ac0096a7572091
DanielDar <daniel@ayende.com>
2012-12-19 17:01:12 +0200

    add samples

--- version_2/code-samples/Sample Projects/RavenDBSamples/RavenDBSamples/BaseForSamples/SampleBase.cs
+++ version_2/code-samples/Sample Projects/RavenDBSamples/RavenDBSamples/BaseForSamples/SampleBase.cs
@@ -5,11 +5,11 @@ namespace RavenDBSamples.BaseForSamples
 {
 	public class SampleBase
 	{
-		public IDocumentStore Store { get; set; }
+		public IDocumentStore DocumentStore { get; set; }
 
 		public SampleBase()
 		{
-			Store = new DocumentStore
+			DocumentStore = new DocumentStore
 			{
 				Url = "http://localhost:8080",
 				DefaultDatabase = "SampleDatabase"
