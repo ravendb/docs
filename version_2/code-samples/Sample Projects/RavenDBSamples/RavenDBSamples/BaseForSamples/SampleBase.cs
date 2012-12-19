ceec8d7359e899bd542294d697c4f392b6809279
DanielDar <daniel@ayende.com>
2012-12-19 13:29:16 +0200

    Sort the project

---
+++ version_2/code-samples/Sample Projects/RavenDBSamples/RavenDBSamples/BaseForSamples/SampleBase.cs
@@ -0,0 +1,19 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+
+namespace RavenDBSamples.BaseForSamples
+{
+	public class SampleBase
+	{
+		public IDocumentStore Store { get; set; }
+
+		public SampleBase()
+		{
+			Store = new DocumentStore
+			{
+				Url = "http://localhost:8080",
+				DefaultDatabase = "SampleDatabase"
+			}.Initialize();
+		}
+	}
+}
---
+++ version_2/code-samples/Sample Projects/RavenDBSamples/RavenDBSamples/Storing Dates/StoringDatesSample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using RavenDBSamples.BaseForSamples;
+
+namespace RavenDBSamples.Storing_Dates
+{
+	public class StoringDatesSample : SampleBase
+	{
+		private DateTime Local { get; set; }
+		private DateTime Utc { get; set; }
+		public StoringDatesSample()
+		{
+			Local = DateTime.Now;
+			Utc = DateTime.UtcNow;
+		}
+
+		public void StoreLocalTime()
+		{
+			using (var session = Store.OpenSession())
+			{
+				session.Store(new TimeSample
+					{
+						Title = "Local",
+						Time = Local
+					});
+			}
+		}
+
+		public void StoreUtcTime()
+		{
+			using (var session = Store.OpenSession())
+			{
+				session.Store(new TimeSample
+				{
+					Title = "Utc",
+					Time = Utc
+				});
+			}
+		}
+
+		public void StoreWithOffset()
+		{
+			using (var session = Store.OpenSession())
+			{
+				session.Store(new TimeOffsetSample
+				{
+					Title = "Offset",
+					Time = new DateTimeOffset(Local)
+				});
+			}
+		}
+	}
+}
