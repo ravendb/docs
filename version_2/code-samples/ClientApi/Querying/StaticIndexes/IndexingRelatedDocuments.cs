7bc85e3b4a34c1f1f05b76686e5530c8ddae8cca
Pawel Pekrol <ppekrol@ais.pl>
2013-01-04 08:20:42 +0100

    updated packages

---
+++ version_2/code-samples/ClientApi/Querying/StaticIndexes/IndexingRelatedDocuments.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace RavenCodeSamples.ClientApi.Querying.StaticIndexes
+{
+	namespace Foo
+	{
+
+		public class Invoice
+		{
+			public string Id { get; set; }
+
+			public string CustomerId { get; set; }
+		}
+
+		public class Customer
+		{
+			public string Id { get; set; }
+
+			public string Name { get; set; }
+		}
+
+		public class SampleIndex : AbstractIndexCreationTask<Invoice>
+		{
+			public SampleIndex()
+			{
+				Map = invoices => from invoice in invoices
+								  select new
+								  {
+									  CustomerId = invoice.CustomerId
+								  };
+			}
+		}
+
+	}
+
+	public class IndexingRelatedDocuments : CodeSampleBase
+	{
+
+	}
+}
