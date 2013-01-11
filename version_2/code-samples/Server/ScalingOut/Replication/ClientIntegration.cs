e64fe92d06d5b7a2af0ab235e014e0c1df6f3bdd
Arkadiusz Palinski <arek@ais.pl>
2013-01-11 09:14:54 +0100

    RDoc-45 - Raven/ServerPrefixForHilo

--- version_2/code-samples/Server/ScalingOut/Replication/ClientIntegration.cs
+++ version_2/code-samples/Server/ScalingOut/Replication/ClientIntegration.cs
@@ -1,6 +1,7 @@
 ﻿namespace RavenCodeSamples.Server.ScalingOut.Replication
 {
 	using Raven.Client.Document;
+	using Raven.Json.Linq;
 
 	public class ClientIntegration : CodeSampleBase
 	{
@@ -11,6 +12,16 @@
 				#region client_integration_1
 				documentStore.Conventions.FailoverBehavior = FailoverBehavior.FailImmediately;
 
+				#endregion
+
+				#region client_integration_2
+
+				documentStore.DatabaseCommands.Put("Raven/ServerPrefixForHilo", null,
+				                                   new RavenJObject
+					                                {
+						                                {"ServerPrefix", "NorthServer/"}
+					                                },
+				                                   new RavenJObject());
 				#endregion
 			}
 		}
