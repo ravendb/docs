930b52bc654b7769f6dc5a69f041ab846d46580f
Pawel Pekrol <ppekrol@ais.pl>
2013-01-15 10:29:51 +0100

    RDoc-6. Triggers: Put, Delete, Read

---
+++ version_2/code-samples/Server/Extending/Plugins/Index.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Threading;
+using Lucene.Net.Search;
+using Raven.Abstractions.Data;
+using Raven.Database;
+using Raven.Database.Data;
+using Raven.Database.Plugins;
+using Raven.Json.Linq;
+
+namespace RavenCodeSamples.Server.Extending.Plugins
+{
+	namespace Foo
+	{
+		#region plugins_1
+		public abstract class AbstractPutTrigger
+		{
+			public virtual VetoResult AllowPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
+			{
+				return VetoResult.Allowed;
+			}
+
+			public virtual void OnPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
+			{
+			}
+
+			public virtual void AfterPut(string key, RavenJObject document, RavenJObject metadata, Guid etag, TransactionInformation transactionInformation)
+			{
+			}
+
+			public virtual void AfterCommit(string key, RavenJObject document, RavenJObject metadata, Guid etag)
+			{
+			}
+
+			public virtual void Initialize()
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+		}
+
+		#endregion
+
+		#region plugins_2
+		public class VetoResult
+		{
+			public static VetoResult Allowed
+			{
+				get { return new VetoResult(true, "allowed"); }
+			}
+
+			public static VetoResult Deny(string reason)
+			{
+				return new VetoResult(false, reason);
+			}
+
+			private VetoResult(bool allowed, string reason)
+			{
+				IsAllowed = allowed;
+				Reason = reason;
+			}
+
+			public bool IsAllowed { get; private set; }
+
+			public string Reason { get; private set; }
+		}
+
+		#endregion
+
+		#region plugins_3
+		public abstract class AbstractAttachmentPutTrigger
+		{
+			public virtual VetoResult AllowPut(string key, Stream data, RavenJObject metadata)
+			{
+				return VetoResult.Allowed;
+			}
+
+			public virtual void OnPut(string key, Stream data, RavenJObject metadata)
+			{
+			}
+
+			public virtual void AfterPut(string key, Stream data, RavenJObject metadata, Guid etag)
+			{
+			}
+
+			public virtual void AfterCommit(string key, Stream data, RavenJObject metadata, Guid etag)
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+
+			public virtual void Initialize()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+		}
+
+		#endregion
+
+		#region plugins_4
+		public class SecurityTrigger : AbstractPutTrigger
+		{
+			public override VetoResult AllowPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
+			{
+				var doc = Database.Get(key, transactionInformation);
+				if (doc == null) // new document
+					return VetoResult.Allowed;
+
+				if (doc.Metadata["Document-Owner"] == null)// no security
+					return VetoResult.Allowed;
+
+				if (doc.Metadata["Document-Owner"].Value<string>() == Thread.CurrentPrincipal.Identity.Name)
+					return VetoResult.Allowed;
+
+				return VetoResult.Deny("You are not the document owner, cannot modify document");
+			}
+
+			public override void OnPut(string key, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
+			{
+				if (metadata["Document-Owner"] == null) // user didn't explicitly set it
+				{
+					// modify the metadata to the current user
+					metadata["Document-Owner"] = RavenJObject.FromObject(Thread.CurrentPrincipal.Identity.Name);
+				}
+			}
+		}
+
+		#endregion
+
+		#region plugins_5
+		public abstract class AbstractDeleteTrigger
+		{
+			public virtual VetoResult AllowDelete(string key, TransactionInformation transactionInformation)
+			{
+				return VetoResult.Allowed;
+			}
+
+			public virtual void OnDelete(string key, TransactionInformation transactionInformation)
+			{
+			}
+
+			public virtual void AfterDelete(string key, TransactionInformation transactionInformation)
+			{
+			}
+
+			public virtual void AfterCommit(string key)
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+
+			public virtual void Initialize()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+		}
+
+		#endregion
+
+		#region plugins_6
+		public abstract class AbstractAttachmentDeleteTrigger
+		{
+			public virtual VetoResult AllowDelete(string key)
+			{
+				return VetoResult.Allowed;
+			}
+
+			public virtual void OnDelete(string key)
+			{
+			}
+
+			public virtual void AfterDelete(string key)
+			{
+			}
+
+			public virtual void AfterCommit(string key)
+			{
+
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+			public virtual void Initialize()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+		}
+
+		#endregion
+
+		#region plugins_7
+		public class CascadeDeleteTrigger : AbstractDeleteTrigger
+		{
+			public override void OnDelete(string key, TransactionInformation txInfo)
+			{
+				var document = Database.Get(key, txInfo);
+				if (document == null)
+					return;
+
+				Database.Delete(document.Metadata.Value<string>("Cascade-Delete"), null, txInfo);
+			}
+		}
+
+		#endregion
+
+		#region plugins_8
+		public abstract class AbstractReadTrigger
+		{
+			public virtual ReadVetoResult AllowRead(string key, RavenJObject metadata, ReadOperation operation, TransactionInformation transactionInformation)
+			{
+				return ReadVetoResult.Allowed;
+			}
+
+			public virtual void OnRead(string key, RavenJObject document, RavenJObject metadata, ReadOperation operation, TransactionInformation transactionInformation)
+			{
+			}
+
+			public virtual void Initialize()
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+		}
+
+		#endregion
+
+		#region plugins_9
+		public abstract class AbstractAttachmentReadTrigger
+		{
+			public virtual ReadVetoResult AllowRead(string key, Stream data, RavenJObject metadata, ReadOperation operation)
+			{
+				return ReadVetoResult.Allowed;
+			}
+
+			public virtual void OnRead(string key, Attachment attachment)
+			{
+			}
+
+			public virtual void OnRead(AttachmentInformation information)
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+			public virtual void Initialize()
+			{
+			}
+
+			public DocumentDatabase Database { get; set; }
+
+		}
+
+		#endregion
+
+		#region plugins_1_0
+		public class ReadVetoResult
+		{
+			public static ReadVetoResult Allowed
+			{
+				get { return new ReadVetoResult(ReadAllow.Allow, "allowed"); }
+			}
+
+			public static ReadVetoResult Ignore
+			{
+				get { return new ReadVetoResult(ReadAllow.Ignore, "ignore"); }
+			}
+
+			public static ReadVetoResult Deny(string reason)
+			{
+				return new ReadVetoResult(ReadAllow.Deny, reason);
+			}
+
+			private ReadVetoResult(ReadAllow allowed, string reason)
+			{
+				Veto = allowed;
+				Reason = reason;
+			}
+
+			public ReadAllow Veto { get; private set; }
+
+			public enum ReadAllow
+			{
+				Allow,
+				Deny,
+				Ignore
+			}
+
+			public string Reason { get; private set; }
+		}
+
+		#endregion
+
+		#region plugins_1_1
+		public class SecurityReadTrigger : AbstractReadTrigger
+		{
+			public override ReadVetoResult AllowRead(string key, RavenJObject metadata, ReadOperation operation, TransactionInformation transactionInformation)
+			{
+				if (metadata.Value<string>("Document-Owner") == Thread.CurrentPrincipal.Identity.Name)
+					return ReadVetoResult.Allowed;
+
+				if (operation == ReadOperation.Load)
+					return ReadVetoResult.Deny("You don't have permission to read this document");
+
+				return ReadVetoResult.Ignore;
+			}
+		}
+
+		#endregion
+
+		#region plugins_1_2
+		public class EmbedLinkDocument : AbstractReadTrigger
+		{
+			public override void OnRead(string key, RavenJObject document, RavenJObject metadata, ReadOperation operation, TransactionInformation transactionInformation)
+			{
+				var linkName = metadata.Value<string>("Raven-Link-Name");
+				var link = metadata.Value<string>("Raven-Link");
+				if (link == null)
+					return;
+
+				var linkedDocument = Database.Get(link, transactionInformation);
+				document.Add(linkName, linkedDocument.ToJson());
+			}
+		}
+
+		#endregion
+
+		#region plugins_1_3
+		public abstract class AbstractIndexQueryTrigger
+		{
+			public virtual void Initialize()
+			{
+			}
+
+			public virtual void SecondStageInit()
+			{
+			}
+
+
+			public DocumentDatabase Database { get; set; }
+
+			public abstract Query ProcessQuery(string indexName, Query query, IndexQuery originalQuery);
+		}
+
+		#endregion
+	}
+
+	public class Index : CodeSampleBase
+	{
+
+	}
+}
---
+++ version_2/docs/server/extending/plugins/index.markdown
@@ -0,0 +1,96 @@
+﻿#Plugins
+
+Under `Raven.Database.Plugins` namespace various interfaces and classes can be found that might be used to extend the database behavior.
+
+##Triggers
+
+This type of extensions grants the ability to manipulate when certain action is taking place e.g. document is deleted or index is updated.
+
+Triggers can be divided to four categories:    
+* **PUT** triggers   
+* **DELETE** triggers  
+* **Read** triggers   
+* **Query** triggers   
+
+###PUT triggers
+
+To create his own trigger, one must inherit from `AbstractPutTrigger` or `AbstractAttachmentPutTrigger`, but before we will do that lets take a look at them more closely.
+
+{CODE plugins_1@Server\Extending\Plugins\Index.cs /}
+
+{CODE plugins_3@Server\Extending\Plugins\Index.cs /}
+
+{CODE plugins_2@Server\Extending\Plugins\Index.cs /}
+
+where:   
+* **AllowPut** is used to `grant` or `deny` the put operation.   
+* **OnPut** is used to perform any logic just before the document is saved to the disk.   
+* **AfterPut** is used to perform any logic after the document was inserted but still in the same transaction as in `OnPut` method.   
+* **AfterCommit** is used to perform any logic `after` the transaction was commited.    
+* **Initialize** and **SecondStageInit** are used in trigger initialization process.   
+
+**Example: Security trigger**
+
+{CODE plugins_4@Server\Extending\Plugins\Index.cs /}
+
+Most of the logic is in `AllowPut` method, where we check the existing owner (by checking the current version of the document) and reject the update if it if the owner doesn't match.
+In the `OnPut` method, we ensure that the metadata we need is setup correctly. To control attachment putting, similar trigger can be created.
+
+###DELETE triggers
+
+Delete triggers are similar in shape to the put triggers, but in contrast to them they control the delete operations. To build your own trigger, one must inherit from `AbstractDeleteTrigger` or `AbstractAttachmentDeleteTrigger`.
+
+{CODE plugins_5@Server\Extending\Plugins\Index.cs /}
+
+{CODE plugins_6@Server\Extending\Plugins\Index.cs /}
+
+where:   
+* **AllowDelete** is used to `grant` or `deny` the delete operation.   
+* **OnDelete** is used to perform any logic just before the document is deleted.   
+* **AfterDelete** is used to perform any logic after the document has been deleted but still in the same transaction as in `OnDelete` method.   
+* **AfterCommit** is used to perform any logic `after` the transaction was commited.    
+* **Initialize** and **SecondStageInit** are used in trigger initialization process.   
+
+**Example: Cascading deletes**
+
+{CODE plugins_7@Server\Extending\Plugins\Index.cs /}
+
+In this case, we perform another delete operation as part of the current delete operation. This operation is done under the same transaction as the original operation.
+
+###Read triggers
+
+Another type of triggers is used to control the access to the documents and manipulate their context when performing read operations. Similar to the previous triggers, two classes were introduced, the `AbstractReadTrigger` and `AbstractAttachmentReadTrigger`.
+
+{CODE plugins_8@Server\Extending\Plugins\Index.cs /}
+
+{CODE plugins_9@Server\Extending\Plugins\Index.cs /}
+
+{CODE plugins_1_0@Server\Extending\Plugins\Index.cs /}
+
+where:   
+* **AllowRead** is used to `grant` or `deny` the read operation.   
+* **OnRead** is used to perform any logic just before the document is read e.g. modify the document or document metadata (modified values are transient and are `not saved` to the database).     
+* **Initialize** and **SecondStageInit** are used in trigger initialization process.      
+
+**Example: Information hiding**
+
+{CODE plugins_1_1@Server\Extending\Plugins\Index.cs /}
+
+In the example above, we only let the owner of a document to read it. You can see that a Read trigger can deny the read to the user (returning an error to the user) or ignoring the read (hiding the presence of the document. You can also make decisions based on whatever that specific document was requested, or if the document was read as part of a query.
+
+**Example: Linking document on the server side**
+
+{CODE plugins_1_2@Server\Extending\Plugins\Index.cs /}
+
+In this case, we detect that a document with a link was requested, and we stitch the document together with its link to create a single document.
+
+###Query triggers
+
+Query triggers have been introduced to extend the query parsing capabilities and provide users with a way to modify the queries before they are executed against the index. To write your own query trigger, you must inherit from `AbstractIndexQueryTrigger` class.
+
+{CODE plugins_1_3@Server\Extending\Plugins\Index.cs /}
+
+where:   
+* **ProcessQuery** is used to perform any logic on the providen query.   
+* **Initialize** and **SecondStageInit** are used in trigger initialization process.    
+
