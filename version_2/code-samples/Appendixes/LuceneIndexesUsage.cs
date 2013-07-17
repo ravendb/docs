87c18fe07343b69c7b3b2dbf8bb30f9675339384
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-07-17 10:20:51 +0300

    Use AssemblyQualifiedName instead of FullName by default, in order to avoid confuses. And add a note that FullName can also be used with the built-in indexes.

--- version_2/code-samples/Appendixes/LuceneIndexesUsage.cs
+++ version_2/code-samples/Appendixes/LuceneIndexesUsage.cs
@@ -12,21 +12,22 @@
 			var store = new DocumentStore();
 
 			#region using_custom_analyzers
-			store.DatabaseCommands.PutIndex(
-				"Movies",
+			store.DatabaseCommands.PutIndex("Movies",
 				new IndexDefinition
 					{
 						Map = "from movie in docs.Movies select new { movie.Name, movie.Tagline }",
 						Analyzers =
 							{
-								{ "Name", typeof(SimpleAnalyzer).FullName }, 
-								{ "Tagline", typeof(StopAnalyzer).FullName },
+								{ "Name", typeof(SimpleAnalyzer).AssemblyQualifiedName }, 
+								{ "Tagline", typeof(StopAnalyzer).AssemblyQualifiedName },
+								{ "Other", typeof(MyCustomAnalyzer).AssemblyQualifiedName },
 							}
 					});
-
-			#endregion
+			#endregion	
 		}
 
-
+		public class MyCustomAnalyzer
+		{
+		}
 	}
 }
