93a5698b4aae5c68f72ad16d60206d64780a7b2a
Pawel Pekrol <ppekrol@ais.pl>
2013-12-02 14:15:21 +0100

    switched from csharp to dotnet, some cosmetics

---
+++ Tools/RavenDB.DocsCompiler.New/RavenDB.DocsCompiler/Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace RavenDB.DocsCompiler.Extensions
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum @enum)
+        {
+            var enumInfo = @enum.GetType().GetField(@enum.ToString());
+            var attributes = (DescriptionAttribute[])enumInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : @enum.ToString();
+        }
+    }
+}
