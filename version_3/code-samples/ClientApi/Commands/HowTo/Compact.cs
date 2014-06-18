1e4afd0970fbb0638c5c39dd48b03936a1ca050c
Pawel Pekrol <ppekrol@ais.pl>
2014-06-18 15:01:02 +0200

    documented client-api/commands/how-to/compact-database

---
+++ version_3/code-samples/ClientApi/Commands/HowTo/Compact.cs
@@ -0,0 +1,27 @@
+﻿using Raven.Client.Document;
+
+namespace Raven.Documentation.CodeSamples.ClientApi.Commands.HowTo
+{
+	public class Compact
+	{
+		private interface IFoo
+		{
+			#region compact_1
+			void CompactDatabase(string databaseName);
+			#endregion
+		}
+
+		public Compact()
+		{
+			using (var store = new DocumentStore())
+			{
+				#region compact_2
+				store
+					.DatabaseCommands
+					.GlobalAdmin
+					.CompactDatabase("Northwind");
+				#endregion
+			}
+		}
+	}
+}
