56d7e34598607781df582733f00d2a3b1cd3e302
Pawel Pekrol <ppekrol@ais.pl>
2014-07-21 11:51:26 +0200

    documented client-api/session/how-to/refresh-entity

---
+++ version_3/code-samples/ClientApi/Session/HowTo/Refresh.cs
@@ -0,0 +1,35 @@
+﻿using Raven.Client.Document;
+
+using Xunit;
+
+namespace Raven.Documentation.CodeSamples.ClientApi.Session.HowTo
+{
+	public class Refresh
+	{
+		private interface IFoo
+		{
+			#region refresh_1
+			void Refresh<T>(T entity);
+			#endregion
+		}
+
+		public Refresh()
+		{
+			using (var store = new DocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					#region refresh_2
+					var person = session.Load<Person>("people/1");
+					Assert.Equal("Doe", person.LastName);
+
+					// LastName changed to 'Shmoe'
+
+					session.Advanced.Refresh(person);
+					Assert.Equal("Shmoe", person.LastName);
+					#endregion
+				}
+			}
+		}
+	}
+}
