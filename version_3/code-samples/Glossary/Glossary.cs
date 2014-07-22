3a192d6a739651f2b71a53468771d5d33cc23e35
Pawel Pekrol <ppekrol@ais.pl>
2014-07-22 13:51:36 +0200

    added glossary/bulk-insert/bulk-insert-operation added glossary/bulk-insert/bulk-insert-options

---
+++ version_3/code-samples/Glossary/Glossary.cs
@@ -0,0 +1,46 @@
+﻿namespace Raven.Documentation.CodeSamples.Glossary
+{
+	public class Glossary
+	{
+		/*
+		#region bulk_insert_operation
+		public class BulkInsertOperation
+		{
+			public delegate void BeforeEntityInsert(
+				string id,
+				RavenJObject data,
+				RavenJObject metadata);
+
+			public event BeforeEntityInsert OnBeforeEntityInsert = delegate { };
+		
+			public bool IsAborted { get { ... } }
+		
+			public void Abort() { ... }
+		
+			public Guid OperationId { get { ... } }
+		
+			public event Action<string> Report { ... }
+		
+			public void Store(object entity) { ... }
+		
+			public void Store(object entity, string id) { ... }
+		
+			public void Dispose() { ... }
+		}
+		#endregion
+		*/
+
+		#region bulk_insert_options
+		public class BulkInsertOptions
+		{
+			public bool OverwriteExisting { get; set; }
+
+			public bool CheckReferencesInIndexes { get; set; }
+
+			public int BatchSize { get; set; }
+
+			public int WriteTimeoutMilliseconds { get; set; }
+		}
+		#endregion
+	}
+}
---
+++ version_3/docs/glossary/bulk-insert/bulk-insert-operation.markdown
@@ -0,0 +1,28 @@
+﻿# Glossary : BulkInsertOperation
+
+{CODE bulk_insert_operation@Glossary\Glossary.cs /}
+
+OnBeforeEntityInsert
+:   Type: event   
+`event` that will be raised before entity will be processed.
+
+IsAborted
+:   Type: property   
+`property` indicates if operation has aborted.
+
+Abort
+:   Type: method   
+`method` used to abort the operation.
+
+OperationId
+:   Type: property   
+Unique operation Id.
+
+Report
+:   Type: event   
+`event` that will be raised every time a batch has finished processing and after the whole operation.
+
+Store
+:   Type: method   
+`method` used to store the entity, with optional `id` parameter to explicitly declare the entity identifier (will be generated automatically on client-side when overload without `id` will be used).
+
