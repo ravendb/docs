0a6d61a6eb491d14e81a93c8aca6d8f785feef4d
Pawel Pekrol <ppekrol@ais.pl>
2014-07-24 12:54:35 +0200

    moved indexes/boosting from old docs

---
+++ version_3/code-samples/Indexes/Boosting.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+using Raven.Client.Linq.Indexing;
+
+namespace Raven.Documentation.CodeSamples.Indexes
+{
+	#region boosting_2
+	public class Users_ByName : AbstractIndexCreationTask<User>
+	{
+		public Users_ByName()
+		{
+			Map = users => from user in users
+						   select new
+								{
+									FirstName = user.FirstName.Boost(10),
+									LastName = user.LastName
+								};
+		}
+	}
+
+	#endregion
+
+	public class Boosting
+	{
+		#region boosting_1
+		public class User
+		{
+			public string FirstName { get; set; }
+
+			public string LastName { get; set; }
+		}
+		#endregion
+
+		public Boosting()
+		{
+			using (var store = new DocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					#region boosting_3
+					session.Query<User, Users_ByName>()
+						   .Where(x => x.FirstName == "Bob" || x.LastName == "Bob")
+						   .ToList();
+
+					#endregion
+				}
+			}
+		}
+	}
+}
