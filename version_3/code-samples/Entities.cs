721fe68f60ddb4203df4e8a32e6cf991bc7d0c75
Pawel Pekrol <ppekrol@ais.pl>
2014-07-24 11:58:46 +0200

    moved indexes/querying/handling-document-relationships

--- version_3/code-samples/Entities.cs
+++ version_3/code-samples/Entities.cs
@@ -69,4 +69,120 @@ namespace Raven.Documentation.CodeSamples
 		public string Title { get; set; }
 		public string Content { get; set; }
 	}
+
+	#region order_classes_basic
+	public class Order
+	{
+		public string CustomerId { get; set; }
+		public string[] SupplierIds { get; set; }
+		public Referral Refferal { get; set; }
+		public LineItem[] LineItems { get; set; }
+		public double TotalPrice { get; set; }
+	}
+
+	public class Customer
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public short Age { get; set; }
+		public string HashedPassword { get; set; }
+	}
+	#endregion
+
+	#region order_classes_referral
+	public class Referral
+	{
+		public string CustomerId { get; set; }
+		public double CommissionPercentage { get; set; }
+	}
+	#endregion
+
+	#region order_classes_lineitem
+	public class LineItem
+	{
+		public string ProductId { get; set; }
+		public string Name { get; set; }
+		public int Quantity { get; set; }
+		public double Price { get; set; }
+	}
+	#endregion
+
+	public class Supplier
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+	}
+
+	public class Product
+	{
+		public string Name { get; set; }
+		public string[] Images { get; set; }
+		public double Price { get; set; }
+	}
+
+	#region order_classes2
+	public class Order2
+	{
+		public int Customer2Id { get; set; }
+		public Guid[] Supplier2Ids { get; set; }
+		public Referral2 Refferal2 { get; set; }
+		public LineItem2[] LineItem2s { get; set; }
+		public double TotalPrice { get; set; }
+	}
+
+	public class Customer2
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public short Age { get; set; }
+		public string HashedPassword { get; set; }
+	}
+
+	public class Referral2
+	{
+		public int Customer2Id { get; set; }
+		public double CommissionPercentage { get; set; }
+	}
+
+	public class LineItem2
+	{
+		public Guid Product2Id { get; set; }
+		public string Name { get; set; }
+		public int Quantity { get; set; }
+	}
+	#endregion
+
+	public class Supplier2
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+	}
+
+	public class Product2
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string[] Images { get; set; }
+		public double Price { get; set; }
+	}
+
+	public class Order3
+	{
+		public DenormalizedCustomer Customer { get; set; }
+		public string[] SupplierIds { get; set; }
+		public Referral Refferal { get; set; }
+		public LineItem[] LineItems { get; set; }
+		public double TotalPrice { get; set; }
+	}
+
+	#region order_classes_denormalizedcustomer
+	public class DenormalizedCustomer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+	}
+	#endregion
 }
