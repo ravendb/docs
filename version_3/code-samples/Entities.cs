7be782900f044df7f3a6d6b41b6f363cfec82ad5
Pawel Pekrol <ppekrol@ais.pl>
2014-07-24 11:42:32 +0200

    documented indexes/querying/paging

--- version_3/code-samples/Entities.cs
+++ version_3/code-samples/Entities.cs
@@ -52,4 +52,21 @@ namespace Raven.Documentation.CodeSamples
 		public DateTime HiredAt { get; set; }
 		public double HourlyRate { get; set; }
 	}
+
+	public class BlogPost
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Category { get; set; }
+		public string Content { get; set; }
+		public DateTime PublishedAt { get; set; }
+		public string[] Tags { get; set; }
+		public BlogComment[] Comments { get; set; }
+	}
+
+	public class BlogComment
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+	}
 }
