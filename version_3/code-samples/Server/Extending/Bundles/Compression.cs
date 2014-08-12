10c34811fca36e6cfb44ba57178898a2921da545
Pawel Pekrol <ppekrol@ais.pl>
2014-08-12 14:18:15 +0200

    documented server/extending/bundles/compression

---
+++ version_3/code-samples/Server/Extending/Bundles/Compression.cs
@@ -0,0 +1,32 @@
+﻿using Raven.Abstractions.Data;
+using Raven.Client.Document;
+
+namespace Raven.Documentation.CodeSamples.Server.Extending.Bundles
+{
+	public class Compression
+	{
+		public void Sample()
+		{
+			using (var store = new DocumentStore())
+			{
+				#region compression_1
+				store
+					.DatabaseCommands
+					.GlobalAdmin
+					.CreateDatabase(
+						new DatabaseDocument
+							{
+								Id = "CompressedDB",
+								// Other configuration options omitted for simplicity
+								Settings =
+									{
+										// ...
+										{ "Raven/ActiveBundles", "Compression" }
+									}
+							});
+
+				#endregion
+			}
+		}
+	}
+}
