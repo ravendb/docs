d09c6990aa6f77718bb9cf10367498bbc10b0238
Pawel Pekrol <ppekrol@ais.pl>
2014-08-12 14:11:51 +0200

    documented server/extending/bundles/cascade-delete

---
+++ version_3/code-samples/Server/Extending/Bundles/CascadeDelete.cs
@@ -0,0 +1,31 @@
+﻿using Raven.Client.Document;
+using Raven.Json.Linq;
+
+namespace Raven.Documentation.CodeSamples.Server.Extending.Bundles
+{
+	public class CascadeDelete
+	{
+		private class User
+		{
+			public string Name { get; set; }
+		}
+
+		public void Sample()
+		{
+			using (var store = new DocumentStore())
+			{
+				var parent = new User();
+
+				#region cascadedelete1
+				using (var session = store.OpenSession())
+				{
+					session.Store(parent);
+					session.Advanced.GetMetadataFor(parent)["Raven-Cascade-Delete-Documents"] = RavenJToken.FromObject(new[] { "childId1", "childId2" });
+					session.Advanced.GetMetadataFor(parent)["Raven-Cascade-Delete-Attachments"] = RavenJToken.FromObject(new[] { "attachmentId1", " attachmentId2" });
+					session.SaveChanges();
+				}
+				#endregion
+			}
+		}
+	}
+}
