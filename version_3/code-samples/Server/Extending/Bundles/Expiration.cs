281be0386221918ad0e354418ba908c06208df51
Pawel Pekrol <ppekrol@ais.pl>
2014-08-13 08:20:49 +0200

    documented server/extending/bundles/expiration

---
+++ version_3/code-samples/Server/Extending/Bundles/Expiration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Raven.Client.Document;
+using Raven.Json.Linq;
+
+namespace Raven.Documentation.CodeSamples.Server.Extending.Bundles
+{
+	public class Expiration
+	{
+		private class User
+		{
+			public string Name { get; set; }
+		}
+
+		public void Sample()
+		{
+			using (var store = new DocumentStore())
+			{
+				var user = new User();
+
+				#region expiration1
+				var expiry = DateTime.UtcNow.AddMinutes(5);
+				using (var session = store.OpenSession())
+				{
+					session.Store(user);
+					session.Advanced.GetMetadataFor(user)["Raven-Expiration-Date"] = new RavenJValue(expiry);
+					session.SaveChanges();
+				}
+				#endregion
+			}
+		}
+	}
+}
