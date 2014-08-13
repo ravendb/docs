6f32e94777785f547c2d527e8650a7ecbd3b1f23
Pawel Pekrol <ppekrol@ais.pl>
2014-08-13 14:54:09 +0200

    documented server/scaling-out/replication/write-assurance

---
+++ version_3/code-samples/Server/ScalingOut/Replication/WriteAssurance.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="WriteAssurance.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+using Raven.Client.Document;
+using Raven.Json.Linq;
+
+namespace Raven.Documentation.CodeSamples.Server.ScalingOut.Replication
+{
+	public class WriteAssurance
+	{
+		public async Task Sample()
+		{
+			using (var store = new DocumentStore())
+			{
+				var user = new RavenJObject
+					{
+						{"Name", "John"}
+					};
+
+				using (var session = store.OpenAsyncSession())
+				{
+					#region write_assurance_1
+					await session.StoreAsync(user);
+					await session.SaveChangesAsync();
+
+					var userEtag = session
+						.Advanced
+						.GetEtagFor(user);
+
+					var replicas = await store
+						.Replication
+						.WaitAsync(etag: userEtag, timeout: TimeSpan.FromMinutes(1), replicas: 1);
+					#endregion
+
+					#region write_assurance_2
+					await store
+						.Replication
+						.WaitAsync();
+					#endregion
+				}
+			}
+		}
+	}
+}
