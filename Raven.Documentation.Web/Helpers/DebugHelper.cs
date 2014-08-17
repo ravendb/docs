106aaded957a3b489ea55f41e5b041af44c423f3
Pawel Pekrol <ppekrol@ais.pl>
2014-08-17 18:13:52 +0200

    various minor adjustments

---
+++ Raven.Documentation.Web/Helpers/DebugHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Raven.Documentation.Web.Helpers
+{
+	public static class DebugHelper
+	{
+		public static bool IsDebug()
+		{
+#if DEBUG
+			return true;
+#else 
+			return false;
+#endif
+		}
+	}
+}
