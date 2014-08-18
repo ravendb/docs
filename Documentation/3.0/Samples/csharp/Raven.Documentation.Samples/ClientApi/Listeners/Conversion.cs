a4efd667500e8cadc0db96cb44ce4be2d3f18ef9
Arkadiusz Palinski <arek@ais.pl>
2014-08-18 14:03:42 +0200

    Conversion listener fixes

--- Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Listeners/Conversion.cs
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Listeners/Conversion.cs
@@ -8,46 +8,24 @@
 		public interface IDocumentConversionListener
 		{
 			/// <summary>
-			/// Called when converting an entity to a document and metadata
-			/// </summary>
-			void EntityToDocument(string key, object entity, RavenJObject document, 
-									RavenJObject metadata);
-
-			/// <summary>
-			/// Called when converting a document and metadata to an entity
-			/// </summary>
-			void DocumentToEntity(string key, object entity, RavenJObject document, 
-									RavenJObject metadata);
-
-		}
-		#endregion
-
-		#region document_extended_conversion_interface
-		public interface IExtendedDocumentConversionListener
-		{
-			/// <summary>
 			/// Called before converting an entity to a document and metadata
 			/// </summary>
-			void BeforeConversionToDocument(string key, object entity, 
-												RavenJObject metadata);
+			void BeforeConversionToDocument(string key, object entity, RavenJObject metadata);
 
 			/// <summary>
 			/// Called after having converted an entity to a document and metadata
 			/// </summary>
-			void AfterConversionToDocument(string key, object entity, RavenJObject document,
-											RavenJObject metadata);
+			void AfterConversionToDocument(string key, object entity, RavenJObject document, RavenJObject metadata);
 
 			/// <summary>
 			/// Called before converting a document and metadata to an entity
 			/// </summary>
-			void BeforeConversionToEntity(string key, RavenJObject document, 
-											RavenJObject metadata);
+			void BeforeConversionToEntity(string key, RavenJObject document, RavenJObject metadata);
 
 			/// <summary>
 			/// Called after having converted a document and metadata to an entity
 			/// </summary>
-			void AfterConversionToEntity(string key, RavenJObject document, 
-											RavenJObject metadata, object entity);
+			void AfterConversionToEntity(string key, RavenJObject document, RavenJObject metadata, object entity);
 		}
 		#endregion
 
@@ -63,8 +41,7 @@
 
 		public class MetadataToPropertyConversionListener : IDocumentConversionListener
 		{
-			public void EntityToDocument(string key, object entity, RavenJObject document,
-											RavenJObject metadata)
+			public void AfterConversionToDocument(string key, object entity, RavenJObject document, RavenJObject metadata)
 			{
 				if (entity is Item == false)
 					return;
@@ -72,13 +49,20 @@
 				document.Remove("Revision");
 			}
 
-			public void DocumentToEntity(string key, object entity, RavenJObject document, 
-											RavenJObject metadata)
+			public void AfterConversionToEntity(string key, RavenJObject document, RavenJObject metadata, object entity)
 			{
 				if (entity is Item == false)
 					return;
 
 				((Item)entity).Revision = metadata.Value<string>("Raven-Document-Revision");
+			}
+
+			public void BeforeConversionToDocument(string key, object entity, RavenJObject metadata)
+			{
+			}
+
+			public void BeforeConversionToEntity(string key, RavenJObject document, RavenJObject metadata)
+			{
 			}
 		}
 		#endregion
