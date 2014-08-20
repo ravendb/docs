abaf60773ecc19b692900408c1965c598469268e
Arkadiusz Palinski <arek@ais.pl>
2014-08-20 14:33:17 +0200

    ClientApi::HowTo::WorkWithAuthentication

---
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/HowTo/WorkWithAuthentication.cs
@@ -0,0 +1,52 @@
+﻿namespace Raven.Documentation.CodeSamples.ClientApi.HowTo
+{
+	using System.Collections.Generic;
+	using System.Net;
+	using Abstractions.Data;
+	using Client.Document;
+
+	public class WorkWithAuthentication
+	{
+		public WorkWithAuthentication()
+		{
+			#region windows_auth_default_credentials
+			ICredentials Credentials = CredentialCache.DefaultNetworkCredentials;
+			#endregion
+
+
+			#region windows_auth_setup
+			var windowsAuthStore = new DocumentStore()
+			{
+				Credentials = new NetworkCredential("user", "password", "domain")
+			};
+			#endregion
+
+			new 
+			#region api_key_def
+
+			ApiKeyDefinition
+			{
+				Name = "NorthwindAdminAccess",
+				Secret = "MySecret",
+				Enabled = true,
+				Databases = new List<ResourceAccess>
+				{
+					new ResourceAccess
+					{
+						TenantId = "Northwind",
+						Admin = true
+					}
+				}
+			}
+			#endregion
+			;
+
+			#region api_key_setup
+			var oAuthStore = new DocumentStore()
+			{
+				ApiKey = "NorthwindAdminAccess/MySecret"
+			};
+			#endregion
+		} 
+	}
+}
