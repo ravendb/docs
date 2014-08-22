e45750763e5ad551e89ba4b68b4c376308cf7e9a
Arkadiusz Palinski <arek@ais.pl>
2014-08-22 13:04:25 +0200

    Preparing to write the article about conventions

---
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Configuration/Conventions.cs
@@ -0,0 +1,16 @@
+﻿namespace Raven.Documentation.Samples.ClientApi.Configuration
+{
+	using Client.Document;
+
+	public class Conventions
+	{
+		public Conventions()
+		{
+			var store = new DocumentStore();
+
+			#region conventions_1
+			DocumentConvention conventions = store.Conventions;
+			#endregion
+		} 
+	}
+}
