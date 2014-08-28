5b1ffbb37df0b9bbac1d6eb89e2f465f8918d036
Arkadiusz Palinski <arek@ais.pl>
2014-08-28 15:36:03 +0200

    ClientAPI::DocumentIdentifiers::HiLo

---
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/DocumentIdentifiers/HiLoAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿namespace Raven.Documentation.Samples.ClientApi.DocumentIdentifiers
+{
+	using Client.Document;
+	using Json.Linq;
+
+	public class HiLoAlgorithm
+	{
+		public HiLoAlgorithm()
+		{
+			var store = new DocumentStore();
+
+			#region replication_hilo
+
+			store.DatabaseCommands.Put("Raven/ServerPrefixForHilo", null,
+				new RavenJObject
+				{
+					{
+						"ServerPrefix", "NorthServer/"
+					}
+				},
+				new RavenJObject());
+
+			#endregion
+		} 
+	}
+}
