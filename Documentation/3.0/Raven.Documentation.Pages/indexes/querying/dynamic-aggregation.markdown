7f7cc2f91c6342d95bb059a61d0e374befa6e90c
Pawel Pekrol <ppekrol@ais.pl>
2014-09-03 14:53:23 +0200

    documented indexes/querying/dynamic-aggregation

--- Documentation/3.0/Raven.Documentation.Pages/indexes/querying/dynamic-aggregation.markdown
+++ Documentation/3.0/Raven.Documentation.Pages/indexes/querying/dynamic-aggregation.markdown
@@ -1,7 +1,6 @@
-﻿#Dynamic aggregation
+﻿# Dynamic aggregation
 
-This feature is an another way to do aggregations and in contrast to map/reduce indexes, it allows to create much more complex queries. It gives you more options for reporting applications,
-dynamic selection and complex aggregation with additional filtering. 
+This feature is an another way to do aggregations and in contrast to [Map-Reduce]() indexes, it allows to create much more complex queries. It gives you more options for reporting applications, dynamic selection and complex aggregation with additional filtering. 
 
 When working with a map/reduce index we are able to do only limited amount of queries. Let's imagine a sample SQL query:
 
@@ -13,14 +12,21 @@ In order give you the ability to query like this, we introduced the dynamic aggr
 
 {CODE dynamic_aggregation_1@Indexes\Querying\DynamicAggregation.cs /}
 
-The _Orders/All_ index used in the query is a simple map-only index. The only difference is that you have to specify sort options for numeric fields used in the query. This is needed
-for recognizing by RavenDB types of numeric fields when such a query will come in.
+Assuming that your classes look like this:
+
+{CODE currency@Indexes\Querying\DynamicAggregation.cs /}
+
+{CODE order@Indexes\Querying\DynamicAggregation.cs /}
+
+The _Orders/All_ index used in the query is a simple map-only index. The only difference is that you **have to** specify sort options for numeric fields used in the query. This is needed for recognizing by RavenDB types of numeric fields when such a query will come in.
 
 {CODE dynamic_aggregation_index_def@Indexes\Querying\DynamicAggregation.cs /}
 
+{WARNING Not specifying appropriate `SortOptions` for numerical fields will result in an **exception** when aggregation query is executed. /}
+
 {NOTE Results of a dynamic aggregation query are calculated on the fly, while results of map/reduce index are precomputed. Most cases is fast enough but note that you can hit a lot of data. This feature should be used only for complex aggregate queries that cannot be done by using standard map/reduce. /}
 
-##Extended feceted search
+## Extended faceted search
 
 Under the covers this is [a faceted search](../faceted-search) with an extended support for doing aggregations. For example you can aggregate ranges:
 
@@ -34,6 +40,6 @@ Another example is an aggregation on different fields based on same facet:
 
 {CODE dynamic_aggregation_different_fieldss@Indexes\Querying\DynamicAggregation.cs /}
 
-#### Related articles
+## Related articles
 
 TODO
--- Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/Indexes/Querying/DynamicAggregation.cs
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/Indexes/Querying/DynamicAggregation.cs
@@ -1,89 +1,108 @@
 ﻿using System.Linq;
 
+using Raven.Abstractions.Data;
 using Raven.Abstractions.Indexing;
 using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Indexes;
 
-namespace Raven.Documentation.CodeSamples.Indexes.Querying
+namespace Raven.Documentation.Samples.Indexes.Querying
 {
 	public class DynamicAggregation
 	{
-		enum Concurrency
+		#region currency
+		public enum Currency
 		{
 			EUR,
 			USD
 		}
+		#endregion
 
-		class Order
+		#region order
+		public class Order
 		{
 			public double Total { get; set; }
+
 			public string Product { get; set; }
-			public Concurrency Concurrency { get; set; }
+
+			public Currency Currency { get; set; }
 		}
+		#endregion
+
+		#region dynamic_aggregation_index_def
+		public class Orders_All : AbstractIndexCreationTask<Order>
+		{
+			public Orders_All()
+			{
+				Map = orders => from order in orders
+						select new
+						{
+							order.Total,
+							order.Product,
+							Concurrency = order.Currency
+						};
+
+				Sort(x => x.Total, SortOptions.Double);
+			}
+		}
+		#endregion
 
 		public DynamicAggregation()
 		{
 			using (var store = new DocumentStore())
 			{
-
-				#region dynamic_aggregation_index_def
-				store.DatabaseCommands.PutIndex("Orders/All", new IndexDefinitionBuilder<Order>()
-				{
-					Map = orders => from order in orders
-									select new
-									{
-										order.Total,
-										order.Product,
-										order.Concurrency
-									},
-					SortOptions = { { x => x.Product, SortOptions.Double } }
-				});
-				#endregion
-
 				using (var session = store.OpenSession())
 				{
 					#region dynamic_aggregation_1
-					var result = session.Query<Order>("Orders/All")
-										.Where(x => x.Total > 500)
-										.AggregateBy(x => x.Product)
-											.SumOn(x => x.Total)
-										.ToList();
+					FacetResults facetResults = session
+						.Query<Order, Orders_All>()
+						.Where(x => x.Total > 500)
+						.AggregateBy(x => x.Product)
+							.SumOn(x => x.Total)
+						.ToList();
 					#endregion
+				}
 
+				using (var session = store.OpenSession())
+				{
 					#region dynamic_aggregation_range
 
-					result = session.Query<Order>("Orders/All")
+					FacetResults facetResults = session.Query<Order, Orders_All>()
 									.AggregateBy(x => x.Product)
-									.AddRanges(x => x.Total < 100,
-											   x => x.Total >= 100 && x.Total < 500,
-											   x => x.Total >= 500 && x.Total < 1500,
-											   x => x.Total >= 1500)
+									.AddRanges(
+										x => x.Total < 100,
+										x => x.Total >= 100 && x.Total < 500,
+										x => x.Total >= 500 && x.Total < 1500,
+										x => x.Total >= 1500
+									)
 									.SumOn(x => x.Total)
-									.ToList();
-					#endregion
-
-					#region dynamic_aggregation_multiple_items
-					result = session.Query<Order>("Orders/All")
-									.AggregateBy(x => x.Product)
-										.SumOn(x => x.Total)
-										.CountOn(x => x.Total)
-									.AndAggregateOn(x => x.Concurrency)
-										.MinOn(x => x.Total)
-									.ToList();
-					#endregion
-
-					#region dynamic_aggregation_different_fieldss
-					result = session.Query<Order>("Orders/All")
-									.AggregateBy(x => x.Product)
-										.MaxOn(x => x.Total)
-										.MinOn(x => x.Concurrency)
 									.ToList();
 					#endregion
 				}
 
+				using (var session = store.OpenSession())
+				{
+					#region dynamic_aggregation_multiple_items
+					FacetResults facetResults = session.Query<Order, Orders_All>()
+									.AggregateBy(x => x.Product)
+										.SumOn(x => x.Total)
+										.CountOn(x => x.Total)
+									.AndAggregateOn(x => x.Currency)
+										.MinOn(x => x.Total)
+									.ToList();
+					#endregion
+				}
 
-
+				using (var session = store.OpenSession())
+				{
+					#region dynamic_aggregation_different_fieldss
+					FacetResults facetResults = session.Query<Order, Orders_All>()
+									.AggregateBy(x => x.Product)
+										.MaxOn(x => x.Total)
+										.MinOn(x => x.Currency)
+									.ToList();
+					#endregion
+				}
 			}
 		}
 	}
