4980de2b8ccd97ab520edc600e8c34b326b8c0fc
Pawel Pekrol <ppekrol@ais.pl>
2014-10-08 08:14:43 +0200

    documented client-api/commands/indexes/how-to/check-if-index-has-changed

---
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Commands/Indexes/HowTo/IndexHasChanged.cs
@@ -0,0 +1,14 @@
+﻿using Raven.Abstractions.Indexing;
+
+namespace Raven.Documentation.CodeSamples.ClientApi.Commands.Indexes.HowTo
+{
+	public class IndexHasChanged
+	{
+		private interface IFoo
+		{
+			#region index_has_changed_1
+			bool IndexHasChanged(string name, IndexDefinition indexDef);
+			#endregion
+		} 
+	}
+}
