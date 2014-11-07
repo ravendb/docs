bd9fd10564d308cff18e4947cfe9b021732b804b
Pawel Pekrol <ppekrol@ais.pl>
2014-11-07 12:22:31 +0100

    moved 1.0 samples

---
+++ Documentation/1.0/Samples/csharp/Raven.Documentation.Samples/Consumer/Attachments.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Raven.Json.Linq;
+
+namespace RavenCodeSamples.Consumer
+{
+	public class Attachments : CodeSampleBase
+	{
+		public void SimpleAttachments()
+		{
+			using (var documentStore = NewDocumentStore())
+			{
+				#region retrieving_attachment
+				Raven.Abstractions.Data.Attachment attachment = documentStore.DatabaseCommands.GetAttachment("videos/1");
+				#endregion
+
+				#region putting_attachment
+				Stream data = new MemoryStream(new byte[] { 1, 2, 3 }); // don't forget to load the data from a file or something!
+				documentStore.DatabaseCommands.PutAttachment("videos/2", null, data,
+				                                             new RavenJObject {{"Description", "Kids play in the garden"}});
+				#endregion
+
+				#region deleting_attachment
+				documentStore.DatabaseCommands.DeleteAttachment("videos/1", null);
+				#endregion
+			}
+		}
+	}
+}
