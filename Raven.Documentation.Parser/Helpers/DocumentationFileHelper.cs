b533543d1c987d4a7edf5916bb38322cf2c22976
Arkadiusz Palinski <arek@ais.pl>
2015-10-08 11:52:43 +0200

    converting .docslist to new format that supports mappings

--- Raven.Documentation.Parser/Helpers/DocumentationFileHelper.cs
+++ Raven.Documentation.Parser/Helpers/DocumentationFileHelper.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
 using Raven.Documentation.Parser.Data;
@@ -28,6 +28,57 @@ namespace Raven.Documentation.Parser.Helpers
 				};
 
 				yield return item;
+			}
+		}
+
+		private static readonly Regex DocsListLine = new Regex(@"^([\w\-/\.]{2,})\t(.+)$", RegexOptions.Compiled | RegexOptions.Multiline);
+
+		// used to convert .docslist files to .docs.json ones which support mappings
+		public static void CovertDocslistFileToNewFormatWithMappings()
+		{
+			string folder = @"C:\workspaces\HIRS\ravendb_docs\Documentation\3.0\Raven.Documentation.Pages";
+
+			if(string.IsNullOrEmpty(folder))
+				return;
+
+			if(Directory.Exists(folder) == false)
+				throw new DirectoryNotFoundException();
+
+			var docslistFiles = Directory.GetFiles(folder, ".docslist", SearchOption.AllDirectories);
+			
+			foreach (var docListFilePath in docslistFiles)
+			{
+				var converted = new List<DocumentationFile>();
+				var content = File.ReadAllText(docListFilePath);
+
+				var matches = DocsListLine.Matches(content);
+
+				foreach (Match match in matches)
+				{
+					var fileName = match.Groups[1].Value.Trim();
+					var description = match.Groups[2].Value.Trim();
+
+					converted.Add(new DocumentationFile()
+					{
+						Path = fileName,
+						Name = description
+					});
+				}
+				
+				var directoryName = Path.GetDirectoryName(docListFilePath);
+
+				using (var convertedFile = File.Open(Path.Combine(directoryName, ".docs.json"), FileMode.OpenOrCreate))
+				using (var streamWriter = new StreamWriter(convertedFile))
+				{
+					JsonSerializer.Create(new JsonSerializerSettings()
+					{
+						Formatting = Formatting.Indented
+					}).Serialize(streamWriter, converted);
+
+					convertedFile.Flush();
+				}
+
+				File.Delete(docListFilePath);
 			}
 		}
 	}
