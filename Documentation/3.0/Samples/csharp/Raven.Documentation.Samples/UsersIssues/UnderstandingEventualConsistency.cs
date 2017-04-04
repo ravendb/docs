b8051ee1756d99f702a11ce1a5f1f6fd12cc1711
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-04-04 15:28:38 +0200

    TMP-290 fix migrated kb articles sample code

---
+++ Documentation/3.0/Samples/csharp/Raven.Documentation.Samples/UsersIssues/UnderstandingEventualConsistency.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using Raven.Client.Document;
+
+namespace Raven.Documentation.Samples.UsersIssues
+{
+    class UnderstandingEventualConsistency
+    {
+        class Product
+        {
+            public string Name;
+        }
+
+
+        public void Sample1()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var pageSize = 0;
+                    var pageNumber = 0;
+
+                    #region userissues_1
+                    var results = session.Query<Product>()
+                        .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
+                        .OrderBy(x => x.Name)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize);
+                    #endregion
+                }
+            }
+        }
+
+        public void Sample2()
+        {
+            var productId = "Products/1";
+            var pageSize = 0;
+            var pageNumber = 0;
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+        #region userissues_2
+                    var product = session.Load<Product>(productId);
+                    var etag = session.Advanced.GetEtagFor(product);
+
+                    var results = session.Query<Product>()
+                        .Customize(x => x.WaitForNonStaleResultsAsOf(etag))
+                        .OrderBy(x => x.Name)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize);
+        #endregion
+                }
+            }
+        }
+
+    }
+}
