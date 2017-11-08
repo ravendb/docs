9d6f80a69c3158a4ad4bec209c8377738056bdbb
Pawel Pekrol <ppekrol@ravendb.net>
2017-11-08 14:34:39 +0100

    fixed compilation

--- Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/OpeningSession.cs
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/OpeningSession.cs
@@ -1,54 +1,54 @@
-﻿using Raven.Client;
-using Raven.Client.Document;
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
 
 namespace Raven.Documentation.Samples.ClientApi.Session
 {
-	public class OpeningSession
-	{
-		private interface IFoo
-		{
-			#region open_session_1
-			// Open session for a 'default' database configured in 'DocumentStore'
-			IDocumentSession OpenSession();
+    public class OpeningSession
+    {
+        private interface IFoo
+        {
+            #region open_session_1
+            // Open session for a 'default' database configured in 'DocumentStore'
+            IDocumentSession OpenSession();
 
-			// Open session for a specified database
-			IDocumentSession OpenSession(string database);
+            // Open session for a specified database
+            IDocumentSession OpenSession(string database);
 
-			IDocumentSession OpenSession(OpenSessionOptions options);
-			#endregion
-		}
+            IDocumentSession OpenSession(SessionOptions options);
+            #endregion
+        }
 
-		public OpeningSession()
-		{
-			string databaseName = "DB1";
+        public OpeningSession()
+        {
+            string databaseName = "DB1";
 
-			using (var store = new DocumentStore())
-			{
-				#region open_session_2
-				store.OpenSession(new OpenSessionOptions());
-				#endregion
+            using (var store = new DocumentStore())
+            {
+                #region open_session_2
+                store.OpenSession(new SessionOptions());
+                #endregion
 
-				#region open_session_3
-				store.OpenSession(new OpenSessionOptions
-									  {
-										  Database = databaseName
-									  });
-				#endregion
+                #region open_session_3
+                store.OpenSession(new SessionOptions
+                {
+                    Database = databaseName
+                });
+                #endregion
 
-				#region open_session_4
-				using (IDocumentSession session = store.OpenSession())
-				{
-					// code here
-				}
-				#endregion
+                #region open_session_4
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    // code here
+                }
+                #endregion
 
-				#region open_session_5
-				using (IAsyncDocumentSession session = store.OpenAsyncSession())
-				{
-					// async code here
-				}
-				#endregion
-			}
-		}
-	}
+                #region open_session_5
+                using (IAsyncDocumentSession session = store.OpenAsyncSession())
+                {
+                    // async code here
+                }
+                #endregion
+            }
+        }
+    }
 }
