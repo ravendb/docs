d17dee52ba20c0ef6b579dee2d145e11354633c5
Idan Haim Shalom <haimdude@gmail.com>
2017-11-15 16:58:01 +0200

    RDoc-760, RDoc-767, RDoc-768 playground-server, creating-document-store and what-is-a-document-store

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/CreatingDocumentStore.cs
@@ -0,0 +1,44 @@
+﻿namespace Raven.Documentation.Samples.ClientApi
+{
+	using System;
+	using Client;
+	using Client.Documents;
+
+	public class CreatingDocumentStore
+	{
+	    public CreatingDocumentStore()
+	    {
+	        #region document_store_creation
+
+	        using (IDocumentStore store = new DocumentStore()
+	        {
+	            Urls = new[] {"http://localhost:8080"}
+	        }.Initialize())
+	        {
+
+	        }
+
+	        #endregion
+	    }
+
+	    #region document_store_holder
+		public class DocumentStoreHolder
+		{
+			private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
+
+			public static IDocumentStore Store => store.Value;
+
+		    private static IDocumentStore CreateStore()
+			{
+				IDocumentStore store = new DocumentStore()
+				{
+				    Urls = new[] { "http://localhost:8080" },
+                    Database = "Northwind"
+				}.Initialize();
+
+				return store;
+			}
+		}
+		#endregion
+	}
+}
