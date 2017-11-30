e5027d1a2cbda703e53f43b1fd16f5633816ba7f
Judah Gabriel Himango <judahgabriel@gmail.com>
2017-11-30 00:56:46 -0600

    RDoc-816 First stab at document metadata.

--- Documentation/4.0/Raven.Documentation.Pages/client-api/commands/documents/how-to/get-document-metadata-only.dotnet.markdown
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/commands/documents/how-to/get-document-metadata-only.dotnet.markdown
@@ -1,18 +1,17 @@
 # Commands : Documents : How to get document metadata only?
 
-**Head** is used to retrieve document metadata from a database.
+**GetDocumentCommand** can be used to retrieve the metadata of a document.
 
 ## Syntax
 
 {CODE head_1@ClientApi\Commands\Documents\HowTo\Head.cs /}
 
+
 | Parameters | | |
 | ------------- | ------------- | ----- |
-| **key** | string | key of a document to get metadata for |
-
-| Return Value | |
-| ------------- | ----- |
-| [JsonDocumentMetadata](../../../../glossary/json-document-metadata) | Metadata information for document with given key. |
+| **id** | string | ID of a document to get metadata for |
+| **includes** | string | Related documents to fetch along with the document |
+| **metadataOnly** | boolean | Whether to fetch the whole document, or just the metadata. Pass true to get metadata only. |
 
 ## Example
 
@@ -22,5 +21,4 @@
 
 - [Get](../../../../client-api/commands/documents/get)  
 - [Put](../../../../client-api/commands/documents/put)  
-- [Delete](../../../../client-api/commands/documents/delete)  
-- [Revisions and concurrency with ETags](../../../../client-api/concurrency/revisions-and-concurrency-with-etags)   
+- [Delete](../../../../client-api/commands/documents/delete)
---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Commands/Documents/HowTo/Head.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Raven.Client.Documents;
+using Sparrow.Json;
+using GetDocumentCommand = Raven.Client.Documents.Commands.GetDocumentCommand;
+
+namespace Raven.Documentation.Samples.ClientApi.Commands.Documents.HowTo
+{
+    public class Foo
+    {
+        public class GetDocumentCommand
+        {
+            #region head_1
+            public GetDocumentCommand(string id, string[] includes, bool metadataOnly)
+            #endregion
+            {
+
+            }
+        }
+    }
+
+    public class Foo2
+    { 
+        public Foo2()
+        {
+            using (var docStore = new DocumentStore())
+            {
+                #region head_2
+                // Create a command to fetch the metadata.
+                var docId = "FooBars/1-A";
+                var getMetaCommand = new GetDocumentCommand(docId, includes: null, metadataOnly: true);
+
+                var executor = Raven.Client.Http.RequestExecutor.Create(docStore.Urls, docStore.Database, docStore.Certificate, docStore.Conventions);
+                using (var opContext = JsonOperationContext.ShortTermSingleUse())
+                {
+                    // Fetch the metadata
+                    executor.Execute(getMetaCommand, opContext);
+                    var metaResult = getMetaCommand.Result; // null if does not exist
+
+                    // We asked for a single document, so get the first (only) item.
+                    var metaResultItem = (BlittableJsonReaderObject)metaResult.Results[0];
+
+                    // We asked for metadata only, so grab that object.
+                    var docMeta = (BlittableJsonReaderObject)metaResultItem["@metadata"];
+
+                    // Print out all the metadata properties.
+                    foreach (var metaPropName in docMeta.GetPropertyNames())
+                    {
+                        docMeta.TryGet<object>(metaPropName, out var metaPropValue);
+                        Console.WriteLine("{0} = {1}", metaPropName, metaPropValue);
+                    }
+                }
+                #endregion
+            }
+        }
+    }
+}
