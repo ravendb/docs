71c04567ebda354ca3935545f718020277c61263
Pawel Pekrol <ppekrol@ravendb.net>
2017-12-01 13:44:18 +0100

    RDoc-850 Client API > Session > Querying > How to get query statistics?

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Querying/HowToGetQueryStatistics.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Orders;namespace Raven.Documentation.Samples.ClientApi.Session.Querying
+{
+    public class HowToGetQueryStatistics
+    {
+        private interface IFoo<TResult>
+        {
+            #region stats_1
+            IRavenQueryable<TResult> Statistics(out QueryStatistics stats);
+            #endregion
+        }
+
+        public HowToGetQueryStatistics()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region stats_2
+                    List<Employee> employees = session.Query<Employee>()
+                        .Where(x => x.FirstName == "Robert")
+                        .Statistics(out QueryStatistics stats)
+                        .ToList();
+
+                    int totalResults = stats.TotalResults;
+                    long durationInMilliseconds = stats.DurationInMs;
+                    #endregion
+                }
+            }
+        }
+    }
+}
