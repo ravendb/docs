f741422a37e0adf2f52172e67e1b5b25f1761125
talweiss1982 <tal@ayende.com>
2017-12-10 16:35:00 +0200

    RDoc-799 changes api

---
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/changes/how-to-subscribe-to-index-changes.dotnet.markdown
@@ -0,0 +1,86 @@
+# Changes API : How to subscribe to index changes?
+
+Following methods allow you to subscribe to index changes:
+
+- [ForIndex](../../client-api/changes/how-to-subscribe-to-index-changes#forindex)
+- [ForAllIndexes](../../client-api/changes/how-to-subscribe-to-index-changes#forallindexes)
+
+{PANEL:ForIndex}
+
+Index changes for one index can be observed using `ForIndex` method.
+
+### Syntax
+
+{CODE index_changes_1@ClientApi\Changes\HowToSubscribeToIndexChanges.cs /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **indexName** | string | Name of an index for which notifications will be processed. |
+
+| Return value | |
+| ------------- | ----- |
+| IChangesObservable<[IndexChange](../../client-api/changes/how-to-subscribe-to-index-changes#indexchange)> | Observable that allows to add subscriptions to notifications for index with given name. |
+
+### Example
+
+{CODE index_changes_2@ClientApi\Changes\HowToSubscribeToIndexChanges.cs /}
+
+{PANEL/}
+
+{PANEL:ForAllIndexes}
+
+Index changes for all indexex can be observed using `ForAllIndexes` method.
+
+| Return value | |
+| ------------- | ----- |
+| IChangesObservable<[IndexChange](../../client-api/changes/how-to-subscribe-to-index-changes#indexchange)> | Observable that allows to add subscriptions to notifications for all indexes. |
+
+### Syntax
+
+{CODE index_changes_3@ClientApi\Changes\HowToSubscribeToIndexChanges.cs /}
+
+### Example
+
+{CODE index_changes_4@ClientApi\Changes\HowToSubscribeToIndexChanges.cs /}
+
+{PANEL/}
+
+{PANEL:IndexChange}
+
+### Properties
+
+| Name | Type | Description |
+| ------------- | ------------- | ----- |
+| **Type** | [IndexChangeTypes](../../client-api/changes/how-to-subscribe-to-index-changes#indexchangetypes) | Change type |
+| **Name** | string | Index name |
+| **Etag** | long? | Index Etag |
+
+<hr />
+{PANEL/}
+
+{PANEL:IndexChangeTypes}
+# IndexChangeTypes (enum flags)
+
+### Members
+
+| Name | Value |
+| ---- | ----- |
+| **None** | `0` |
+| **BatchCompleted** | `1` |
+| **IndexAdded** | `8` |
+| **IndexRemoved** | `16` |
+| **IndexDemotedToIdle** | `32` |
+| **IndexPromotedFromIdle** | `64` |
+| **IndexDemotedToDisabled** | `256` |
+| **IndexMarkedAsErrored** | `512` |
+| **SideBySideReplace** | `1024` |
+| **Renamed** | `2048` |
+| **IndexPaused** | `4096` |
+| **LockModeChanged** | `8192` |
+| **PriorityChanged** | `16384` |
+{PANEL/}
+
+## Remarks
+
+{INFO To get more method overloads, especially the ones supporting delegates, please add [Reactive Extensions Core](https://www.nuget.org/packages/System.Reactive.Core/) package to your project. /}
+
--- Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Changes/HowToSubscribeToIndexChanges.cs
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Changes/HowToSubscribeToIndexChanges.cs
@@ -1,10 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Changes;
 
 namespace Raven.Documentation.Samples.ClientApi.Changes
 {
     public class HowToSubscribeToIndexChanges
     {
+        private interface IFoo
+        {
+            #region index_changes_1
+            IChangesObservable<IndexChange>
+                ForIndex(string indexName);
+            #endregion
+
+            #region index_changes_3
+            IChangesObservable<IndexChange>
+                ForAllIndexes();
+            #endregion
+        }
+
+        public HowToSubscribeToIndexChanges()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region index_changes_2
+                IDisposable subscription = store
+                    .Changes()
+                    .ForIndex("Orders/All")
+                    .Subscribe(
+                        change =>
+                        {
+                            switch (change.Type)
+                            {
+                                case IndexChangeTypes.None:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.BatchCompleted:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexAdded:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexRemoved:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexDemotedToIdle:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexPromotedFromIdle:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexDemotedToDisabled:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexMarkedAsErrored:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.SideBySideReplace:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.Renamed:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.IndexPaused:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.LockModeChanged:
+                                    //Do someting
+                                    break;
+                                case IndexChangeTypes.PriorityChanged:
+                                    //Do someting
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        });
+                #endregion
+            }
+
+            using (var store = new DocumentStore())
+            {
+                #region index_changes_4
+                IDisposable subscription = store
+                    .Changes()
+                    .ForAllIndexes()
+                    .Subscribe(change => Console.WriteLine("{0} on index {1}", change.Type, change.Name));
+                #endregion
+            }
+        }
     }
 }
