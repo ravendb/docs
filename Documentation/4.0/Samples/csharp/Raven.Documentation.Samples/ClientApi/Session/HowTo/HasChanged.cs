ff6932f21e160d7b86cd73985d7c5db4562ee219
Idan Haim Shalom <haimdude@gmail.com>
2017-12-13 15:30:52 +0200

    RDoc-867 - RDoc-866 add check if entity has changed and check if there are any changes on a session

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/HowTo/HasChanged.cs
@@ -0,0 +1,31 @@
+﻿using Raven.Client.Documents;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.HowTo
+{
+    public class HasChanged
+	{
+		private interface IFoo
+		{
+			#region has_changed_1
+			bool HasChanged(object entity);
+			#endregion
+		}
+
+		public HasChanged()
+		{
+			using (var store = new DocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					#region has_changed_2
+					Employee employee = session.Load<Employee>("employees/1-A");
+					bool hasChanged = session.Advanced.HasChanged(employee); // false
+					employee.LastName = "Shmoe";
+					hasChanged = session.Advanced.HasChanged(employee); // true
+					#endregion
+				}
+			}
+		}
+	}
+}
