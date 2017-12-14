0443b02b59a674de8e449b2b53e6746e813d2435
idanhaim <haimdude@gmail.com>
2017-12-14 16:02:18 +0200

    RDoc-862 add get-entity-change-vector, get-entity-id and get-entity-metadata

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/HowTo/GetMetadata.cs
@@ -0,0 +1,30 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.HowTo
+{
+    public class GetMetadata
+	{
+		private interface IFoo
+		{
+            #region get_metadata_1
+		    IMetadataDictionary GetMetadataFor<T>(T instance);
+			#endregion
+		}
+
+		public GetMetadata()
+		{
+			using (var store = new DocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					#region get_metadata_2
+					Employee employee = session.Load<Employee>("employees/1-A");
+					IMetadataDictionary metadata = session.Advanced.GetMetadataFor(employee);
+					#endregion
+				}
+			}
+		}
+	}
+}
