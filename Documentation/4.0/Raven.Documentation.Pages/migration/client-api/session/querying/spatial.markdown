10f95ff0250bc418f9466f6acf1d906681572db7
Pawel Pekrol <ppekrol@ravendb.net>
2017-12-21 12:27:50 +0100

    RDoc-674 migration guide for Spatial

---
+++ Documentation/4.0/Raven.Documentation.Pages/migration/client-api/session/querying/spatial.markdown
@@ -0,0 +1,47 @@
+﻿# How to migrate Spatial from 3.x?
+
+Spatial functionality have been merged into [RQL]() and to reflect that change the Client API have integrated this feature into the `session.Query` and `session.Advanced.DocumentQuery`. Below migration samples will focus on the `session.Query` - the most common and recommended way of interaction with querying capabilities on RavenDB.
+
+## Namespaces
+
+| 3.x | 4.0 |
+|:---:|:---:|
+| {CODE spatial_1@Migration\ClientApi\Session\Querying\Spatial.cs /} | {CODE spatial_2@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+## Example I - Index
+
+Following changes have been applied:
+
+1. All spatial fields must be created by using `CreateSpatialField` method
+2. No support for GeoJSON and other non-standard formats
+3. No support for spatial clustering
+
+| 3.x |
+|:---:|
+| {CODE spatial_index_1@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+| 4.0 |
+|:---:|
+| {CODE spatial_index_2@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+## Example II - RelatesToShape
+
+| 3.x | 4.0 |
+|:---:|:---:|
+| {CODE spatial_3@Migration\ClientApi\Session\Querying\Spatial.cs /} | {CODE spatial_4@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+## Example III - WithinRadiusOf
+
+| 3.x | 4.0 |
+|:---:|:---:|
+| {CODE spatial_5@Migration\ClientApi\Session\Querying\Spatial.cs /} | {CODE spatial_6@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+## Example IV - SortByDistance
+
+| 3.x | 4.0 |
+|:---:|:---:|
+| {CODE spatial_7@Migration\ClientApi\Session\Querying\Spatial.cs /} | {CODE spatial_8@Migration\ClientApi\Session\Querying\Spatial.cs /} |
+
+## Remarks
+
+{INFO You can read more about Spatial in our dedicated [Client API article](../../../../client-api/session/querying/how-to-query-a-spatial-index) or our [Indexing article](../../../../indexes/indexing-spatial-data). /}
---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/Migration/ClientApi/Session/Querying/Spatial.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+#region spatial_2
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes.Spatial;
+#endregion
+
+using Raven.Client.Documents.Indexes;
+
+/*
+#region spatial_1
+using Raven.Abstractions.Indexing;
+using Raven.Client.Document;
+#endregion
+*/
+
+namespace Raven.Documentation.Samples.Migration.ClientApi.Session.Querying
+{
+    public class Spatial
+    {
+        private class Foo
+        {
+            public Foo()
+            {
+                /*
+                #region spatial_3
+                List<SpatialDoc> results = session
+                    .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                    .Customize(x => x
+                        .RelatesToShape(
+                            "Shape", 
+                            "Circle(32.1234 23.4321 d=10.0000)", 
+                            SpatialRelation.Within))
+                    .ToList();
+                #endregion
+                */
+
+                /*
+                #region spatial_5
+                List<SpatialDoc> results = session
+                    .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                    .Customize(x => x
+                        .WithinRadiusOf("Shape", 10, 32.1234, 23.4321))
+                    .ToList();
+                #endregion
+                */
+
+                /*
+                #region spatial_7
+                List<SpatialDoc> results = session
+                    .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                    .Customize(x => x.SortByDistance())
+                    .Spatial(
+                        x => x.Shape, 
+                        criteria => criteria
+                            .WithinRadiusOf(10, 32.1234, 23.4321))
+                    .ToList();
+                #endregion
+                */
+            }
+
+            /*
+            #region spatial_index_1
+            private class SpatialDoc_ByShapeAndPoint : AbstractIndexCreationTask<SpatialDoc>
+            {
+                public SpatialDoc_ByShapeAndPoint()
+                {
+                    Map = docs => from spatial in docs
+                        select new
+                        {
+                            Shape = spatial.Shape,
+                            Point = spatial.Point,
+                            _ = SpatialGenerate("Coordinates", spatial.Point.Latitude, spatial.Point.Longitude),
+                            _ = SpatialClustering("Clustering", spatial.Point.Latitude, spatial.Point.Longitude)
+                        };
+
+                    Spatial(x => x.Shape, options => options.Geography.Default());
+                    Spatial(x => x.Point, options => options.Cartesian.BoundingBoxIndex());
+                }
+            }
+            #endregion
+            */
+        }
+
+        public Spatial()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region spatial_4
+                    List<SpatialDoc> results = session
+                        .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                        .Spatial(
+                            x => x.Shape,
+                            criteria => criteria
+                                .RelatesToShape(
+                                    "Circle(32.1234 23.4321 d=10.0000)",
+                                    SpatialRelation.Within))
+                        .ToList();
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region spatial_6
+                    List<SpatialDoc> results = session
+                        .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                        .Spatial(
+                            x => x.Shape,
+                            criteria => criteria
+                                .WithinRadius(10, 32.1234, 23.4321))
+                        .ToList();
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region spatial_8
+                    List<SpatialDoc> results = session
+                        .Query<SpatialDoc, SpatialDoc_ByShapeAndPoint>()
+                        .Spatial(
+                            x => x.Shape,
+                            criteria => criteria
+                                .WithinRadius(10, 32.1234, 23.4321))
+                        .OrderByDistance(x => x.Shape, 32.1234, 23.4321)
+                        .ToList();
+                    #endregion
+                }
+            }
+        }
+
+        private class SpatialDoc
+        {
+            public Shape Shape { get; set; }
+
+            public Point Point { get; set; }
+        }
+
+        public class Point
+        {
+            public double Latitude { get; set; }
+
+            public double Longitude { get; set; }
+        }
+
+        public class Shape
+        {
+            public string Wkt { get; set; }
+        }
+
+        #region spatial_index_2
+        private class SpatialDoc_ByShapeAndPoint : AbstractIndexCreationTask<SpatialDoc>
+        {
+            public SpatialDoc_ByShapeAndPoint()
+            {
+                Map = docs => from spatial in docs
+                              select new
+                              {
+                                  Shape = CreateSpatialField(spatial.Shape.Wkt),
+                                  Point = CreateSpatialField(spatial.Point.Latitude, spatial.Point.Longitude),
+                                  Coordinates = CreateSpatialField(spatial.Point.Latitude, spatial.Point.Longitude)
+                              };
+
+                Spatial(x => x.Shape, options => options.Geography.Default());
+                Spatial(x => x.Point, options => options.Cartesian.BoundingBoxIndex());
+            }
+        }
+        #endregion
+    }
+}
