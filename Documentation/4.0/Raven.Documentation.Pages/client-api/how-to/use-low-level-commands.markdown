ed0023be0f8f6064ba4f83bd658335b694517c65
aviv86 <aviv@ayende.com>
2017-12-24 14:51:33 +0200

    RDoc-1030 : Client API > How to use low-level commands?

---
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/how-to/use-low-level-commands.markdown
@@ -0,0 +1,53 @@
+#How to use low-level commands?
+
+Low-level commands are the mechanisem that [Operations](../../client-api/operations/what-are-operations) are bult on top of.  
+When preforming an operation, there is an underlying low-level command that is in charge of sending the appropriate request to the server 
+(via the **Request Executor**) and parsing the server reply.
+
+In order to use low-level commands directly, you will need to use the `Execute` or `ExectueAsync` method of a  **Request Executor**.
+
+{CODE-TABS}
+{CODE-TAB:csharp:Execute Execute@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TAB:csharp:ExecuteAsync Execute_async@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TABS/}
+
+### Examples using session.Advanced.RequestExecutor  
+
+{PANEL:GetDocumentsCommand}
+{CODE-TABS}
+{CODE-TAB:csharp:Sync commands_1@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TAB:csharp:Async commands_1_async@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TABS/}
+{PANEL/}
+
+{PANEL:DeleteDocumentCommand}
+{CODE-TABS}
+{CODE-TAB:csharp:Sync commands_2@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TAB:csharp:Async commands_2_async@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TABS/}
+{PANEL/}
+
+### Examples using documentStore.GetRequestExecutor  
+
+{PANEL:GetDocumentsCommand}
+{CODE-TABS}
+{CODE-TAB:csharp:Sync commands_3@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TAB:csharp:Async commands_3_async@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TABS/}
+{PANEL/}
+
+{PANEL:DeleteDocumentCommand}
+{CODE-TABS}
+{CODE-TAB:csharp:Sync commands_4@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TAB:csharp:Async commands_4_async@ClientApi\HowTo\UseLowLevelCommands.cs /}
+{CODE-TABS/}
+{PANEL/}
+
+
+### The following low-level commands are available:
+
+#### Documents
+* [GetDocumentsCommand](../../client-api/commands/documents/get)   
+* [PutDocumentCommand](../../client-api/commands/documents/put)    
+* [DeleteDocumentCommand](../../client-api/commands/documents/delete)    
+
---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/HowTo/UseLowLevelCommands.cs
@@ -0,0 +1,121 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Session;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Documentation.Samples.ClientApi.HowTo
+{
+    public class UseLowLevelCommands
+    {
+        public interface IInterface
+        {
+            #region Execute
+            void Execute<TResult>(RavenCommand<TResult> command, JsonOperationContext context, CancellationToken token = default(CancellationToken), SessionInfo sessionInfo = null)
+            #endregion
+            ;
+            #region Execute_async
+            Task ExecuteAsync<TResult>(RavenCommand<TResult> command, JsonOperationContext context, CancellationToken token = default(CancellationToken), SessionInfo sessionInfo = null)
+            #endregion
+            ;
+        }
+
+        public async Task Examples()
+        {
+            using (var documentStore = new DocumentStore())
+            {
+                #region commands_1
+                using (var session = documentStore.OpenSession())
+                {
+                    var command = new GetDocumentsCommand("orders/1-A", null, false);
+                    session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
+                    var order = (BlittableJsonReaderObject)command.Result.Results[0];
+
+                }
+                #endregion
+
+                #region commands_1_async
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    var command = new GetDocumentsCommand("orders/1-A", null, false);
+                    await session.Advanced.RequestExecutor.ExecuteAsync(command, session.Advanced.Context);
+                    var order = (BlittableJsonReaderObject)command.Result.Results[0];
+                }
+                #endregion
+
+                #region commands_2
+                using (var session = documentStore.OpenSession())
+                {
+                    var command = new DeleteDocumentCommand("employees/1-A", null);
+                    session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
+
+                }
+                #endregion
+
+                #region commands_2_async
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    var command = new DeleteDocumentCommand("employees/1-A", null);
+                    await session.Advanced.RequestExecutor.ExecuteAsync(command, session.Advanced.Context);
+                }
+                #endregion
+
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    #region commands_3
+                    var requestExecutor = documentStore.GetRequestExecutor();
+                    using (var context = JsonOperationContext.ShortTermSingleUse())
+                    {
+                        var command = new GetDocumentsCommand("orders/1-A", null, false);
+                        requestExecutor.Execute(command, context);
+                        var order = (BlittableJsonReaderObject)command.Result.Results[0];
+                    }
+                    #endregion
+                }
+
+
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    #region commands_3_async
+                    var requestExecutor = documentStore.GetRequestExecutor();
+                    using (var context = JsonOperationContext.ShortTermSingleUse())
+                    {
+                        var command = new GetDocumentsCommand("orders/1-A", null, false);
+                        await requestExecutor.ExecuteAsync(command, context);
+                        var order = (BlittableJsonReaderObject)command.Result.Results[0];
+                    }
+                    #endregion
+                }
+
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    #region commands_4
+                    var requestExecutor = documentStore.GetRequestExecutor();
+                    using (var context = JsonOperationContext.ShortTermSingleUse())
+                    {
+                        var command = new DeleteDocumentCommand("employees/1-A", null);
+                        requestExecutor.Execute(command, context);
+                    }
+                    #endregion
+                }
+
+
+                using (var session = documentStore.OpenAsyncSession())
+                {
+                    #region commands_4_async
+                    var requestExecutor = documentStore.GetRequestExecutor();
+                    using (var context = JsonOperationContext.ShortTermSingleUse())
+                    {
+                        var command = new DeleteDocumentCommand("employees/1-A", null);
+                        await requestExecutor.ExecuteAsync(command, context);
+                    }
+                    #endregion
+                }
+
+
+            }
+        }
+    }
+}
