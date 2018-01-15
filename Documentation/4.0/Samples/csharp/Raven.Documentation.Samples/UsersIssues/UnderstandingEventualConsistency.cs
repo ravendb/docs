e06be8146cbd354f186086939dbb85d00dd47332
talweiss1982 <tal@ayende.com>
2018-01-15 13:56:21 +0200

    RDoc-1017 understanding eventual consistency

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/UsersIssues/UnderstandingEventualConsistency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Documents;
+
+namespace Raven.Documentation.Samples.UsersIssues
+{
+    class UnderstandingEventualConsistency
+    {
+        class Product
+        {
+            public string Name;
+        }
+
+
+        public void Sample1()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var pageSize = 0;
+                    var pageNumber = 0;
+                    var timeToWaitForQueryToBecomeNoneStaleInSec = TimeSpan.FromSeconds(5);
+                    #region userissues_1
+                    var results = session.Query<Product>()
+                        .Customize(x => x.WaitForNonStaleResults(timeToWaitForQueryToBecomeNoneStaleInSec))
+                        .OrderBy(x => x.Name)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize);
+                    #endregion
+                }
+            }
+        }
+
+        public void Sample2()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var pageSize = 0;
+                    var pageNumber = 0;
+                    var timeToWaitForIndexToUpdateInSec = TimeSpan.FromSeconds(5);
+                    #region userissues_2                    
+                    session.Advanced.WaitForIndexesAfterSaveChanges(timeToWaitForIndexToUpdateInSec);
+                    var results = session.Query<Product>()
+                        .OrderBy(x => x.Name)
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize);
+                    #endregion
+                }
+            }
+        }
+    }
+}
