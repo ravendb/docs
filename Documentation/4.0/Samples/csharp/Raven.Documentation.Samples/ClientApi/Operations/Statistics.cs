cfaf1b0395aa5f3f3a1b0eaf853b8cc41b2fb92f
Marcin Lewandowski <marcin@ravendb.net>
2018-01-18 15:56:07 +0100

    RDoc-834 Client API > Commands > How To > ...get database and server statistics

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Statistics.cs
@@ -0,0 +1,33 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Operations.Indexes;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations
+{
+    public class Statistics
+    {
+        private interface IFoo
+        {
+            /*
+            #region stats_1
+            public GetStatisticsOperation()
+            #endregion
+            */
+        }
+
+        public Statistics()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region stats_2
+                DatabaseStatistics stats = store.Maintenance.Send(new GetStatisticsOperation());
+                long countOfDocuments = stats.CountOfDocuments;
+                long countOfIndexes = stats.CountOfIndexes;
+                bool isStale = stats.Indexes[0].IsStale;
+                #endregion
+
+            }
+        }
+    }
+}
