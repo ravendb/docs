2623ababd3f5b1349ed38b4b7736abb8cc2ece6a
Marcin Lewandowski <marcin@ravendb.net>
2018-01-29 14:54:50 +0100

    RDoc-1076 Client API: How to wire up certificate

---
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/setting-up-authentication.dotnet.markdown
@@ -0,0 +1,14 @@
+# Setting up authentication 
+
+`Certificate` property allows to pass certificate which will be used by RavenDB client to connect to server. 
+
+{NOTE Your server must be running `https` if you want to use client certificate. You can find more information [here](../server/security/overview). /}
+
+## Example I
+
+{CODE client_cert@ClientApi\Certificate.cs /}
+
+
+## Related Articles
+
+- [How to create **secured server**?](../server/security/overview)
---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Certificate.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Raven.Documentation.Samples.ClientApi
+{
+	using System;
+	using Client.Documents;
+
+	public class Certificate
+	{
+	    public Certificate()
+	    {
+            #region client_cert
+            // load certificate
+            X509Certificate2 clientCertificate = new X509Certificate2("C:\\path_to_your_pfx_file\\cert.pfx");
+
+            using (IDocumentStore store = new DocumentStore()
+            {
+                Certificate = clientCertificate,
+                Database = "Northwind",
+                Urls = new[] {"https://my_secured_raven"}
+            }.Initialize())
+            {
+                // do your work here
+            }
+
+            #endregion
+	    }
+
+	}
+}
