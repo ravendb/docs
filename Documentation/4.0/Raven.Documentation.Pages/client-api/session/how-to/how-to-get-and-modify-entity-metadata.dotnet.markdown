e80b157a22f67cae027804ded38c7d92a6a99b3d
Idan Haim Shalom <haimdude@gmail.com>
2018-02-07 17:18:42 +0200

    RDoc-863 update get-entity-metadata to how-to-get-and-modify-entity-metadata

--- Documentation/4.0/Raven.Documentation.Pages/client-api/session/how-to/how-to-get-and-modify-entity-metadata.dotnet.markdown
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/session/how-to/how-to-get-and-modify-entity-metadata.dotnet.markdown
@@ -1,8 +1,8 @@
-# Session : How to Get Entity Metadata
+# Session : How to Get and Modify Entity Metadata
 
 When a document is downloaded from the server, it contains various metadata information e.g. ID or current change-vector. This information is stored in session and is available for each entity using the `GetMetadataFor` method from the `Advanced` session operations.
 
-## Syntax
+## Get the metadata
 
 {CODE get_metadata_1@ClientApi\Session\HowTo\GetMetadata.cs /}
 
@@ -14,6 +14,13 @@ When a document is downloaded from the server, it contains various metadata info
 | ------------- | ----- |
 | IMetadataDictionary | Returns the metadata for the specified entity. If the `instance` is transient, it will load the metadata from the store and associate the current state of the entity with the metadata from the server. |
 
-## Example
+### Example
 
 {CODE get_metadata_2@ClientApi\Session\HowTo\GetMetadata.cs /}
+
+
+## Modify the metadata
+After getting the metadata from ```session.session.Advanced.GetMetadataFor``` you can modify it just like any other dictionary.
+{NOTE ```Constants.Documents.Metadata``` class can help you with the defult convention keys of the metadata /}
+
+{CODE modify_metadata_1@ClientApi\Session\HowTo\GetMetadata.cs /}
--- Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/HowTo/GetMetadata.cs
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/HowTo/GetMetadata.cs
@@ -1,30 +1,47 @@
-﻿using Raven.Client.Documents;
+﻿using System;
+using Raven.Client;
+using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Indexes.Querying;
 using Raven.Documentation.Samples.Orders;
 
 namespace Raven.Documentation.Samples.ClientApi.Session.HowTo
 {
     public class GetMetadata
-	{
-		private interface IFoo
-		{
-            #region get_metadata_1
-		    IMetadataDictionary GetMetadataFor<T>(T instance);
-			#endregion
-		}
+    {
+        public GetMetadata()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region get_metadata_2
 
-		public GetMetadata()
-		{
-			using (var store = new DocumentStore())
-			{
-				using (var session = store.OpenSession())
-				{
-					#region get_metadata_2
-					Employee employee = session.Load<Employee>("employees/1-A");
-					IMetadataDictionary metadata = session.Advanced.GetMetadataFor(employee);
-					#endregion
-				}
-			}
-		}
-	}
+                    var employee = session.Load<Employee>("employees/1-A");
+                    var metadata = session.Advanced.GetMetadataFor(employee);
+
+                    #endregion
+
+                    #region modify_metadata_1
+
+                    var user = session.Load<User>("users/1-A");
+                    var userMetadata = session.Advanced.GetMetadataFor(user);
+                    if (userMetadata.ContainsKey(Constants.Documents.Metadata.LastModified))
+                        userMetadata[Constants.Documents.Metadata.LastModified] = DateTime.UtcNow;
+                    session.SaveChanges();
+
+                    #endregion
+                }
+            }
+        }
+
+        private interface IFoo
+        {
+            #region get_metadata_1
+
+            IMetadataDictionary GetMetadataFor<T>(T instance);
+
+            #endregion
+        }
+    }
 }
