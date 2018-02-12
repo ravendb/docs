7373acb087de9d742ba352024b066e77cb4735e3
iftahbe <iftahbe@gmail.com>
2018-02-12 18:45:55 +0200

    RDoc-699 move code to file

---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/Server/Security/GiveMeKey.cs
@@ -0,0 +1,21 @@
+﻿#region writing_key
+using System;
+using System.Security.Cryptography;
+
+namespace GiveMeKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new byte[256 / 8];
+            using (var cryptoRandom = new RNGCryptoServiceProvider())
+            {
+                cryptoRandom.GetBytes(buffer);
+            }
+            var stream = Console.OpenStandardOutput();
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
+#endregion
