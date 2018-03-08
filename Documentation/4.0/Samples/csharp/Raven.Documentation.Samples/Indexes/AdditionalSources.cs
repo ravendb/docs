96157fadaad7317233903dd179a51d6fa8aa2d8d
talweiss1982 <tal@ayende.com>
2018-03-08 15:57:22 +0200

    RDoc-1089 fixing AdditionalSources documentation

--- Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/Indexes/AdditionalSources.cs
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/Indexes/AdditionalSources.cs
@@ -19,36 +19,35 @@ namespace Raven.Documentation.Samples.Indexes
 #region additional_sources_1
         public class PeopleByEmail : AbstractIndexCreationTask<Person>
         {
-            public class PeopleByEmailResult
-            {
-                public string Email { get; set; }
-            }
-
             public PeopleByEmail()
             {
-                Map = people => from person in people
-                                select new
-                                {
-                                    _ = CreateField("Email", CalculatePersonEmail(person.Name, person.Age), true, true),
-                                };
+                Map = people => from person in people select new
+                        {
+                            // Calling the custom funcion
+                            Email = CalculatePersonEmail(person.Name, person.Age)
+                        };
+
+                // Add your custom logic here. 
                 AdditionalSources = new Dictionary<string, string>
                 {
                     {
-                        "PeopleUtil",
-                        @"
-                        using System;
-                        using NodaTime;
-                        using static Raven.Documentation.Samples.Indexes.PeopleUtil;
-                        namespace Raven.Documentation.Samples.Indexes
-                        {
-                            public static class PeopleUtil
-                            {
-                                public static string CalculatePersonEmail(string name, uint age)
-                                {
-                                    return $""{name}.{Instant.FromDateTimeUtc(DateTime.Now.ToUniversalTime()).ToDateTimeUtc().Year - age}@ayende.com"";
-                                }
-                            }
-                        }"
+        "PeopleUtil",
+        @"
+        using System;
+        using NodaTime; /* using an external library */
+        using static Raven.Documentation.Samples.Indexes.PeopleUtil;
+
+        namespace Raven.Documentation.Samples.Indexes
+        {
+            public static class PeopleUtil
+            {
+                public static string CalculatePersonEmail(string name, uint age)
+                {
+                    return $""{name}.{Instant.FromDateTimeUtc(DateTime.Now.ToUniversalTime())
+                                    .ToDateTimeUtc().Year - age}@ayende.com"";
+                }
+            }
+        }"
                     }
                 };
             }
