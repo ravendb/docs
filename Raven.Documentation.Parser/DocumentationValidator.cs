5d42fea3f69a986d0a37457db4d3470b9a568ff6
Pawel Pekrol <ppekrol@ravendb.net>
2018-04-18 11:33:44 +0200

    validating also related articles section

--- Raven.Documentation.Parser/DocumentationValidator.cs
+++ Raven.Documentation.Parser/DocumentationValidator.cs
@@ -1,203 +1,221 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Raven.Documentation.Parser.Data;
+using Raven.Documentation.Parser.Helpers;
 
 namespace Raven.Documentation.Parser
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Net.Http;
+    public class DocumentationValidator
+    {
+        private readonly ParserOptions _options;
 
-	using Raven.Documentation.Parser.Data;
-	using Raven.Documentation.Parser.Helpers;
+        private readonly Language _currentLanguage;
 
-	public class DocumentationValidator
-	{
-		private readonly ParserOptions _options;
+        public DocumentationValidator(ParserOptions options, Language currentLanguage)
+        {
+            _options = options;
+            _currentLanguage = currentLanguage;
+        }
 
-		private Language _currentLanguage;
+        public IEnumerable<PageLinksValidationResult> ValidateLinks(IList<DocumentationPage> pages)
+        {
+            var results = new ConcurrentBag<PageLinksValidationResult>();
 
-		public DocumentationValidator(ParserOptions options, Language currentLanguage)
-		{
-			_options = options;
-			_currentLanguage = currentLanguage;
-		}
+            var p = new List<DocumentationPage>[2];
 
-		public IEnumerable<PageLinksValidationResult> ValidateLinks(IList<DocumentationPage> pages)
-		{
-			var results = new ConcurrentBag<PageLinksValidationResult>();
+            var half = pages.Count / 2;
+            p[0] = pages.Take(half).ToList();
+            p[1] = pages.Skip(half).ToList();
 
-			var p = new List<DocumentationPage>[2];
+            Parallel.For(
+                0,
+                2,
+                i =>
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var pagesToCheck = p[i];
+                        foreach (var page in pagesToCheck)
+                            results.Add(ValidatePageLinks(client, page));
+                    }
+                });
 
-			var half = pages.Count / 2;
-			p[0] = pages.Take(half).ToList();
-			p[1] = pages.Skip(half).ToList();
+            return results;
+        }
 
-			Parallel.For(
-				0,
-				2,
-				i =>
-				{
-					using (var client = new HttpClient())
-					{
-						var pagesToCheck = p[i];
-						foreach (var page in pagesToCheck)
-							results.Add(ValidatePageLinks(client, page, pages));
-					}
-				});
+        private PageLinksValidationResult ValidatePageLinks(HttpClient client, DocumentationPage page)
+        {
+            void ValidateLinks(HttpClient httpClient, List<HtmlNode> toValidate, Uri baseUri, PageLinksValidationResult results)
+            {
+                foreach (var link in toValidate)
+                {
+                    var hrefAttribute = link.Attributes["href"];
+                    var href = hrefAttribute.Value.Trim();
 
-			return results;
-		}
+                    try
+                    {
+                        var isValid = false;
+                        Uri uri = null;
+                        if (string.IsNullOrEmpty(href) == false)
+                        {
+                            uri = href.StartsWith("http")
+                                ? new Uri(href, UriKind.Absolute)
+                                : new Uri(baseUri + href, UriKind.Absolute);
+                            var indexOfHash = uri.AbsoluteUri.IndexOf("#", StringComparison.InvariantCultureIgnoreCase);
+                            if (indexOfHash != -1)
+                            {
+                                var potentialGuid = uri.AbsoluteUri.Substring(indexOfHash + 1);
+                                if (Guid.TryParse(potentialGuid, out Guid _))
+                                    continue;
+                            }
 
-		private PageLinksValidationResult ValidatePageLinks(HttpClient client, DocumentationPage page, IList<DocumentationPage> pages)
-		{
-			var result = new PageLinksValidationResult(page.Key, page.Language == Language.All ? _currentLanguage : page.Language, page.Version);
+                            isValid = ValidatePageLink(httpClient, uri);
+                        }
 
-			var htmlDocument = HtmlHelper.ParseHtml(page.HtmlContent);
-			var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
-			if (links == null)
-				return result;
+                        results.Links[$"[{link.InnerText}][{uri}]"] = isValid;
+                    }
+                    catch (Exception)
+                    {
+                        results.Links[href] = false;
+                    }
+                }
+            }
 
-			var currentUri = new Uri(_options.RootUrl + page.Key + "/../");
+            var result = new PageLinksValidationResult(page.Key, page.Language == Language.All ? _currentLanguage : page.Language, page.Version);
 
-			foreach (var link in links)
-			{
-				var hrefAttribute = link.Attributes["href"];
-				var href = hrefAttribute.Value.Trim();
+            var htmlDocument = HtmlHelper.ParseHtml(page.HtmlContent);
+            var linksToValidate = new List<HtmlNode>();
 
-				try
-				{
-					var isValid = false;
-					Uri uri = null;
-					if (string.IsNullOrEmpty(href) == false)
-					{
-						uri = href.StartsWith("http") ? new Uri(href, UriKind.Absolute) : new Uri(currentUri + href, UriKind.Absolute);
-						var indexOfHash = uri.AbsoluteUri.IndexOf("#", StringComparison.InvariantCultureIgnoreCase);
-						if (indexOfHash != -1)
-						{
-							var potentialGuid = uri.AbsoluteUri.Substring(indexOfHash + 1);
-							Guid guid;
-							if (Guid.TryParse(potentialGuid, out guid))
-								continue;
-						}
+            var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+            if (links != null)
+                linksToValidate.AddRange(links);
 
-						isValid = ValidatePageLink(client, uri);
-					}
+            if (string.IsNullOrWhiteSpace(page.RelatedArticlesContent) == false)
+            {
+                htmlDocument = HtmlHelper.ParseHtml(page.RelatedArticlesContent);
+                links = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+                if (links != null)
+                    linksToValidate.AddRange(links);
+            }
 
-					result.Links[string.Format("[{0}][{1}]", link.InnerText, uri)] = isValid;
-				}
-				catch (Exception)
-				{
-					result.Links[href] = false;
-				}
-			}
+            if (linksToValidate.Count == 0)
+                return result;
 
-			return result;
-		}
+            var currentUri = new Uri(_options.RootUrl + page.Key + "/../");
 
-		private static bool ValidatePageLink(HttpClient client, Uri uri)
-		{
-			try
-			{
-				var url = uri.AbsoluteUri;
+            ValidateLinks(client, linksToValidate, currentUri, result);
 
-				var response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)).Result;
-				if (response.IsSuccessStatusCode == false) 
-					return false;
+            return result;
+        }
 
-				var content = response.Content.ReadAsStringAsync().Result;
-				if (content.Contains("The article you are looking for does not exist."))
-					return false;
+        private static bool ValidatePageLink(HttpClient client, Uri uri)
+        {
+            try
+            {
+                var url = uri.AbsoluteUri;
 
-				return true;
-			}
-			catch (Exception)
-			{
-				return false;
-			}
-		}
+                var response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)).Result;
+                if (response.IsSuccessStatusCode == false)
+                    return false;
 
-		private static readonly MappingVersion[] MappingVersionsToCheck = {
-			new MappingVersion { FloatValue = 1.0f, StringValue = "1.0"},
-			new MappingVersion { FloatValue = 2.0f, StringValue = "2.0"},
+                var content = response.Content.ReadAsStringAsync().Result;
+                if (content.Contains("The article you are looking for does not exist."))
+                    return false;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static readonly MappingVersion[] MappingVersionsToCheck = {
+            new MappingVersion { FloatValue = 1.0f, StringValue = "1.0"},
+            new MappingVersion { FloatValue = 2.0f, StringValue = "2.0"},
             new MappingVersion { FloatValue = 2.5f, StringValue = "2.5"},
             new MappingVersion { FloatValue = 3.0f, StringValue = "3.0"},
             new MappingVersion { FloatValue = 3.5f, StringValue = "3.5"}
         };
 
-		public IEnumerable<PageMappingsValidationResult> ValidateMappings(IList<DocumentationPage> pages)
-		{
-			var results = new ConcurrentBag<PageMappingsValidationResult>();
+        public IEnumerable<PageMappingsValidationResult> ValidateMappings(IList<DocumentationPage> pages)
+        {
+            var results = new ConcurrentBag<PageMappingsValidationResult>();
 
-			var p = new List<DocumentationPage>[2];
+            var p = new List<DocumentationPage>[2];
 
-			var half = pages.Count / 2;
-			p[0] = pages.Take(half).ToList();
-			p[1] = pages.Skip(half).ToList();
+            var half = pages.Count / 2;
+            p[0] = pages.Take(half).ToList();
+            p[1] = pages.Skip(half).ToList();
 
-			Parallel.For(
-				0,
-				2,
-				i =>
-				{
-					using (var client = new HttpClient())
-					{
-						var pagesToCheck = p[i];
+            Parallel.For(
+                0,
+                2,
+                i =>
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var pagesToCheck = p[i];
 
-						foreach (var page in pagesToCheck)
-						{
-							if (page.Key.EndsWith("/index") || page.Key == "index") // legacy index.markdown files
-								continue;
+                        foreach (var page in pagesToCheck)
+                        {
+                            if (page.Key.EndsWith("/index") || page.Key == "index") // legacy index.markdown files
+                                continue;
 
-							var result = new PageMappingsValidationResult(page.Key, page.Language == Language.All ? _currentLanguage : page.Language, page.Version);
+                            var result = new PageMappingsValidationResult(page.Key, page.Language == Language.All ? _currentLanguage : page.Language, page.Version);
 
-							foreach (var versionToCheck in MappingVersionsToCheck.Where(x => x.StringValue != page.Version))
-							{
-								var explicitMapping = page.Mappings.FirstOrDefault(x => Math.Abs(x.Version - versionToCheck.FloatValue) < float.Epsilon);
+                            foreach (var versionToCheck in MappingVersionsToCheck.Where(x => x.StringValue != page.Version))
+                            {
+                                var explicitMapping = page.Mappings.FirstOrDefault(x => Math.Abs(x.Version - versionToCheck.FloatValue) < float.Epsilon);
 
-								DocumentationMapping inheritedMapping = null;
+                                DocumentationMapping inheritedMapping = null;
 
-								if (explicitMapping == null)
-								{
-									if (versionToCheck.FloatValue < float.Parse(page.Version)) // switching from higher to lower version
-									{
-										inheritedMapping =
-											page.Mappings.OrderBy(x => x.Version).LastOrDefault(y => y.Version < versionToCheck.FloatValue);
-									}
-									else if (page.Mappings.Any(x => x.Version >= float.Parse(page.Version)))
-										// when switching from lower to higher version, make sure we don't get too lower mapping
-									{
-										inheritedMapping =
-											page.Mappings.OrderByDescending(x => x.Version).FirstOrDefault(y => y.Version < versionToCheck.FloatValue);
-									}
-								}
+                                if (explicitMapping == null)
+                                {
+                                    if (versionToCheck.FloatValue < float.Parse(page.Version)) // switching from higher to lower version
+                                    {
+                                        inheritedMapping =
+                                            page.Mappings.OrderBy(x => x.Version).LastOrDefault(y => y.Version < versionToCheck.FloatValue);
+                                    }
+                                    else if (page.Mappings.Any(x => x.Version >= float.Parse(page.Version)))
+                                    // when switching from lower to higher version, make sure we don't get too lower mapping
+                                    {
+                                        inheritedMapping =
+                                            page.Mappings.OrderByDescending(x => x.Version).FirstOrDefault(y => y.Version < versionToCheck.FloatValue);
+                                    }
+                                }
 
-								Uri uriToCheck;
+                                Uri uriToCheck;
 
-								var versionSpecificRootUrl = _options.RootUrl.Replace(page.Version, versionToCheck.StringValue);
+                                var versionSpecificRootUrl = _options.RootUrl.Replace(page.Version, versionToCheck.StringValue);
 
-								if (explicitMapping != null)
-								{
-									uriToCheck = new Uri(versionSpecificRootUrl + explicitMapping.Key);
-								}
-								else if (inheritedMapping != null)
-								{
-									uriToCheck = new Uri(versionSpecificRootUrl + inheritedMapping.Key);
-								}
-								else
-								{
-									uriToCheck = new Uri(versionSpecificRootUrl + page.Key);
-								}
+                                if (explicitMapping != null)
+                                {
+                                    uriToCheck = new Uri(versionSpecificRootUrl + explicitMapping.Key);
+                                }
+                                else if (inheritedMapping != null)
+                                {
+                                    uriToCheck = new Uri(versionSpecificRootUrl + inheritedMapping.Key);
+                                }
+                                else
+                                {
+                                    uriToCheck = new Uri(versionSpecificRootUrl + page.Key);
+                                }
 
-								result.Mappings.Add(versionToCheck.StringValue + " " + uriToCheck, ValidatePageLink(client, uriToCheck));
-							}
-							
-							results.Add(result);
-						}
-					}
-				});
+                                result.Mappings.Add(versionToCheck.StringValue + " " + uriToCheck, ValidatePageLink(client, uriToCheck));
+                            }
 
-			return results;
-		}
-	}
+                            results.Add(result);
+                        }
+                    }
+                });
+
+            return results;
+        }
+    }
 }
