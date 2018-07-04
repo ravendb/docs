9e49c0ace401d84fef7d651e36616fd3f72e4fa4
Pawel Pekrol <ppekrol@ravendb.net>
2018-07-04 09:28:14 +0200

    RDoc-1243 Document GetLogsConfigurationOperation and SetLogsConfigurationOperation

---
+++ Documentation/4.0/Raven.Documentation.Pages/client-api/operations/server-wide/logs/get-logs-configuration.markdown
@@ -0,0 +1,26 @@
+﻿# Operations : Server : How to Get Logs Configuration
+
+To get the server logs configuration use **GetLogsConfigurationOperation** from `Maintenance.Server`
+
+## Syntax
+
+{CODE:csharp get_logs_1@ClientApi\Operations\Server\GetAndSetLogsConfiguration.cs /}
+
+### Return Value
+
+The result of executing GetLogsConfigurationOperation is a **GetLogsConfigurationResult** object: 
+
+{CODE:csharp get_logs_2@ClientApi\Operations\Server\GetAndSetLogsConfiguration.cs /}
+
+| ------------- |----- |
+| **CurrentMode** | Current mode that is active |
+| **Mode** | Mode that is written in the configuration file and which will be used after server restart |
+| **Path** | Path to which logs will be written |
+| **UseUtcTime** | Indicates if logs will be written in UTC or in server local time |
+
+##Example
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync get_logs_3@ClientApi\Operations\Server\GetAndSetLogsConfiguration.cs /}
+{CODE-TAB:csharp:Async get_logs_4@ClientApi\Operations\Server\GetAndSetLogsConfiguration.cs /}
+{CODE-TABS/}
---
+++ Documentation/4.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/GetAndSetLogsConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide.Operations.Logs;
+using Sparrow.Logging;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Server
+{
+    public class GetAndSetLogsConfiguration
+    {
+        private class Syntax
+        {
+            public class GetLogsConfigurationOperation
+            {
+                #region get_logs_1
+                public GetLogsConfigurationOperation()
+                #endregion
+                {
+                }
+            }
+
+            #region get_logs_2
+            public class GetLogsConfigurationResult
+            {
+                public LogMode CurrentMode { get; set; }
+
+                public LogMode Mode { get; set; }
+
+                public string Path { get; set; }
+
+                public bool UseUtcTime { get; set; }
+            }
+            #endregion
+
+            public class SetLogsConfigurationOperation
+            {
+                #region set_logs_2
+                public class Parameters
+                {
+                    public LogMode Mode { get; set; }
+                }
+                #endregion
+
+                #region set_logs_1
+                public SetLogsConfigurationOperation(Parameters parameters)
+                #endregion
+                {
+                }
+            }
+        }
+
+        public async Task Samples()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region get_logs_3
+                GetLogsConfigurationResult logsConfiguration = store
+                    .Maintenance
+                    .Server
+                    .Send(new GetLogsConfigurationOperation());
+                #endregion
+
+                #region set_logs_3
+                store
+                    .Maintenance
+                    .Server
+                    .Send(new SetLogsConfigurationOperation(
+                        new SetLogsConfigurationOperation.Parameters
+                        {
+                            Mode = LogMode.Information
+                        }));
+                #endregion
+            }
+
+            using (var store = new DocumentStore())
+            {
+                #region get_logs_4
+                GetLogsConfigurationResult logsConfiguration = await store
+                    .Maintenance
+                    .Server
+                    .SendAsync(new GetLogsConfigurationOperation());
+                #endregion
+
+                #region set_logs_4
+                await store
+                    .Maintenance
+                    .Server
+                    .SendAsync(new SetLogsConfigurationOperation(
+                        new SetLogsConfigurationOperation.Parameters
+                        {
+                            Mode = LogMode.Information
+                        }));
+                #endregion
+            }
+        }
+    }
+}
