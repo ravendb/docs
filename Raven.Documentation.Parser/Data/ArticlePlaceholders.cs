3fd87fcb6b7d70f79b13038cf159f2364d74943a
Tomasz Opalach <tomtom@ravendb.net>
2018-07-10 13:44:32 +0200

    article: "ACID 2020: The Next Generation of Data Consistency"

---
+++ Raven.Documentation.Parser/Data/ArticlePlaceholders.cs
@@ -0,0 +1,8 @@
+﻿namespace Raven.Documentation.Parser.Data
+{
+    public static class ArticlePlaceholders
+    {
+        public const string WhitepaperBanner = "{{WHITEPAPER_BANNER}}";
+        public const string IntroVideo = "{{INTRO_VIDEO}}";
+    }
+}
