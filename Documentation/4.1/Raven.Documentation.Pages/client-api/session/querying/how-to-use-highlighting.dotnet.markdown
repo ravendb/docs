5e00faa99f62ef878176bab6ee3d6d86a23344d7
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-28 12:54:29 +0200

    RDoc-1273 documented highlighting in client-api/session/querying/how-to-use-highlighting

---
+++ Documentation/4.1/Raven.Documentation.Pages/client-api/session/querying/how-to-use-highlighting.dotnet.markdown
@@ -0,0 +1,40 @@
+# Session : Querying : How to use Highlighting
+
+**Highlighting** can be a great feature for increasing search UX. To take leverage of it use `Highlight` method.
+
+## Syntax
+
+{CODE highlight_1@ClientApi\Session\Querying\HowToUseHighlighting.cs /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **fieldName** | string | Name of a field to highlight. |
+| **path** | `Expression<Func<T, object>>` | Path to a field to highlight. |
+| **fragmentLength** | int | Maximum length of text fragments that will be returned. |
+| **fragmentCount** | int | Maximum number of fragments that will be returned. |
+| **options** | `HighlightingOptions`  | Options that can be used for customization. |
+| **highlightings** | `Highlightings` | Instance of a Highlightings that contains the highlight fragments for each returned result. |
+
+### Options
+
+{CODE options@ClientApi\Session\Querying\HowToUseHighlighting.cs /}
+
+| Options | | |
+| ------------- | ------------- | ----- |
+| **GroupKey** | string | Grouping key for the results. If `null` results are grouped by document ID (default). |
+| **PreTags** | `string[]` | Array of pre tags used when highlighting. |
+| **PostTags** | `string[]` | Array of post tags used when highlighting. |
+
+## Example
+
+{CODE highlight_2@ClientApi\Session\Querying\HowToUseHighlighting.cs /}
+
+## Related articles
+
+### Session
+
+- [How to Query](../../../client-api/session/querying/how-to-query)
+
+### Indexes
+
+- [Highlighting](../../../indexes/querying/highlighting)
---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Querying/HowToUseHighlighting.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Queries.Highlighting;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Querying
+{
+    public class HowToUseHighlighting
+    {
+        public class ContentSearchIndex : AbstractIndexCreationTask<SearchItem>
+        {
+            public ContentSearchIndex()
+            {
+                Map = docs => from doc in docs
+                              select new
+                              {
+                                  Text = doc.Text
+                              };
+
+                Index(x => x.Text, FieldIndexing.Search);
+                Store(x => x.Text, FieldStorage.Yes);
+                TermVector(x => x.Text, FieldTermVector.WithPositionsAndOffsets);
+            }
+        }
+
+        public class SearchItem
+        {
+            public string Id { get; set; }
+
+            public string Text { get; set; }
+        }
+
+        private class Foo
+        {
+            #region options
+            public string GroupKey { get; set; }
+
+            public string[] PreTags { get; set; }
+
+            public string[] PostTags { get; set; }
+            #endregion
+        }
+
+        private interface IFoo<T>
+        {
+            #region highlight_1
+            IRavenQueryable<T> Highlight(
+                string fieldName,
+                int fragmentLength,
+                int fragmentCount,
+                out Highlightings highlightings);
+
+            IRavenQueryable<T> Highlight(
+                string fieldName,
+                int fragmentLength,
+                int fragmentCount,
+                HighlightingOptions options,
+                out Highlightings highlightings);
+
+            IRavenQueryable<T> Highlight(
+                Expression<Func<T, object>> path,
+                int fragmentLength,
+                int fragmentCount,
+                out Highlightings highlightings);
+
+            IRavenQueryable<T> Highlight(
+                Expression<Func<T, object>> path,
+                int fragmentLength,
+                int fragmentCount,
+                HighlightingOptions options,
+                out Highlightings highlightings);
+            #endregion
+        }
+
+        public HowToUseHighlighting()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region highlight_2
+                    SearchItem[] results = session
+                        .Query<SearchItem, ContentSearchIndex>()
+                        .Highlight(x => x.Text, 128, 1, out Highlightings highlightings)
+                        .Search(x => x.Text, "raven")
+                        .ToArray();
+
+                    StringBuilder builder = new StringBuilder()
+                        .AppendLine("<ul>");
+
+                    foreach (SearchItem result in results)
+                    {
+                        string[] fragments = highlightings.GetFragments(result.Id);
+                        builder.AppendLine($"<li>{fragments.First()}</li>");
+                    }
+
+                    string ul = builder
+                        .AppendLine("</ul>")
+                        .ToString();
+                    #endregion
+                }
+            }
+        }
+    }
+}
