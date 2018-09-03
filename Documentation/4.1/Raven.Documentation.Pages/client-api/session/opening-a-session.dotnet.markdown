199eaa7d657de0bef51fd50238b416aacff92a7d
Pawel Pekrol <ppekrol@ravendb.net>
2018-09-03 12:02:40 +0200

    RDoc-1289 & RDoc-1294 copied articles

---
+++ Documentation/4.1/Raven.Documentation.Pages/client-api/session/opening-a-session.dotnet.markdown
@@ -0,0 +1,63 @@
+# Session : Opening a Session
+
+To open synchronous session use the `OpenSession` method from `DocumentStore` or `OpenAsyncSession` if you prefer working in an asynchronous manner.
+
+## Syntax
+
+There are three overloads of `OpenSession / OpenAsyncSession` methods
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync open_session_1@ClientApi\Session\OpeningSession.cs /}
+{CODE-TAB:csharp:Async open_session_1_1@ClientApi\Session\OpeningSession.cs /}
+{CODE-TABS/}
+
+The first method is an equivalent of doing
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync open_session_2@ClientApi\Session\OpeningSession.cs /}
+{CODE-TAB:csharp:Async open_session_2_1@ClientApi\Session\OpeningSession.cs /}
+{CODE-TABS/}
+
+The second method is an equivalent of doing
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync open_session_3@ClientApi\Session\OpeningSession.cs /}
+{CODE-TAB:csharp:Async open_session_3_1@ClientApi\Session\OpeningSession.cs /}
+{CODE-TABS/}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **options** | `OpenSessionOptions` | Options **containing** information such as **name of database** and **RequestExecutor**. |
+
+| Return Value | |
+| ------------- | ----- |
+| IDocumentSession / IAsyncDocumentSession | Instance of a session object. |
+
+## Example
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync open_session_4@ClientApi\Session\OpeningSession.cs /}
+{CODE-TAB:csharp:Async open_session_5@ClientApi\Session\OpeningSession.cs /}
+{CODE-TABS/}
+
+
+{DANGER:Important}
+**Always remember to release session allocated resources after usage by invoking the `Dispose` method or wrapping the session object in the `using` statement.**
+{DANGER/}
+
+## Related Articles
+
+### Session
+
+- [What is a Session and How Does it Work](../../client-api/session/what-is-a-session-and-how-does-it-work) 
+- [Storing Entities](../../client-api/session/storing-entities)
+- [Loading Entities](../../client-api/session/loading-entities)
+- [Saving Changes](../../client-api/session/saving-changes)
+
+### Querying
+
+- [Basics](../../indexes/querying/basics)
+
+### Document Store
+
+- [What is a Document Store](../../client-api/what-is-a-document-store)
---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/OpeningSession.cs
@@ -0,0 +1,91 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Raven.Client.Http;
+
+namespace Raven.Documentation.Samples.ClientApi.Session
+{
+    public class OpeningSession
+    {
+        private interface IFoo2
+        {
+            #region session_options
+            string Database { get; set; }
+
+            bool NoTracking { get; set; }
+
+            bool NoCaching { get; set; }
+
+            RequestExecutor RequestExecutor { get; set; }
+
+            TransactionMode TransactionMode { get; set; }
+            #endregion
+        }
+
+        private interface IFoo
+        {
+            #region open_session_1
+            // Open session for a 'default' database configured in 'DocumentStore'
+            IDocumentSession OpenSession();
+
+            // Open session for a specified database
+            IDocumentSession OpenSession(string database);
+
+            IDocumentSession OpenSession(SessionOptions options);
+            #endregion
+
+            #region open_session_1_1
+            // Open session for a 'default' database configured in 'DocumentStore'
+            IAsyncDocumentSession OpenAsyncSession();
+
+            // Open session for a specified database
+            IAsyncDocumentSession OpenAsyncSession(string database);
+
+            IAsyncDocumentSession OpenAsyncSession(SessionOptions options);
+            #endregion
+        }
+
+        public OpeningSession()
+        {
+            string databaseName = "DB1";
+
+            using (var store = new DocumentStore())
+            {
+                #region open_session_2
+                store.OpenSession(new SessionOptions());
+                #endregion
+
+                #region open_session_2_1
+                store.OpenAsyncSession(new SessionOptions());
+                #endregion
+
+                #region open_session_3
+                store.OpenSession(new SessionOptions
+                {
+                    Database = databaseName
+                });
+                #endregion
+
+                #region open_session_3_1
+                store.OpenAsyncSession(new SessionOptions
+                {
+                    Database = databaseName
+                });
+                #endregion
+
+                #region open_session_4
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    // code here
+                }
+                #endregion
+
+                #region open_session_5
+                using (IAsyncDocumentSession session = store.OpenAsyncSession())
+                {
+                    // async code here
+                }
+                #endregion
+            }
+        }
+    }
+}
