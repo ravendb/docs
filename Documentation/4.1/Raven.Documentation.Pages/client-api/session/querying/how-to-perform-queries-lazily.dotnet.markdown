dd1150ad31e7226d649f6a4c69cc363b103a12a4
efrat@ayende.com <efrat@ayende.com>
2018-11-04 14:27:55 +0200

    RDoc-1280 Copy files

---
+++ Documentation/4.1/Raven.Documentation.Pages/client-api/session/querying/how-to-perform-queries-lazily.dotnet.markdown
@@ -0,0 +1,73 @@
+# Session : Querying : How to Perform Queries Lazily
+
+In some situations, query execution must be delayed. To cover such a scenario, `Lazily` and many other query extensions have been introduced.
+
+{PANEL:Lazily and LazilyAsync}
+
+{CODE lazy_1@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **onEval** | Action<IEnumerable&lt;TResult&gt;> | An action that will be performed on the query results. |
+
+| Return Value | |
+| ------------- | ----- |
+| Lazy<IEnumerable&lt;TResult&gt;> | Lazy query initializer returning query results. |
+
+### Example
+
+{CODE-TABS}
+{CODE-TAB:csharp:Sync lazy_2@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+{CODE-TAB:csharp:Async lazy_3@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+{CODE-TABS/}
+
+{PANEL/}
+
+{PANEL:Counts}
+
+{CODE lazy_4@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+| Return Value | |
+| ------------- | ----- |
+| Lazy<int> | Lazy query initializer returning a count of matched documents. |
+
+### Example
+
+{CODE lazy_5@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+{PANEL/}
+
+{PANEL:Suggestions}
+
+{CODE lazy_6@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+| Return Value | |
+| ------------- | ----- |
+| Lazy<Dictionary<string, SuggestionResult>> | Lazy query initializer containing a dictionary with suggestions for matching executed query |
+
+### Example
+
+{CODE lazy_7@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+{PANEL/}
+
+{PANEL:Facets}
+
+{CODE lazy_8@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+| Return Value | |
+| ------------- | ----- |
+| Lazy<Dictionary<string, FacetResult>> | Lazy query initializer containing a dictionary with facet results matching executed query |
+
+### Example
+
+{CODE lazy_9@ClientApi\Session\Querying\HowToPerformQueriesLazily.cs /}
+
+{PANEL/}
+
+## Related Articles
+
+### Session
+
+- [How to Query](../../../client-api/session/querying/how-to-query)
+- [How to Perform Operations Lazily](../../../client-api/session/how-to/perform-operations-lazily)
---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Querying/HowToPerformQueriesLazily.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Queries.Facets;
+using Raven.Client.Documents.Queries.Suggestions;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Querying
+{
+    public class HowToPerformQueriesLazily
+    {
+        private interface IFoo
+        {
+            #region lazy_1
+            Lazy<IEnumerable<T>> Lazily<T>();
+
+            Lazy<IEnumerable<T>> Lazily<T>(Action<IEnumerable<T>> onEval);
+
+            Lazy<Task<IEnumerable<T>>> LazilyAsync<T>();
+
+            Lazy<Task<IEnumerable<T>>> LazilyAsync<T>(Action<IEnumerable<T>> onEval);
+            #endregion
+
+            #region lazy_4
+            Lazy<int> CountLazily<T>();
+
+            Lazy<Task<int>> CountLazilyAsync<T>(CancellationToken token = default(CancellationToken));
+            #endregion
+
+
+            #region lazy_6
+            Lazy<Dictionary<string, SuggestionResult>> ExecuteLazy(Action<Dictionary<string, SuggestionResult>> onEval = null);
+
+            Lazy<Task<Dictionary<string, SuggestionResult>>> ExecuteLazyAsync(Action<Dictionary<string, SuggestionResult>> onEval = null);
+            #endregion
+
+            #region lazy_8
+            Lazy<Dictionary<string, FacetResult>> ExecuteLazy(Action<Dictionary<string, FacetResult>> onEval = null);
+
+            Lazy<Task<Dictionary<string, FacetResult>>> ExecuteLazyAsync(Action<Dictionary<string, FacetResult>> onEval = null);
+            #endregion
+        }
+
+        public HowToPerformQueriesLazily()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region lazy_2
+                    Lazy<IEnumerable<Employee>> employeesLazy = session
+                        .Query<Employee>()
+                        .Where(x => x.FirstName == "Robert")
+                        .Lazily();
+
+                    IEnumerable<Employee> employees = employeesLazy.Value; // query will be executed here
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region lazy_5
+                    Lazy<int> countLazy = session
+                        .Query<Employee>()
+                        .Where(x => x.FirstName == "Robert")
+                        .CountLazily();
+
+                    int count = countLazy.Value; // query will be executed here
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region lazy_7
+                    Lazy<Dictionary<string, SuggestionResult>> suggestLazy = session
+                        .Query<Employee>("Employees_ByFullName")
+                        .SuggestUsing(builder => builder.ByField("FullName", "johne"))
+                        .ExecuteLazy();
+
+                    Dictionary<string, SuggestionResult> suggest = suggestLazy.Value; // query will be executed here
+                    List<string> suggestions = suggest["FullName"].Suggestions;
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region lazy_9
+                    Lazy<Dictionary<string, FacetResult>> facetsLazy = session
+                        .Query<Camera>("Camera/Costs")
+                        .AggregateUsing("facets/CameraFacets")
+                        .ExecuteLazy();
+
+                    Dictionary<string, FacetResult> facets = facetsLazy.Value; // query will be executed here
+                    FacetResult results = facets["Manufacturer"];
+                    #endregion
+                }
+            }
+        }
+
+        private async Task LazilyAsync()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region lazy_3
+                    Lazy<Task<IEnumerable<Employee>>> employeesLazy = session
+                        .Query<Employee>()
+                        .Where(x => x.FirstName == "Robert")
+                        .LazilyAsync();
+
+                    IEnumerable<Employee> employees = await employeesLazy.Value; // query will be executed here
+                    #endregion
+                }
+            }
+        }
+    }
+}
