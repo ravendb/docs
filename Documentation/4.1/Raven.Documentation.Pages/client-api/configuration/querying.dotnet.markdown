43b316251a300bec694545d9a2f0c1b139b0b687
aviv86 <aviv@ayende.com>
2018-11-06 13:07:17 +0200

    RDoc-1367 : add FindProjectionPropertyNameForIndex convention to ClientApi/Configuration/Querying

---
+++ Documentation/4.1/Raven.Documentation.Pages/client-api/configuration/querying.dotnet.markdown
@@ -0,0 +1,61 @@
+﻿# Conventions : Querying
+
+##FindPropertyNameForIndex and FindPropertyNameForDynamicIndex
+
+These two conventions specify functions that are used to find the indexed property name for a static index or a dynamic one. This can be useful when you are indexing nested properties 
+of complex types. Their default implementations are:
+
+{CODE find_prop_name@ClientApi\Configuration\Querying.cs /}
+
+The arguments in the order of appearance: an indexed document type, an index name, a current path and a property path.
+
+##FindProjectionPropertyNameForIndex
+
+This convention specifies a function that is used to find the projected property name for a static index.  
+This can be useful when you want to use a static index to project nested properties which are not [`Stored`](../../indexes/storing-data-in-index).
+
+{CODE find_projected_prop_name@ClientApi\Configuration\Querying.cs /}
+
+Same as in the two conventions above, the arguments in the order of appearance: an indexed document type, an index name, a current path and a property path.
+
+By default `FindProjectionPropertyNameForIndex` is set to `null`.  
+When `FindProjectionPropertyNameForIndex` is `null` (or returns `null`),  
+the `FindPropertyNameForIndex` convention is used instead.
+
+###Example
+Consider we have the following index, and we want to query `School.Id`:
+{CODE users_index@ClientApi\Configuration\Querying.cs /}
+
+{CODE find_projected_prop_exmple1@ClientApi\Configuration\Querying.cs /}
+
+And without setting `FindProjectedPropertyNameForIndex`:
+{CODE find_projected_prop_exmple2@ClientApi\Configuration\Querying.cs /}
+`School_Id` is indexed but not `Stored`, meaning that we will try to
+fetch `School_Id` from the document - which doesn't have this property.
+
+##ThrowIfQueryPageSizeIsNotSet
+
+Since RavenDB 4.0 there is no limitation for the number of results returned for a single query by the server. The `ThrowIfQueryPageSizeIsNotSet` convention decides whether RavenDB Client
+should prevent from executing queries if their page size is not explicitly set. 
+
+Enabling this configuration at development stage can be useful to pinpoint all the possible performance bottlenecks.
+
+{CODE throw_if_query_page_is_not_set@ClientApi\Configuration\Querying.cs /}
+
+## Related articles
+
+### Conventions
+
+- [Conventions](../../client-api/configuration/conventions)
+- [Serialization](../../client-api/configuration/serialization)
+- [Load Balance & Failover](../../client-api/configuration/load-balance-and-failover)
+
+### Document Identifiers
+
+- [Working with Document Identifiers](../../client-api/document-identifiers/working-with-document-identifiers)
+- [Global ID Generation Conventions](../../client-api/configuration/identifier-generation/global)
+- [Type-specific ID Generation Conventions](../../client-api/configuration/identifier-generation/type-specific)
+
+### Document Store
+
+- [What is a Document Store](../../client-api/what-is-a-document-store)
---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Configuration/Querying.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+
+namespace Raven.Documentation.Samples.ClientApi.Configuration
+{
+    public class Querying
+    {
+        #region users_index
+        private class UsersIndex : AbstractIndexCreationTask<User>
+        {
+            public UsersIndex()
+            {
+                Map = users => from user in users
+                    select new
+                    {
+                        SchoolId = user.School.Id
+                    };
+            }
+        }
+        #endregion
+
+        private class User
+        {
+            public Reference School { get; set; }
+        }
+
+        private class Reference
+        {
+            public string Id { get; set; }
+
+            #region find_projected_prop_name
+            public Func<Type, string, string, string, string> FindProjectedPropertyNameForIndex
+            #endregion
+            ;
+        }
+
+        public Querying()
+        {
+            {
+                var store = new DocumentStore()
+                {
+                    Conventions =
+                    {
+                        #region find_prop_name
+                        FindPropertyNameForIndex = (indexedType, indexName, path, prop) =>
+                            (path + prop).Replace(",", "_").Replace(".", "_"),
+
+                        FindPropertyNameForDynamicIndex = (indexedType, indexName, path, prop) => path + prop,
+                        #endregion
+
+                        #region throw_if_query_page_is_not_set
+                        ThrowIfQueryPageSizeIsNotSet = true
+                        #endregion
+                    }
+                };
+            }
+            {
+                #region find_projected_prop_exmple1
+                using (var store = new DocumentStore
+                {
+                    Conventions =
+                    {
+                        FindProjectedPropertyNameForIndex = (indexedType, indexName, path, prop) => path + prop
+                    }
+                })
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        var query1 = session.Query<User, UsersIndex>()
+                            .Select(u => u.School.Id).ToList();
+
+                        // Generated RQL : "from index 'UsersIndex' select School.Id"
+
+                        var query2 = session.Query<User, UsersIndex>()
+                            .Where(u => u.School.Id != null).ToList();
+
+                        // Generated RQL : "from index 'UsersIndex' where School_Id != null"
+
+                    }
+                }
+                #endregion
+            }
+            {
+                #region find_projected_prop_exmple2
+                using (var store = new DocumentStore())
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        // FindProjectedPropertyNameForIndex is null, FindPropertyNameForIndex will be used instead
+
+                        var query = session.Query<User, UsersIndex>()
+                            .Select(u => u.School.Id).ToList();
+
+                        // Generated RQL : "from index 'UsersIndex' select School_Id"
+                    }
+                }
+                #endregion
+            }
+        }
+    }
+}
