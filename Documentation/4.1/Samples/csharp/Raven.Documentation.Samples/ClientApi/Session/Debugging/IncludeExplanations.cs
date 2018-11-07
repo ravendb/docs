630d55defb81816982b4f402df85a92e7ccdfb14
Marcin Lewandowski <marcin@ravendb.net>
2018-11-07 10:21:54 +0100

    RDoc-1217 Document IncludeExplanations

---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Debugging/IncludeExplanations.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="IncludeExplanations.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Queries.Explanation;
+using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Debugging
+{
+    public class IncludeExplanations
+    {
+        public interface IFoo<T>
+        {
+            #region explain_1
+            IDocumentQuery<T> IncludeExplanations(out Explanations explanations);
+            #endregion
+        }
+
+        public async Task Explain()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region explain_2
+                    var syrups = session.Advanced.DocumentQuery<Product>()
+                        .IncludeExplanations(out Explanations explanations)
+                        .Search(x => x.Name, "Syrup")
+                        .ToList();
+
+                    string[] scoreDetails = explanations.GetExplanations(syrups[0].Id);
+                    #endregion
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    #region explain_3
+                    var syrups = await session.Advanced.AsyncDocumentQuery<Product>()
+                        .IncludeExplanations(out Explanations explanations)
+                        .Search(x => x.Name, "Syrup")
+                        .ToListAsync();
+
+                    string[] scoreDetails = explanations.GetExplanations(syrups[0].Id);
+                    #endregion
+                }
+            }
+        }
+    }
+}
