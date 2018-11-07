9df38011da99183712a2513e0e7edfe0ae513e22
aviv86 <aviv@ayende.com>
2018-11-07 16:15:55 +0200

    RDoc-1275 : Document CounterBatchOperation

--- Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Counters.cs
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Counters.cs
@@ -6,7 +6,7 @@ namespace Raven.Documentation.Samples.ClientApi.Operations
 {
     public class Counters
     {
-        public class GetCountersOperation
+        private class GetCountersOperation
         {
             #region get_single_counter
             public GetCountersOperation(string docId, string counter, bool returnFullResults = false)
@@ -25,6 +25,58 @@ namespace Raven.Documentation.Samples.ClientApi.Operations
 
         }
 
+        #region counter_batch
+
+        public class CounterBatch
+        {
+            public bool ReplyWithAllNodesValues; // A flag that indicates if the results should include a
+                                                 // dictionary of counter values per database node
+            public List<DocumentCountersOperation> Documents = new List<DocumentCountersOperation>();
+            public bool FromEtl;
+        }
+
+        #endregion
+
+        private class CounterBatchOperation
+        {
+            #region counter_batch_op
+            public CounterBatchOperation(CounterBatch counterBatch)
+            #endregion
+            { }
+        }
+
+        #region document_counters_op
+
+        public class DocumentCountersOperation
+        {
+            public string DocumentId; // Id of the document that holds the counters
+            public List<CounterOperation> Operations; // A list of counter operations to perform
+        }
+
+        #endregion
+
+        #region counter_operation
+
+        public class CounterOperation
+        {
+            public CounterOperationType Type;
+            public string CounterName;
+            public long Delta; // the value to increment by
+        }
+
+        #endregion
+
+        #region counter_operation_type
+
+        public enum CounterOperationType
+        {
+            Increment,
+            Delete,
+            Get
+        }
+
+        #endregion
+
         #region counters_detail
         public class CountersDetail
         {
@@ -38,7 +90,7 @@ namespace Raven.Documentation.Samples.ClientApi.Operations
             public string DocumentId; // ID of the document that holds the counter
             public string CounterName; // The counter name
             public long TotalValue; // Total counter value
-            public Dictionary<string, long> CounterValues; // A dictionary of counter value per database node
+            public Dictionary<string, long> CounterValues; // A dictionary of counter values per database node
             public long Etag; // Counter Etag
             public string ChangeVector; // Change vector of the counter
         }
@@ -84,6 +136,199 @@ namespace Raven.Documentation.Samples.ClientApi.Operations
                     var operationResult = store.Operations
                         .Send(new GetCountersOperation("users/1", "likes", true));
 
+                    #endregion
+                }
+                {
+                    #region counter_batch_exmpl1
+
+                    var operationResult = store.Operations.Send(new CounterBatchOperation(new CounterBatch
+                    {
+                        Documents = new List<DocumentCountersOperation>
+                        {
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/1",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Increment,
+                                        CounterName = "likes",
+                                        Delta = 5
+                                    },
+                                    new CounterOperation
+                                    {
+                                        // Delta not specified, increment by 1
+
+                                        Type = CounterOperationType.Increment,
+                                        CounterName = "dislikes"
+                                    }
+                                }
+                            },
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/2",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Increment,
+                                        CounterName = "likes",
+                                        Delta = 100
+                                    },
+                                    new CounterOperation
+                                    {
+                                        // this will create a new counter "score", with initial value 50
+                                        // "score" will be added to counter-names in "users/2" metadata
+
+                                        Type = CounterOperationType.Increment,
+                                        CounterName = "score", 
+                                        Delta = 50
+                                    }
+                                }
+                            }
+                        }
+                    }));
+                    #endregion
+                }
+                {
+                    #region counter_batch_exmpl2
+
+                    var operationResult = store.Operations.Send(new CounterBatchOperation(new CounterBatch
+                    {
+                        Documents = new List<DocumentCountersOperation>
+                        {
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/1",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "likes"
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "downloads"
+                                    }
+                                }
+                            },
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/2",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "likes"
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "dislikes"
+                                    }
+                                }
+                            }
+                        }
+                    }));
+                    #endregion
+                }
+                {
+                    #region counter_batch_exmpl3
+
+                    var operationResult = store.Operations.Send(new CounterBatchOperation(new CounterBatch
+                    {
+                        Documents = new List<DocumentCountersOperation>
+                        {
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/1",
+                                Operations = new List<CounterOperation>
+                                {
+                                    // "likes" and "dislikes" will be removed from counter-names in "users/1" metadata
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Delete,
+                                        CounterName = "likes"
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Delete,
+                                        CounterName = "dislikes"
+                                    }
+                                }
+                            },
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/2",
+                                Operations = new List<CounterOperation>
+                                {
+                                    // "downloads" will be removed from counter-names in "users/2" metadata
+
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Delete,
+                                        CounterName = "downloads"
+                                    }
+                                }
+                            }
+                        }
+                    }));
+                    #endregion
+                }
+
+                {
+                    #region counter_batch_exmpl4
+
+                    var operationResult = store.Operations.Send(new CounterBatchOperation(new CounterBatch
+                    {
+                        Documents = new List<DocumentCountersOperation>
+                        {
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/1",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Increment,
+                                        CounterName = "likes",
+                                        Delta = 10
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "dislikes"
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Delete,
+                                        CounterName = "downloads"
+                                    }
+                                }
+                            },
+                            new DocumentCountersOperation
+                            {
+                                DocumentId = "users/2",
+                                Operations = new List<CounterOperation>
+                                {
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Get,
+                                        CounterName = "likes"
+                                    },
+                                    new CounterOperation
+                                    {
+                                        Type = CounterOperationType.Delete,
+                                        CounterName = "dislikes"
+                                    }
+                                }
+                            }
+                        }
+                    }));
                     #endregion
                 }
             }
