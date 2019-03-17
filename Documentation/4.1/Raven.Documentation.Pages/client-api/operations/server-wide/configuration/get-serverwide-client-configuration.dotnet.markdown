c421de5a8366ebb6a19804610146cd22816f3a22
egor <egor@ravendb.net>
2019-03-17 12:50:32 +0200

    RDoc-1472: Copy files to v4.1

---
+++ Documentation/4.1/Raven.Documentation.Pages/client-api/operations/server-wide/configuration/get-serverwide-client-configuration.dotnet.markdown
@@ -0,0 +1,21 @@
+﻿# Operations : Server : How to Get Server-Wide Client Configuration
+
+**GetServerWideClientConfigurationOperation** is used to return a server-wide client configuration which is saved on the server and overrides the client behavior. 
+
+{NOTE `ClientConfiguration` defined at the database level overrides server-wide client configurations. /}
+
+## Syntax
+
+{CODE config_1_0@ClientApi\Operations\Server\ClientConfig.cs /}
+
+| Return Value | |
+| ------------- | ---- |
+| `ClientConfiguration` | configuration which will be used by the RavenDB Client |
+
+## Example
+
+{CODE config_1_2@ClientApi\Operations\Server\ClientConfig.cs /}
+
+## Related Articles
+
+- [How to get **client configuration**?](../../../../client-api/operations/maintenance/configuration/get-client-configuration)
---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/ClientConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Configuration;
+using Raven.Client.Documents.Queries;
+using Raven.Client.Http;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations.Configuration;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Server
+{
+    public class ClientConfig
+    {
+        private interface IFoo
+        {
+            /*
+            #region config_1_0
+            public GetServerWideClientConfigurationOperation()
+            #endregion
+            */
+
+            /*
+            #region config_2_0
+            public PutServerWideClientConfigurationOperation(ClientConfiguration configuration)
+            #endregion
+            */
+        }
+
+
+        public ClientConfig()
+        {
+            using (var store = new DocumentStore())
+            {
+                {
+                    #region config_1_2
+                    ClientConfiguration clientConfiguration =
+                        store.Maintenance.Server.Send(new GetServerWideClientConfigurationOperation());
+                    #endregion
+                }
+
+                {
+                    #region config_2_2
+                    ClientConfiguration clientConfiguration = new ClientConfiguration
+                    {
+                        MaxNumberOfRequestsPerSession = 100,
+                        ReadBalanceBehavior = ReadBalanceBehavior.FastestNode
+                    };
+                    
+                    store.Maintenance.Server.Send(new PutServerWideClientConfigurationOperation(clientConfiguration));
+                    #endregion
+                }
+
+
+
+            }
+        }
+    }
+}
