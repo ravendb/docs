8323c2fd9a3f36c168fee60b39934758350737c6
Marcin Lewandowski <marcin@ravendb.net>
2019-03-22 10:25:24 +0100

    RDoc-1483 copied certificate related operations from 4.0 -> 4.1

---
+++ Documentation/4.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/DeleteClientCertificate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide.Operations;
+using Raven.Client.ServerWide.Operations.Certificates;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Server
+{
+
+    public class DeleteClientCertificate
+    {
+        private interface IFoo
+        {
+            /*
+            #region delete_cert_1
+            public DeleteCertificateOperation(string thumbprint)
+            #endregion
+            */
+        }
+
+        public DeleteClientCertificate()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region delete_cert_2
+                string thumbprint = "a909502dd82ae41433e6f83886b00d4277a32a7b";
+                store.Maintenance.Server.Send(new DeleteCertificateOperation(thumbprint));
+                #endregion
+            }
+        }
+    }
+}
