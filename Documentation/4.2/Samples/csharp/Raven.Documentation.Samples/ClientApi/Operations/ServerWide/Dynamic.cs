11e05baa2c1bb9e2d5429c3bc70c33f48abe2da3
Mor <mor@ravendb.net>
2019-09-12 18:54:25 +0300

    RDoc-1632: SetDatabaseDynamicDistributionOperation documented

---
+++ Documentation/4.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/ServerWide/Dynamic.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Client.ServerWide.Operations;
+using Raven.Client.Documents;
+using System.Threading.Tasks;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.ServerWide
+{
+    class Class1
+    {
+        static async Task MainInternal()
+        {
+            using (IDocumentStore documentStore = new DocumentStore())
+            {
+                #region operation
+
+                SetDatabaseDynamicDistributionOperation operation =
+                    new SetDatabaseDynamicDistributionOperation("NorthWind", true);
+                documentStore.Maintenance.Server.Send(operation);
+
+                #endregion
+            }
+            
+            #region syntax
+            public SetDatabaseDynamicDistributionOperation(string databaseName, bool allowDynamicDistribution);
+            #endregion
+            
+        }
+    }
+}
