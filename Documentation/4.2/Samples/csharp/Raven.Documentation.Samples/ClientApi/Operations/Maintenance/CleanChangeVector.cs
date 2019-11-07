b58c7404a99561b40bbc74b169f8cc43ebcd912e
morhilai <mor@ayende.com>
2019-11-07 20:09:46 +0200

    RDoc-1671: New page for the operation to remove obsolete entries from the database change vector. - Added Change Vector Overview page, changed nothing besides adding this text linking to the new page:
    "If an instance is no longer being used, its entry can be removed from the change vector to save space using the [UpdateUnusedDatabasesOperation]()"

---
+++ Documentation/4.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Maintenance/CleanChangeVector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide.Operations;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Maintenance
+{
+    class CleanChangeVector
+    {
+        static void Main(string[] args)
+        {
+            /*
+            #region syntax_1
+            public UpdateUnusedDatabasesOperation(string database, HashSet<string> unusedDatabaseIds)
+            #endregion
+            */
+
+            var documentStore = new DocumentStore
+            {
+                Urls = new[]
+                {
+                    //"http://live-test.ravendb.net"
+                    "http://localhost:8080"
+                },
+                //Database = "Demo"
+                Database = "nwww"
+            }.Initialize();
+
+            using (var session = documentStore.OpenSession())
+            {
+                #region example_sync
+                documentStore.Maintenance.Server.Send(
+                                new UpdateUnusedDatabasesOperation(documentStore.Database, new HashSet<string>
+                {
+                    "0N64iiIdYUKcO+yq1V0cPA",
+                    "xwmnvG1KBkSNXfl7/0yJ1A"
+                }));
+                #endregion
+            }
+
+            using (var session = documentStore.OpenAsyncSession())
+            {
+                /*
+                #region example_async
+                await documentStore.Maintenance.Server.SendAsync(
+                                new UpdateUnusedDatabasesOperation(documentStore.Database, new HashSet<string>
+                {
+                    "0N64iiIdYUKcO+yq1V0cPA",
+                    "xwmnvG1KBkSNXfl7/0yJ1A"
+                }));
+                #endregion
+                */
+            }
+        }
+    }
+}
