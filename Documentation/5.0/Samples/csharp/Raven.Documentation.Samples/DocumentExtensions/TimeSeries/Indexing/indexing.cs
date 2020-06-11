7c65275201337f1acbc0797dd64b05032fd66e80
morhilai <mor@ayende.com>
2020-06-11 21:19:22 +0300

    RDoc-1703_TimeSeries indexes, WIP code samples and documentation

--- Documentation/5.0/Samples/csharp/Raven.Documentation.Samples/DocumentExtensions/TimeSeries/Indexing/indexing.cs
+++ Documentation/5.0/Samples/csharp/Raven.Documentation.Samples/DocumentExtensions/TimeSeries/Indexing/indexing.cs
@@ -17,7 +17,7 @@ namespace Raven.Documentation.Samples.DocumentExtensions.TimeSeries.Indexing
     {
         public void Examples()
         {
-            //map multimap mapreduce
+            //map multimap mapreduce TimeSeriesNamesFor
             var documentStore = new DocumentStore
             {
                 Urls = new[] { "http://localhost:8080" },
@@ -26,6 +26,7 @@ namespace Raven.Documentation.Samples.DocumentExtensions.TimeSeries.Indexing
             documentStore.Initialize();
 
             //
+            #region indexes_IndexDefinition
             documentStore.Maintenance.Send(new PutIndexesOperation(
                                            new TimeSeriesIndexDefinition
                                            {
@@ -41,10 +42,22 @@ namespace Raven.Documentation.Samples.DocumentExtensions.TimeSeries.Indexing
                                                }
                                            }
             ));
+            #endregion
+
+
+            #region indexes_IndexDefinitionBuilder
+            var TSIndexDefBuilder = new TimeSeriesIndexDefinitionBuilder<Company>("bob's index");
+            TSIndexDefBuilder.AddMap("StockPrice",
+                        timeseries => from ts in timeseries
+                                      from entry in ts.Entries
+                                      select entry.Value);
+
+
+            #endregion
         }
         //
 
-
+        #region indexes_CreationTask
         private class MyTsIndex : AbstractTimeSeriesIndexCreationTask<Company>
         {
             public MyTsIndex()
@@ -52,21 +65,23 @@ namespace Raven.Documentation.Samples.DocumentExtensions.TimeSeries.Indexing
                 AddMap(
                     "HeartRate",
                     timeSeries => from ts in timeSeries
-                                    from entry in ts.Entries
-                                    select new
-                                    {
-                                        HeartBeat = entry.Values[0],
-                                        entry.Timestamp.Date,
-                                    });
+                                  from entry in ts.Entries
+                                  select new
+                                  {
+                                      HeartBeat = entry.Values[0],
+                                      entry.Timestamp.Date,
+                                  });
             }
         }
+        #endregion
 
         //strong/query syntax multi map
+        #region indexes_MultiMapCreationTask
         private class MyMultiMapTsIndex : AbstractMultiMapTimeSeriesIndexCreationTask
         {
             public MyMultiMapTsIndex()
             {
-                AddMap<Company>(
+                AddMap<Employee>(
                     "HeartRate",
                     timeSeries => from ts in timeSeries
                                     from entry in ts.Entries
@@ -79,36 +94,41 @@ namespace Raven.Documentation.Samples.DocumentExtensions.TimeSeries.Indexing
                                     select entry.Values[0]);
             }
         }
+        #endregion
 
-        //mapreduce
-        private class AverageHeartRateDaily_ByDateAndUser : AbstractTimeSeriesIndexCreationTask<User>
+        #region indexes_MapReduce
+        private class MyMapReduceTSIndex : AbstractTimeSeriesIndexCreationTask<Company, MyMapReduceTSIndex.Result>
         {
+            public class Result
+            {
+                public double TradeVolume { get; set; }
+                public DateTime Date { get; set; }
+                public string CompanyID { get; set; }
+            }
 
-            public AverageHeartRateDaily_ByDateAndUser()
+            public MyMapReduceTSIndex()
             {
                 AddMap(
                     "HeartRate",
                     timeSeries => from ts in timeSeries
                                     from entry in ts.Entries
-                                    select new
+                                    select new Result
                                     {
-                                        HeartBeat = entry.Value,
-                                        User = ts.DocumentId,
+                                        TradeVolume = entry.Values[4],
+                                        CompanyID = ts.DocumentId,
                                         Date = entry.Timestamp,
-                                        Count = 1
                                     });
 
                 Reduce = results => from r in results
-                                    group r by new { r.Date, r.User } into g
-                                    let sumHeartBeat = g.Sum(x => x.HeartBeat)
-                                    let sumCount = g.Sum(x => x.Count)
-                                    select new
-                                    {
-                                        HeartBeat = sumHeartBeat / sumCount,
-                                        Count = sumCount
-                                    };
+                                    group r by new { r.Date, r.CompanyID } into g
+                                    select new Result();
             }
         }
+        #endregion
+    }
+
+    internal class Employee
+    {
     }
 
     internal class User
