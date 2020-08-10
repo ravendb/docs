06c96dc15ebe81c9894505a1152909e84d8355b9
reebhub <reebreeb@gmail.com>
2020-08-10 10:28:07 +0300

    RDoc-1760 Attachments bulk-insert explained

---
+++ Documentation/5.0/Raven.Documentation.Pages/document-extensions/attachments/bulk-insert.markdown
@@ -0,0 +1,60 @@
+﻿# Bulk Insert: How to Append Time Series
+
+---
+
+{NOTE: }
+
+* `store.BulkInsert` is RavenDB's high-performance data insertion operation.  
+  Use its `AttachmentsFor` interface's `Store` method to add attachments with great speed.  
+
+* In this page:  
+      * [Syntax](../../document-extensions/attachments/bulk-insert#syntax)  
+      * [Usage Flow](../../document-extensions/attachments/bulk-insert#usage-flow)  
+      * [Usage Sample](../../document-extensions/attachments/bulk-insert#usage-sample)  
+
+{NOTE/}
+
+{PANEL: Syntax}
+
+*   `AttachmentsFor`
+    {CODE AttachmentsFor-definition@DocumentExtensions\Attachments\BulkInsert.cs /}  
+
+     | Parameters | Type | Description |
+     |:-------------|:-------------|:-------------|
+     | `id` | `string` | Document ID |
+
+*   `AttachmentsFor.Store`
+    {CODE AttachmentsFor.Store-definition@DocumentExtensions\Attachments\BulkInsert.cs /}  
+
+     | Parameters | Type | Description |
+     |:-------------|:-------------|:-------------|
+     | `name` | `string` | Attachment Name |
+     | `stream` | `Stream` | Attachment Stream |
+     | `contentType` | `string` | Attachment Type (default: null)|
+
+{PANEL/}
+
+{PANEL: Usage Flow}
+
+* Create a `store.BulkInsert` instance.  
+* Pass the instance's `AttachmentsFor` -  
+   * Document ID  
+* Call `Store` as many times as you like. Pass it -  
+   * The attachment Name, Stream, and Type  
+
+{PANEL/}
+
+{PANEL: Usage Sample}
+
+* In this sample, we attach a file to all User documents.  
+   {CODE bulk-insert-attachment@DocumentExtensions\Attachments\BulkInsert.cs /}  
+
+{PANEL/}
+
+## Related articles
+
+### Attachments
+
+- [Storing](../../document-extensions/attachments/storing)
+- [Loading](../../document-extensions/attachments/loading)
+- [Deleting](../../document-extensions/attachments/deleting)
---
+++ Documentation/5.0/Samples/csharp/Raven.Documentation.Samples/DocumentExtensions/Attachments/BulkInsert.cs
@@ -0,0 +1,134 @@
+﻿using System.Linq;
+using Raven.Client.Documents;
+using Xunit;
+using Xunit.Abstractions;
+using System.Collections.Generic;
+using Raven.Client.Documents.Linq;
+using Raven.Client.ServerWide.Operations;
+using Raven.Client.ServerWide;
+using System.IO;
+using System.Text;
+
+namespace Documentation.Samples.DocumentExtensions.TimeSeries
+{
+    public class BulkInsertAttachments
+    {
+        private BulkInsertAttachments(ITestOutputHelper output)
+        {
+        }
+
+        public DocumentStore getDocumentStore()
+        {
+            DocumentStore store = new DocumentStore
+            {
+                Urls = new[] { "http://localhost:8080" },
+                Database = "TestDatabase"
+            };
+            store.Initialize();
+
+            var parameters = new DeleteDatabasesOperation.Parameters
+            {
+                DatabaseNames = new[] { "TestDatabase" },
+                HardDelete = true,
+            };
+            store.Maintenance.Server.Send(new DeleteDatabasesOperation(parameters));
+            store.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord("TestDatabase")));
+
+            return store;
+        }
+
+        [Fact]
+        public async void ReebAppendUsingBulkInsert()
+        {
+            using (var store = getDocumentStore())
+            {
+                // Create documents to bulk-insert to
+                using (var session = store.OpenSession())
+                {
+                    var user1 = new User
+                    {
+                        Name = "Lilly",
+                        Age = 20
+                    };
+                    session.Store(user1);
+
+                    var user2 = new User
+                    {
+                        Name = "Betty",
+                        Age = 25
+                    };
+                    session.Store(user2);
+
+                    var user3 = new User
+                    {
+                        Name = "Robert",
+                        Age = 29
+                    };
+                    session.Store(user3);
+
+                    session.SaveChanges();
+                }
+
+                #region bulk-insert-attachment
+                List<User> result;
+
+                // Pick users younger than 30
+                using (var session = store.OpenSession())
+                {
+                    IRavenQueryable<User> query = session.Query<User>()
+                        .Where(u => u.Age < 30);
+
+                    result = query.ToList();
+                }
+
+                // Bulk-insert an attachment to the chosen users
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    for (var user = 0; user < result.Count; user++)
+                    {
+                        byte[] byteArray = Encoding.UTF8.GetBytes("some contents here");
+                        var stream = new MemoryStream(byteArray);
+
+                        string userId = result[user].Id;
+                        
+                        // Choose the document to attach to
+                        var attachmentsFor = bulkInsert.AttachmentsFor(userId);
+
+                        // Attach the stream
+                        await attachmentsFor.StoreAsync("attName", stream);
+                    }
+                }
+                #endregion
+            }
+        }
+
+        public class User
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public string AddressId { get; set; }
+            public int Count { get; set; }
+            public int Age { get; set; }
+        }
+
+        #region AttachmentsFor-definition
+        public AttachmentsBulkInsert AttachmentsFor(string id)
+        #endregion
+        {
+            return new AttachmentsBulkInsert();
+        }
+
+        public struct AttachmentsBulkInsert
+        {
+        }
+
+        #region AttachmentsFor.Store-definition
+        public void Store(string name, Stream stream, string contentType = null)
+        #endregion
+        {
+        }
+
+    }
+}
+
