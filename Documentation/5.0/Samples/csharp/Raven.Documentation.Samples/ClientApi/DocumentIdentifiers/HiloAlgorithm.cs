4ba22a4246b853e36f205c40cb31efaf521e9b7b
morhilai <dfgsdfg>
2020-08-23 18:04:00 +0300

    RDoc-1773-custom-ID-separator Copied files from 4.x to 5.0 Fixed the language of working-with-document-identifiers.markdown in 4.2 by renaming it working-with-document-identifiers.dotnet.markdown

---
+++ Documentation/5.0/Samples/csharp/Raven.Documentation.Samples/ClientApi/DocumentIdentifiers/HiloAlgorithm.cs
@@ -0,0 +1,46 @@
+﻿using Raven.Client.Documents;
+using Raven.Documentation.Samples.Orders;
+using Raven.Documentation.Samples.Indexes.Querying;
+
+namespace Raven.Documentation.Samples.ClientApi.DocumentIdentifiers
+{
+    class HiloAlgorithm
+    {
+        public HiloAlgorithm()
+        {
+            #region return_hilo_1
+            var store = new DocumentStore();
+
+            using (var session = store.OpenSession())
+            {
+                // Store an entity will give us the hilo range (ex. 1-32)
+                session.Store(new Employee 
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                });
+
+                session.SaveChanges();
+            }
+
+            store.Dispose(); // returning unused range [last=1, max=32]
+
+            #endregion
+
+            #region return_hilo_2
+            var newStore = new DocumentStore();
+            using (var session = newStore.OpenSession())
+            {
+                // Store an entity after disposing the last store will give us  (ex. 2-33) 
+                session.Store(new Employee
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                });
+
+                session.SaveChanges();
+            }
+            #endregion
+        }
+    }
+}
