2b4cbd3e47dab4ffe596e78b1815d3a23228215c
morhilai <dfgsdfg>
2020-10-08 12:39:02 +0300

    RDoc-1807-javascript-index-functionality Attachments -JS version of `AttachmentFor` -LINQ and JS documentation of `LoadAttachment[s]`

--- Documentation/5.1/Raven.Documentation.Pages/indexes/indexing-attachments.dotnet.markdown
+++ Documentation/5.1/Raven.Documentation.Pages/indexes/indexing-attachments.dotnet.markdown
@@ -1,25 +1,82 @@
 # Indexes: Indexing Attachments
+---
 
-The `AttachmentsFor` method returns a list of [attachments](../client-api/session/attachments/what-are-attachments) in a given document as well as basic information like `Name` or `Size` about each of them.
+{NOTE: }
+
+* Attachments can be referenced and loaded using the `AttachmentsFor` and 
+`LoadAttachment`/`LoadAttachments` methods.  
+
+* Auto-indexes for attachments are not available at this time.  
+
+* In this page:  
+  * [Syntax](../../document-extensions/counters/indexing#syntax)  
+  * [Examples](../../document-extensions/counters/indexing#examples)  
+
+{NOTE/}
+
+---
+
+{PANEL: Syntax}
+
+#### Using AttachmentsFor()
+
+The `AttachmentsFor` method returns information about each attachment that extends 
+a specified document, including their names, sizes, and content type.  
 
 {CODE-TABS}
-{CODE-TAB:csharp:AttachmentsFor syntax@Indexes\IndexingAttachments.cs /}
-{CODE-TAB:csharp:AttachmentName result@Indexes\IndexingAttachments.cs /}
+{CODE-TAB:csharp:Method syntax@Indexes\IndexingAttachments.cs /}
+{CODE-TAB:csharp:Result result@Indexes\IndexingAttachments.cs /}
 {CODE-TABS/}
-
-## Creating an index using `AttachmentsFor()`
 
 The `AttachmentsFor` method is available in `AbstractIndexCreationTask`.
 
-{CODE:csharp index@Indexes\IndexingAttachments.cs /}
+#### Using LoadAttachment()/LoadAttachments()
 
-## Querying the index
+`LoadAttachment()` loads an attachment to the index by document and attachment name.  
+`LoadAttachments()` loads all the attachments of a given document.  
+
+{CODE:csharp syntax_2@Indexes\IndexingAttachments.cs /}
+
+| Parameter | Type | Description |
+| - | - | - |
+| **doc** | A server-side document, an entity | The document whose attachments you want to load |
+| **name** | `string` | The name of the attachment you want to load |
+
+{PANEL/}
+
+{PANEL: Examples}
+
+#### Indexes with `AttachmentsFor()`
+
+{CODE-TABS}
+{CODE-TAB:csharp:LINQ-syntax AttFor_index_LINQ@Indexes\IndexingAttachments.cs /}
+{CODE-TAB:csharp:JavaScript-syntax AttFor_index_JS@Indexes\IndexingAttachments.cs /}
+{CODE-TABS/}
+
+#### Indexes with `LoadAttachment()`
+
+{CODE-TABS}
+{CODE-TAB:csharp:LINQ-syntax LoadAtt_index_LINQ@Indexes\IndexingAttachments.cs /}
+{CODE-TAB:csharp:JavaScript-syntax LoadAtt_index_JS@Indexes\IndexingAttachments.cs /}
+{CODE-TABS/}
+
+#### Indexes with `LoadAttachments()`
+
+{CODE-TABS}
+{CODE-TAB:csharp:LINQ-syntax LoadAtts_index_LINQ@Indexes\IndexingAttachments.cs /}
+{CODE-TAB:csharp:JavaScript-syntax LoadAtts_index_JS@Indexes\IndexingAttachments.cs /}
+{CODE-TABS/}
+
+#### Querying the Index
 
 {CODE-TABS}
 {CODE-TAB:csharp:Sync query1@Indexes\IndexingAttachments.cs /}
 {CODE-TAB:csharp:Async query2@Indexes\IndexingAttachments.cs /}
 {CODE-TAB:csharp:DocumentQuery query3@Indexes\IndexingAttachments.cs /}
 {CODE-TABS/}
+
+{PANEL/}
+
 
 ## Related Articles
 
--- Documentation/5.1/Samples/csharp/Raven.Documentation.Samples/Indexes/IndexingAttachments.cs
+++ Documentation/5.1/Samples/csharp/Raven.Documentation.Samples/Indexes/IndexingAttachments.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
@@ -15,22 +17,25 @@ namespace Raven.Documentation.Samples.Indexes
             #region syntax
             IEnumerable<AttachmentName> AttachmentsFor(object doc);
             #endregion
+
+            #region syntax_2
+            public IAttachmentObject LoadAttachment(object doc, string name);
+            public IEnumerable<IAttachmentObject> LoadAttachments(object doc);
+            #endregion
+
         }
 
         private class Foo
         {
             #region result
             public string Name;
-
             public string Hash;
-
             public string ContentType;
-
             public long Size;
             #endregion
         }
 
-        #region index
+        #region AttFor_index_LINQ
         public class Employees_ByAttachmentNames : AbstractIndexCreationTask<Employee>
         {
             public class Result
@@ -46,6 +51,112 @@ namespace Raven.Documentation.Samples.Indexes
                                    {
                                        AttachmentNames = attachments.Select(x => x.Name).ToArray()
                                    };
+            }
+        }
+        #endregion
+
+        #region AttFor_index_JS
+        public class Employees_ByAttachmentNames_JS : AbstractJavaScriptIndexCreationTask
+        {
+            public class Result
+            {
+                public string[] AttachmentNames { get; set; }
+            }
+
+            public Employees_ByAttachmentNames_JS()
+            {
+                Maps = new HashSet<string>
+                {
+                    @"map('Employees', function (e) {
+                        var attachments = attachmentsFor(e);
+                        return {
+                            AttachmentNames: attachments.map(
+                                function(attachment) {
+                                    return attachment.Name;
+                                }
+                        };
+                    })"
+                };
+            }
+        }
+        #endregion
+
+        #region LoadAtt_index_LINQ
+        private class Companies_With_Attachments : AbstractIndexCreationTask<Company>
+        {
+
+            public Companies_With_Attachments()
+            {
+                Map = companies => from company in companies
+                                   let attachment = LoadAttachment(company, company.ExternalId)
+                                   select new
+                                   {
+                                       CompanyName = company.Name,
+                                       AttachmentName = attachment.Name,
+                                       AttachmentContentType = attachment.ContentType,
+                                       AttachmentHash = attachment.Hash,
+                                       AttachmentSize = attachment.Size,
+                                       AttachmentContent = attachment.GetContentAsString(Encoding.UTF8),
+                                   };
+            }
+        }
+        #endregion
+
+        #region LoadAtt_index_JS
+        private class Companies_With_Attachments_JavaScript : AbstractJavaScriptIndexCreationTask
+        {
+            public Companies_With_Attachments_JavaScript()
+            {
+                Maps = new HashSet<string>
+                {
+                    @"map('Companies', function (company) {
+                        var attachment = loadAttachment(company, company.ExternalId);
+                        return {
+                            CompanyName: company.Name,
+                            AttachmentName: attachment.Name,
+                            AttachmentContentType: attachment.ContentType,
+                            AttachmentHash: attachment.Hash,
+                            AttachmentSize: attachment.Size,
+                            AttachmentContent: attachment.getContentAsString('utf8')
+                        };
+                    })"
+                };
+            }
+        }
+        #endregion
+
+        #region LoadAtts_index_LINQ
+        private class Companies_With_All_Attachments : AbstractIndexCreationTask<Company>
+        {
+            public Companies_With_All_Attachments()
+            {
+                Map = companies => from company in companies
+                                   let attachments = LoadAttachments(company)
+                                   from attachment in attachments
+                                   select new
+                                   {
+                                       AttachmentName = attachment.Name,
+                                       AttachmentContent = attachment.GetContentAsString(Encoding.UTF8)
+                                   };
+            }
+        }
+        #endregion
+
+        #region LoadAtts_index_JS
+        private class Companies_With_All_Attachments_JS : AbstractJavaScriptIndexCreationTask
+        {
+            public Companies_With_All_Attachments_JS()
+            {
+                Maps = new HashSet<string>
+                {
+                    @"map('Companies', function (company) {
+                        var attachments = loadAttachments(company);
+                        return attachments.map(attachment => ({
+                            AttachmentName: attachment.Name,
+                            AttachmentContent: attachment.getContentAsString('utf8')
+                        }));
+                    })"
+                };
             }
         }
         #endregion
