98e727cd188e8f1168834941ed4a9c20883685fa
Drorlev <dror@ravendb.net>
2021-08-09 18:42:19 +0300

    RDoc-1972_Docs_5.1_js - deleting-entities port to node.js

---
+++ Documentation/5.1/Samples/nodejs/client-api/session/deletingEntities.js
@@ -0,0 +1,35 @@
+import { DocumentStore, DeleteCommandData } from "ravendb";
+
+const store = new DocumentStore();
+const session = store.openSession();
+
+let entity, id, changeVector;
+
+//region deleting_1
+session.delete(entity);
+session.delete(id, [changeVector]);
+//endregion
+
+class Employee {}
+
+async function examples() { 
+    //region deleting_2
+    const employee = await session.load("employees/1");
+
+    session.delete(employee);
+    await session.saveChanges();
+    //endregion
+
+    //region deleting_3
+    session.delete("employees/1");
+    await session.saveChanges();
+    //endregion
+
+    //region deleting_4
+    session.delete("employees/1");
+    //endregion
+
+    //region deleting_5
+    session.advanced.defer(new DeleteCommandData("employees/1", null));
+    //endregion
+}
