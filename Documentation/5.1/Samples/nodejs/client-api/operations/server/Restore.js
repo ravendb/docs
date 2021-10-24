2a1e956d0aebd22617dafb3fc2abeebaa21dd9c1
Drorlev <dror@ravendb.net>
2021-10-24 14:59:10 +0300

    RDoc-1972_Docs_5.1_js - Restore.js

--- Documentation/5.1/Samples/nodejs/client-api/operations/server/Restore.js
+++ Documentation/5.1/Samples/nodejs/client-api/operations/server/Restore.js
@@ -1,34 +1,37 @@
-import { DocumentStore } from 'ravendb';
+import {DocumentStore, RestoreBackupOperation} from 'ravendb';
+import {IServerOperation} from "ravendb";
+import {OperationIdResult} from "ravendb";
 
+let restoreConfiguration;
 //document_store_creation
 const store = new DocumentStore(["http://localhost:8080"], "Northwind2");
 store.initialize();
 //const session = store.openSession();
-
-//region restore_1
-export class RestoreBackupOperation implements IServerOperation<OperationIdResult> {
-    public constructor(restoreConfiguration: RestoreBackupConfigurationBase, nodeTag?: string)
+{
+    //region restore_1
+    const restoreBackupOperation = new RestoreBackupOperation(restoreConfiguration, "nodeTag");
     //endregion
 }
 
 //region restore_2
 export interface RestoreBackupConfigurationBase {
-    databaseName: string;
-    lastFileNameToRestore: string;
-    dataDirectory: string;
-    encryptionKey: string;
-    disableOngoingTasks: boolean;
-    skipIndexes: boolean;
-    type: RestoreType;
-    backupEncryptionSettings: BackupEncryptionSettings;
+    databaseName,
+    lastFileNameToRestore,
+    dataDirectory,
+    encryptionKey,
+    disableOngoingTasks,
+    skipIndexes,
+    type,
+    backupEncryptionSettings
 }
-    //endregion
-
-//region restore_3
-restoreConfiguration = {
-    databaseName: "Northwind";
-    skipIndexes: false;
-}
-const restoreBackupOperation = RestoreBackupOperation(restoreConfiguration)
-const restoreResult = await store.maintenance.server.send(restoreBackupOperation);
 //endregion
+{
+//region restore_3
+    restoreConfiguration = {
+        databaseName: "Northwind",
+        skipIndexes: false
+    }
+    const restoreBackupOperation = RestoreBackupOperation(restoreConfiguration, "A");
+    const restoreResult = await store.maintenance.server.send(restoreBackupOperation);
+//endregion
+}
