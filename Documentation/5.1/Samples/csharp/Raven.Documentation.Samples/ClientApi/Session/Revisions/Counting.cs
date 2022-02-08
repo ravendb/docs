a20dbdcc26c9459d65d58b522a144a9cf85fc10a
reebhub <reebreeb@gmail.com>
2022-02-08 19:37:00 +0200

    RDoc-2071_GetRevisionCount  - new "Counting" page under session\Revisions for GetCountFor  - errors fixed in the introduction to the the Document Identifier Generation article

---
+++ Documentation/5.1/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Revisions/Counting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Client;
+using Raven.Client.Documents;
+using Raven.Client.Json;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Revisions
+{
+    public class Loading
+    {
+        private interface IFoo
+        {
+            #region syntax
+            long GetCountFor(string id);
+            #endregion
+        }
+
+        public async Task Samples()
+        {
+            using (var store = new DocumentStore())
+            {
+                var CompanyProfile = new Company { Name = "Persian Rugs" };
+                using (var session = store.OpenSession())
+                {
+                    #region example_sync
+                    var RevisionsCount = session.Advanced.Revisions.GetCountFor(CompanyProfile.Id);
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region example_async
+                    var RevisionsCount = await asyncSession.Advanced.Revisions.GetCountForAsync(CompanyProfile.Id);
+                    #endregion
+                }
+            }
+        }
+    }
+}
