aed5d4dc7f05c56a7e01e5bdcd8c6c87579253f4
reebhub <reebreeb@gmail.com>
2022-03-21 17:04:46 +0200

    RDoc-2117_ManualRevisionCreation - updated for 5.3

---
+++ Documentation/5.3/Samples/csharp/Raven.Documentation.Samples/Server/Revisions.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Documents.Operations.Revisions;
+using Raven.Client.Documents.Session;
+using Raven.Client.Json;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.Server
+{
+    public class Versioning
+    {
+        private class User
+        {
+            public string Name { get; set; }
+        }
+
+        private class Loan
+        {
+            public string Id { get; set; }
+        }
+
+        public async Task Sample()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region configuration
+                await store.Maintenance.SendAsync(new ConfigureRevisionsOperation(new RevisionsConfiguration
+                {
+                    Default = new RevisionsCollectionConfiguration
+                    {
+                        Disabled = false,
+                        PurgeOnDelete = false,
+                        MinimumRevisionsToKeep = 5,
+                        MinimumRevisionAgeToKeep = TimeSpan.FromDays(14),
+                    },
+                    Collections = new Dictionary<string, RevisionsCollectionConfiguration>
+                    {
+                        {"Users", new RevisionsCollectionConfiguration {Disabled = true}},
+                        {"Orders", new RevisionsCollectionConfiguration {Disabled = false}}
+                    }
+                }));
+                #endregion
+
+                #region store
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "Ayende Rahien"
+                    });
+
+                    await session.SaveChangesAsync();
+                }
+                #endregion
+
+                Loan loan = new Loan { Id = "loans/1" };
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    #region get_revisions
+                    List<User> revisions = await session
+                        .Advanced
+                        .Revisions
+                        .GetForAsync<User>("users/1", start: 0, pageSize: 25);
+
+                    List<MetadataAsDictionary> revisionsMetadata = await session
+                        .Advanced
+                        .Revisions
+                        .GetMetadataForAsync("users/1", start: 0, pageSize: 25);
+
+                    User revison = await session
+                        .Advanced
+                        .Revisions
+                        .GetAsync<User>(revisionsMetadata[0].GetString(Constants.Documents.Metadata.ChangeVector));
+
+                    User revisonAtYearAgo = await session
+                        .Advanced
+                        .Revisions
+                        .GetAsync<User>("users/1", DateTime.Now.AddYears(-1));
+                    #endregion
+                }
+            }
+        }
+
+        public async Task ForceRevisionCreationForSample()
+        {
+            using (var store = new DocumentStore())
+            {
+                string companyId;
+                using (var session = store.OpenSession())
+                {
+                    #region ForceRevisionCreationByEntity
+                    var company = new Company { 
+                            Name = "CompanyProfile" };
+                    session.Store(company);
+                    companyId = company.Id;
+                    session.SaveChanges();
+
+                    // Forcing the creation of a revision by entity becomes possible 
+                    // only after the document is saved and becomes a tracked entity
+                    session.Advanced.Revisions.ForceRevisionCreationFor<Company>(company);
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region ForceRevisionCreationByID
+                    session.Advanced.Revisions.ForceRevisionCreationFor(companyId);
+                    session.SaveChanges();
+                    #endregion
+
+                    var revisionsCount = session.Advanced.Revisions.GetFor<Company>(companyId).Count;
+                }
+            }
+        }
+
+        public class Company
+        {
+            public string Id { get; set; }
+            public string ExternalId { get; set; }
+            public string Name { get; set; }
+            public Contact Contact { get; set; }
+            public Address Address { get; set; }
+            public string Phone { get; set; }
+            public string Fax { get; set; }
+        }
+
+    }
+}
