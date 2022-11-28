5f29414a8bc8ce0bbbf8c732a90dc43a5d0b9906
danielle9897 <danielle.9897@gmail.com>
2022-11-28 10:28:25 +0200

    RDoc-2317 [Node.js] Client API > Session > How to > Defer operations (C#)

---
+++ Documentation/5.2/Raven.Documentation.Pages/client-api/session/how-to/defer-operations.java.markdown
@@ -0,0 +1,37 @@
+# Session: How to Defer Commands
+
+Commands can be deferred till `saveChanges` is called by using `defer` method in `advanced` session operations.  
+All of the operations will update teh session state appropriately after `saveChanges` is called.
+
+Types of commands that can be deferred:
+
+- [PutCommandData](../../../glossary/put-command-data)
+- [DeleteCommandData](../../../glossary/delete-command-data)
+- DeletePrefixedCommandData
+- [PatchCommandData](../../../glossary/patch-command-data)
+- PutAttachmentCommandData
+- DeleteAttachmentCommandData
+- [CopyAttachmentCommandData](../../../glossary/copy-attachment-command-data)
+- [MoveAttachmentCommandData](../../../glossary/move-attachment-command-data)
+- [CountersBatchCommandData](../../../glossary/counters-batch-command-data)
+
+## Syntax
+
+{CODE:java defer_1@ClientApi\Session\HowTo\Defer.java /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| `ICommandData` | Command to be executed. |
+| `ICommandData[]` | Array of commands implementing `ICommandData` interface. |
+
+## Example
+
+{CODE:java defer_2@ClientApi\Session\HowTo\Defer.java /}
+
+## Related articles
+
+### Session
+
+- [What is a Session and How Does it Work](../../../client-api/session/what-is-a-session-and-how-does-it-work)
+- [Storing Entities](../../../client-api/session/storing-entities)
+- [Saving Changes](../../../client-api/session/saving-changes)
---
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/HowTo/Defer.cs
@@ -0,0 +1,65 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Commands.Batches;
+using Raven.Client.Documents.Operations;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.HowTo
+{
+    public class Defer
+    {
+        public Defer()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region defer_1
+
+                    // Defer is available in the session's Advanced methods 
+                    session.Advanced.Defer(
+                        
+                        // Define commands to be executed:
+                        // i.e. Put a new document
+                        new PutCommandData("products/999-A", null, new DynamicJsonValue
+                            {
+                                ["Name"] = "My Product",
+                                ["Supplier"] = "suppliers/1-A",
+                                ["@metadata"] = new DynamicJsonValue
+                                {
+                                    ["@collection"] = "Products"
+                                }
+                            }),
+                        
+                        // Patch document
+                        new PatchCommandData("products/999-A", null, new PatchRequest
+                                {
+                                    Script = $@"this.Supplier = 'suppliers/2-A';"
+                                },
+                                null),
+                        
+                        // Force a revision to be created
+                        new ForceRevisionCommandData("products/999-A"),
+                        
+                        // Delete a document
+                        new DeleteCommandData("products/1-A", null)
+                    );
+                    
+                    // All deferred commands will be sent to the server upon calling SaveChanges
+                    session.SaveChanges();
+
+                    #endregion
+                }
+            }
+        }
+
+        private interface IFoo
+        {
+            #region syntax
+
+            void Defer(ICommandData command, params ICommandData[] commands);
+            void Defer(ICommandData[] commands);
+
+            #endregion
+        }
+    }
+}
