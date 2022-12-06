4a224b031b02e618ec9b018c4c51308c97cba9f9
danielle9897 <danielle.9897@gmail.com>
2022-12-06 16:23:10 +0200

    RDoc-2326  Client API > Operations > Server wide > Add database node (Node.js)

--- Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/AddDatabaseNode.cs
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/AddDatabaseNode.cs
@@ -17,24 +17,30 @@ namespace Raven.Documentation.Samples.ClientApi.Operations.Server
 
         public AddDatabaseNode()
         {
-
             using (var store = new DocumentStore())
             {
                 #region add_1
-                // Add random node to 'Northwind' database-group
-                DatabasePutResult result = store.Maintenance.Server.Send(new AddDatabaseNodeOperation("Northwind"));
+                // Create AddDatabaseNodeOperation
+                // Add a random node to 'Northwind' database-group
+                var addDatabaseNodeOperation = new AddDatabaseNodeOperation("Northwind");
+                
+                // Send operation to the store
+                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOperation);
                 
                 // Can access the new topology
                 var numberOfReplicas = result.Topology.AllNodes.Count();
-
                 #endregion
             }
             
             using (var store = new DocumentStore())
             {
                 #region add_2
+                // Create AddDatabaseNodeOperation
                 // Add node C to 'Northwind' database-group
-                DatabasePutResult result = store.Maintenance.Server.Send(new AddDatabaseNodeOperation("Northwind", "C"));
+                var addDatabaseNodeOperation = new AddDatabaseNodeOperation("Northwind", "C");
+                
+                // Send operation to the store
+                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOperation);
                 #endregion
             }
         }
