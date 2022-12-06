d3b72f1ad1a859e7bbb84030bb6ece30d18bd0cf
danielle9897 <danielle.9897@gmail.com>
2022-12-06 12:44:29 +0200

    RDoc-2326  Client API > Operations > Server wide > Add database node (C#)

---
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/AddDatabaseNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide.Operations;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Server
+{
+    public class AddDatabaseNode
+    {
+        private interface IFoo
+        {
+            /*
+            #region syntax
+            public AddDatabaseNodeOperation(string databaseName, string nodeTag = null)
+            #endregion
+            */
+        }
+
+        public AddDatabaseNode()
+        {
+
+            using (var store = new DocumentStore())
+            {
+                #region add_1
+                // Add random node to 'Northwind' database-group
+                DatabasePutResult result = store.Maintenance.Server.Send(new AddDatabaseNodeOperation("Northwind"));
+                
+                // Can access the new topology
+                var numberOfReplicas = result.Topology.AllNodes.Count();
+
+                #endregion
+            }
+            
+            using (var store = new DocumentStore())
+            {
+                #region add_2
+                // Add node C to 'Northwind' database-group
+                DatabasePutResult result = store.Maintenance.Server.Send(new AddDatabaseNodeOperation("Northwind", "C"));
+                #endregion
+            }
+        }
+    }
+}
