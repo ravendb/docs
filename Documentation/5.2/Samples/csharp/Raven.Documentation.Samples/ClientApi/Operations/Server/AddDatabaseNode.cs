f7793f90dbf44c511408592b44342279e3d2461d
danielle9897 <danielle.9897@gmail.com>
2023-01-01 19:19:55 +0200

    RDoc-2349 Add database node: Add async examples + small text fixes

--- Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/AddDatabaseNode.cs
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Server/AddDatabaseNode.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.ServerWide.Operations;
 
@@ -20,12 +21,12 @@ namespace Raven.Documentation.Samples.ClientApi.Operations.Server
             using (var store = new DocumentStore())
             {
                 #region add_1
-                // Create AddDatabaseNodeOperation
+                // Create the AddDatabaseNodeOperation
                 // Add a random node to 'Northwind' database-group
-                var addDatabaseNodeOperation = new AddDatabaseNodeOperation("Northwind");
+                var addDatabaseNodeOp = new AddDatabaseNodeOperation("Northwind");
                 
-                // Send operation to the store
-                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOperation);
+                // Execute the operation by passing it to Maintenance.Server.Send
+                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOp);
                 
                 // Can access the new topology
                 var numberOfReplicas = result.Topology.AllNodes.Count();
@@ -35,12 +36,42 @@ namespace Raven.Documentation.Samples.ClientApi.Operations.Server
             using (var store = new DocumentStore())
             {
                 #region add_2
-                // Create AddDatabaseNodeOperation
+                // Create the AddDatabaseNodeOperation
                 // Add node C to 'Northwind' database-group
-                var addDatabaseNodeOperation = new AddDatabaseNodeOperation("Northwind", "C");
+                var addDatabaseNodeOp = new AddDatabaseNodeOperation("Northwind", "C");
                 
-                // Send operation to the store
-                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOperation);
+                // Execute the operation by passing it to Maintenance.Server.Send
+                DatabasePutResult result = store.Maintenance.Server.Send(addDatabaseNodeOp);
+                #endregion
+            }
+        }
+        
+        public async Task AddDatabaseNodeAsync()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region add_1_async
+                // Create the AddDatabaseNodeOperation
+                // Add a random node to 'Northwind' database-group
+                var addDatabaseNodeOp = new AddDatabaseNodeOperation("Northwind");
+                
+                // Execute the operation by passing it to Maintenance.Server.SendAsync
+                DatabasePutResult result = await store.Maintenance.Server.SendAsync(addDatabaseNodeOp);
+                
+                // Can access the new topology
+                var numberOfReplicas = result.Topology.AllNodes.Count();
+                #endregion
+            }
+            
+            using (var store = new DocumentStore())
+            {
+                #region add_2_async
+                // Create the AddDatabaseNodeOperation
+                // Add node C to 'Northwind' database-group
+                var addDatabaseNodeOp = new AddDatabaseNodeOperation("Northwind", "C");
+                
+                // Execute the operation by passing it to Maintenance.Server.SendAsync
+                DatabasePutResult result = await store.Maintenance.Server.SendAsync(addDatabaseNodeOp);
                 #endregion
             }
         }
