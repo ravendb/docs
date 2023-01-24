b0f5348a0ca02ac45f02bf69dede6e14f16252c0
danielle9897 <danielle.9897@gmail.com>
2023-01-24 13:21:06 +0200

    RDoc-2369 [Node.js] Client API > Operations > Maintenance > Indexes > Get terms

---
+++ Documentation/5.2/Samples/nodejs/ClientApi/Operations/Maintenance/Indexes/getIndexTerms.js
@@ -0,0 +1,26 @@
+﻿import { DocumentStore } from "ravendb";
+
+const documentStore = new DocumentStore();
+
+async function getIndexTerms() {
+    {
+        //region get_index_terms
+        // Define the get terms operation
+        // Pass the requested index-name, index-field, start value & page size
+        const getTermsOp = new GetTermsOperation("Orders/Totals", "Employee", "employees/5-a", 10);
+
+        // Execute the operation by passing it to maintenance.send
+        const fieldTerms = await store.maintenance.send(getTermsOp);
+
+        // fieldTerms will contain the all terms that come after term 'employees/5-a' for index-field 'Employee'
+        //endregion
+    }
+}
+
+{
+    //region get_index_terms_syntax
+    // Available overloads:
+    const getTermsOp = new GetTermsOperation(indexName, field, fromValue);
+    const getTermsOp = new GetTermsOperation(indexName, field, fromValue, pageSize);
+    //endregion
+}
