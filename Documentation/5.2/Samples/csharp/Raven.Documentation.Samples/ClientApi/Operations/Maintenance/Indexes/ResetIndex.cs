861eff9802ae6cbb95ad2bb55b66237d15a56e3f
danielle9897 <danielle.9897@gmail.com>
2023-02-01 14:57:42 +0200

    RDoc-2376 [Node.js] Client API > Operations > Maintenance > Indexes > Reset index

---
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Operations/Maintenance/Indexes/ResetIndex.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations.Indexes;
+
+namespace Raven.Documentation.Samples.ClientApi.Operations.Maintenance.Indexes
+{
+    public class ResetIndex
+    {
+        public ResetIndex()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region reset
+                // Define the reset index operation, pass index name
+                var resetIndexOp = new ResetIndexOperation("Orders/Totals");
+
+                // Execute the operation by passing it to Maintenance.Send
+                // An exception will be thrown if index does not exist
+                store.Maintenance.Send(resetIndexOp);
+                #endregion
+            }
+        }
+
+        public async Task ResetIndexAsync()
+        {
+            using (var store = new DocumentStore())
+            {
+                #region reset_async
+                // Define the reset index operation, pass index name
+                var resetIndexOp = new ResetIndexOperation("Orders/Totals");
+
+                // Execute the operation by passing it to Maintenance.SendAsync
+                // An exception will be thrown if index does not exist
+                await store.Maintenance.SendAsync(resetIndexOp);
+                #endregion
+            }
+        }
+        
+        private interface IFoo
+        {
+            /*
+            #region syntax
+            public ResetIndexOperation(string indexName);
+            #endregion
+            */
+        }
+    }
+}
---
+++ Documentation/5.2/Samples/nodejs/ClientApi/Operations/Maintenance/Indexes/resetIndex.js
@@ -0,0 +1,22 @@
+﻿import { DocumentStore } from "ravendb";
+
+const documentStore = new DocumentStore();
+
+async function getStats() {
+    {
+        //region reset
+        // Define the reset index operation, pass index name
+        const resetIndexOp = new ResetIndexOperation("Orders/Totals");
+
+        // Execute the operation by passing it to maintenance.send
+        // An exception will be thrown if index does not exist
+        await store.maintenance.send(resetIndexOp);
+        //endregion
+    }
+}
+
+{
+    //region syntax
+    const resetIndexOp = new ResetIndexOperation(indexName);
+    //endregion
+}
