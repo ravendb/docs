99db52e094dc9fed1017dcf0a07558b191c10b65
reebhub <reebreeb@gmail.com>
2023-07-03 03:06:13 +0300

    RDoc-2464_LongCountAsync - document CountAsync, LongCount and LongCountAsync

---
+++ Documentation/5.2/Raven.Documentation.Pages/client-api/session/querying/how-to-count-query-results.markdown
@@ -0,0 +1,54 @@
+﻿# Count Query Results  
+
+---
+
+{NOTE: }
+
+* Count query results using one of the methods described below.  
+
+* In This Page:  
+    * [CountAsync](../../../client-api/session/querying/how-to-count-query-results#countasync)  
+    * [LongCount and LongCountAsync](../../../client-api/session/querying/how-to-count-query-results#longcount-and-longcountasync)  
+
+{NOTE/}
+
+---
+
+{PANEL: CountAsync}
+
+To count query results where an `Int32` variable is sufficient, use the `CountAsync` method.  
+{CODE CountAsync@ClientApi\Session\Querying\HowToCountResults.cs /}
+
+{PANEL/}
+
+{PANEL: LongCount and LongCountAsync}
+
+To count query results where an `Int64` variable is needed, use the 
+`LongCount` or `LongCountAsync` method.  
+
+{CODE-TABS}
+{CODE-TAB:csharp:LongCount LongCount@ClientApi\Session\Querying\HowToCountResults.cs /}
+{CODE-TAB:csharp:LongCount_async LongCount_async@ClientApi\Session\Querying\HowToCountResults.cs /}
+{CODE-TABS/}
+
+{PANEL/}
+
+## Related Articles
+
+### Client API
+
+- [What is a Document Query](../../../client-api/session/querying/document-query/what-is-document-query)
+- [How to Filter by Field Presence](../../../client-api/session/querying/how-to-filter-by-field)
+
+### Querying
+
+- [Querying: Filtering](../../../indexes/querying/filtering)
+- [Query vs DocumentQuery](../../../client-api/session/querying/document-query/query-vs-document-query)
+- [RQL - Raven Query Language](../../../client-api/session/querying/what-is-rql)
+
+---
+
+### Code Walkthrough
+
+- [Queries - Filtering Results - Basics](https://demo.ravendb.net/demos/csharp/queries/filtering-results-basics)
+- [Queries - Filtering with Multiple Conditions](https://demo.ravendb.net/demos/csharp/queries/filtering-results-multiple-conditions)
---
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Querying/HowToCountResults.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Orders;
+using static NodaTime.TimeZones.ZoneEqualityComparer;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Querying
+{
+    public class CountQueryResults
+    {
+        public async Task CanUseCountAsync(Options options)
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var s = store.OpenAsyncSession())
+                {
+                    await s.StoreAsync(new User
+                    {
+                        Name = "John"
+                    });
+
+                    await s.SaveChangesAsync();
+                }
+
+                using (var s = store.OpenAsyncSession())
+                {
+                    QueryStatistics stats;
+
+                    #region CountAsync
+                    System.Int32 count = await
+                        s.Query<User>()
+                            .Statistics(out stats)
+                            .Search(u => u.Name, "John")
+                            .CountAsync();
+                    #endregion
+                }
+            }
+        }
+
+        public void CanUseLongCount(Options options)
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var s = store.OpenSession())
+                {
+                    s.Store(new User
+                    {
+                        Name = "John"
+                    });
+
+                    s.SaveChanges();
+                }
+
+                using (var s = store.OpenSession())
+                {
+                    QueryStatistics stats;
+
+                    #region LongCount
+                    // Use LongCount in a synchronous session
+                    System.Int64 longCount = s.Query<User>()
+                        .Statistics(out stats)
+                        .Search(u => u.Name, "John")
+                        .LongCount();
+                    #endregion
+                }
+            }
+        }
+
+        public async Task CanUseLongCountAsync(Options options)
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var s = store.OpenAsyncSession())
+                {
+                    await s.StoreAsync(new User
+                    {
+                        Name = "John"
+                    });
+
+                    await s.SaveChangesAsync();
+                }
+
+                using (var s = store.OpenAsyncSession())
+                {
+                    QueryStatistics stats;
+
+                    #region LongCount_async
+                    // Use LongCountAsync in an Async session
+                    System.Int64 longCount = await
+                        s.Query<User>()
+                            .Statistics(out stats)
+                            .Search(u => u.Name, "John")
+                            .LongCountAsync();
+                    #endregion
+                }
+            }
+        }
+
+
+    }
+}
