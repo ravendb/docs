33a519e403357e5eb9ce5724203ed900059f4380
danielle9897 <danielle.9897@gmail.com>
2023-07-10 13:10:43 +0300

    RDoc-2474 Client API > Session > Querying > Text Search - Part 3

---
+++ Documentation/5.2/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/Querying/TextSearch/UsingRegex.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.Querying.TextSearch
+{
+    public class UsingRegex
+    {
+        private class User
+        {
+            public string FirstName { get; set; }
+        }
+
+        public async Task Examples()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region regex_1
+                    // loads all products, which name
+                    // starts with 'N' or 'A'
+                    List<Product> products = session
+                        .Query<Product>()
+                        .Where(x => Regex.IsMatch(x.Name, "^[NA]"))
+                        .ToList();
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region regex_1_async
+                    // loads all products, which name
+                    // starts with 'N' or 'A'
+                    List<Product> products = await asyncSession
+                        .Query<Product>()
+                        .Where(x => Regex.IsMatch(x.Name, "^[NA]"))
+                        .ToListAsync();
+                    #endregion
+                }
+            }
+        }
+    }
+}
