674802c09de1206797838d2afce297c9639c4d34
reebhub <reebreeb@gmail.com>
2024-02-29 05:05:03 +0200

    RDoc-2619 - markdown and sample files copied to version 5.4

---
+++ Documentation/5.4/Raven.Documentation.Pages/client-api/session/loading-entities.js.markdown
@@ -0,0 +1,244 @@
+# Session: Loading Entities
+---
+
+{NOTE: }
+
+* There are several methods that allow users to load documents from the database and convert them to entities.
+
+* This article covers the following methods:
+
+  - [Load](../../client-api/session/loading-entities#load)
+  - [Load with Includes](../../client-api/session/loading-entities#load-with-includes)
+  - [Load - multiple entities](../../client-api/session/loading-entities#load---multiple-entities)
+  - [LoadStartingWith](../../client-api/session/loading-entities#loadstartingwith)
+  - [ConditionalLoad](../../client-api/session/loading-entities#conditionalload)
+  - [IsLoaded](../../client-api/session/loading-entities#isloaded)
+  - [Stream](../../client-api/session/loading-entities#stream)
+
+* For loading entities lazily see [perform requests lazily](../../client-api/session/how-to/perform-operations-lazily).
+
+{NOTE/}
+
+---
+
+{PANEL:Load}
+
+The most basic way to load a single entity is to use session's `load()` method.
+
+{CODE:nodejs loading_entities_1_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **id** | string | Identifier of a document that will be loaded. |
+| **documentType** | function | A class constructor used for reviving the results' entities |
+
+| Return Value | |
+| ------------- | ----- |
+| `Promise<object>` | A `Promise` returning `object` or `null` if a document with a given ID does not exist. |
+
+### Example
+
+{CODE:nodejs loading_entities_1_1@client-api\Session\loadingEntities.js /}
+
+{NOTE In 4.x RavenDB, only string identifiers are supported. If you are upgrading from 3.x, this is a major change, because in 3.x non-string identifiers are supported. /}
+
+{PANEL/}
+
+{PANEL:Load with Includes}
+
+When there is a *relationship* between documents, those documents can be loaded in a single request call using the `include()` and `load()` methods.
+
+{CODE:nodejs loading_entities_2_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **path** | string | Field path in documents in which the server should look for 'referenced' documents. |
+
+| Return Value | |
+| ------------- | ----- |
+| `object{load()}` | The `include()` method by itself does not materialize any requests but returns loader containing methods such as `load()`. |
+
+### Example I
+
+We can use this code to also load an employee which made the order.
+
+{CODE:nodejs loading_entities_2_1@client-api\Session\loadingEntities.js /}
+
+{PANEL/}
+
+{PANEL:Load - multiple entities}
+
+To load multiple entities at once, use one of the following ways to call `load()`.
+
+{CODE:nodejs loading_entities_3_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **idsArray** | string[] | Multiple document identifiers to load |
+| **documentType** | function | A class constructor used for reviving the results' entities |
+| **options** | string | Options with the following properties |
+| **documentType** | function | A class construcor used for reviving the results' entities |
+| **includes** | string[] | Field paths in documents in which the server should look for 'referenced' documents. |
+
+| Return Value | |
+| ------------- | ----- |
+| `Promise<{ [id]: object }>` | A `Promise` resolving to an object mapping document identifiers to `object` or `null` if a document with given ID doesn't exist |
+
+{CODE:nodejs loading_entities_3_1@client-api\Session\loadingEntities.js /}
+
+{PANEL/}
+
+{PANEL:LoadStartingWith}
+
+To load multiple entities that contain a common prefix, use the `loadStartingWith()` method from the `advanced` session operations.
+
+{CODE:nodejs loading_entities_4_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **idPrefix** | string | prefix for which the documents should be returned  |
+| **options** | string | Options with the following properties |
+| **matches** | string | pipe ('&#124;') separated values for which document IDs (after 'idPrefix') should be matched ('?' any single character, '*' any characters) |
+| **start** | number | number of documents that should be skipped  |
+| **pageSize** | number | maximum number of documents that will be retrieved |
+| **exclude** | string | pipe ('&#124;') separated values for which document IDs (after 'idPrefix') should **not** be matched ('?' any single character, '*' any characters) |
+| **skipAfter** | string | skip document fetching until given ID is found and return documents after that ID (default: `null`) |
+| **documentType** | function | A class constructor used for reviving the results' entities |
+
+| Return Value | |
+| ------------- | ----- |
+| `Promise<object[]>` | A `Promise` resolving to an array of entities matching given parameters |
+
+### Example I
+
+{CODE:nodejs loading_entities_4_1@client-api\Session\loadingEntities.js /}
+
+### Example II
+
+{CODE:nodejs loading_entities_4_2@client-api\Session\loadingEntities.js /}
+
+{PANEL/}
+
+{PANEL: ConditionalLoad}
+
+This method can be used to check whether a document has been modified 
+since the last time its change vector was recorded, so that the cost of loading it 
+can be saved if it has not been modified.  
+
+The `conditionalLoad` method takes a document's [change vector](../../server/clustering/replication/change-vector). 
+If the entity is tracked by the session, this method returns the entity. If the entity 
+is not tracked, it checks if the provided change vector matches the document's 
+current change vector on the server side. If they match, the entity is not loaded. 
+If the change vectors _do not_ match, the document is loaded.  
+
+The method is accessible from the `session.Advanced` operations.  
+{CODE:nodejs loading_entities_7_0@client-api\Session\loadingEntities.js /}
+
+| Parameter | Type | Description |
+| ------------- | ------------- | ----- |
+| **id** | `string` | The identifier of a document to be loaded. |
+| **changeVector** | `string` | The change vector you want to compare with the server-side change vector. If the change vectors match, the document is not loaded. |
+
+| Return Type | Description |
+| ------------- | ----- |
+| ValueTuple `(object, changeVector)` | If the given change vector and the server side change vector do not match, the method returns the requested entity and its current change vector.<br/>If the change vectors match, the method returns `default` as the entity, and the current change vector.<br/>If the specified document, the method returns only `default` without a change vector. |
+
+### Example
+
+{CODE:nodejs loading_entities_7_1@client-api\Session\loadingEntities.js /}
+
+{PANEL/}
+
+{PANEL:Stream}
+
+Entities can be streamed from the server using the `stream()` method from the `advanced` session operations.
+
+{CODE:nodejs loading_entities_5_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **idPrefix** | string | prefix for which the documents should be returned  |
+| **query** | query object | a query obtained from a call to `session.query()` or `session.advanced.rawQuery()` |
+| **options** | string | Options with the following properties |
+| **startsWith** | string | prefix for which documents should be streamed |
+| **matches** | string | pipe ('&#124;') separated values for which document IDs should be matched ('?' any single character, '*' any characters) |
+| **start** | number | number of documents that should be skipped  |
+| **pageSize** | number | maximum number of documents that will be retrieved |
+| **skipAfter** | string | skip document fetching until a given ID is found and returns documents after that ID (default: `null`) |
+| **documentType** | function | A class constructor used for reviving the results' entities |
+| **statsCallback** | function | callback returning information about the streaming query (amount of results, which index was queried, etc.) |
+
+
+| Return Value | |
+| ------------- | ----- |
+| `Promise<Readable>` | A `Promise` resolving to readable stream with query results |
+
+
+### Example I
+
+Stream documents for a ID prefix:
+
+{CODE:nodejs loading_entities_5_1@client-api\Session\loadingEntities.js /}
+
+### Example 2
+
+Fetch documents for a ID prefix directly into a writable stream:
+
+{CODE:nodejs loading_entities_5_2@client-api\Session\loadingEntities.js /}
+
+{INFO Entities loaded using `stream()` will be transient (not attached to session). /}
+
+{PANEL/}
+
+{PANEL:IsLoaded}
+
+To check if an entity is attached to a session, e.g. it has been loaded previously, use the `isLoaded` method from the `advanced` session operations.  
+  
+If you try to load a document that does not exist with the `load` method, `isLoaded` will return `true` because that document load has already been attempted.  
+
+{CODE:nodejs loading_entities_6_0@client-api\Session\loadingEntities.js /}
+
+| Parameters | | |
+| ------------- | ------------- | ----- |
+| **id** | string | Entity ID for which the check should be performed. |
+
+| Return Value | |
+| ------------- | ----- |
+| boolean | Indicates if an entity with a given ID is loaded. |
+
+### Example
+
+{CODE:nodejs loading_entities_6_1@client-api\Session\loadingEntities.js /}
+
+{PANEL/}
+
+### On entities loading, JS classes and the **documentType** parameter
+
+Type information about the entity and its contents is by default stored in the document metadata. Based on that its types are revived when loaded from the server.
+
+{INFO: Entity type registration }
+In order to avoid passing **documentType** argument every time, you can register the type in the document conventions using the `registerEntityType()` method before calling DocumentStore's `initialize()` like so:
+
+{CODE:nodejs loading_entities_8@client-api\Session\loadingEntities.js /}
+
+{INFO/}
+
+If you fail to do so, entities (and all subobjects) loaded from the server are going to be plain object literals and not instances of the original type they were stored with.
+
+## Related Articles
+
+### Session
+
+- [What is a Session and How Does it Work](../../client-api/session/what-is-a-session-and-how-does-it-work) 
+- [Opening a Session](../../client-api/session/opening-a-session)
+- [Deleting Entities](../../client-api/session/deleting-entities)
+- [Saving Changes](../../client-api/session/saving-changes)
+
+### Querying
+
+- [Query Overview](../../client-api/session/querying/how-to-query)
+- [Querying an Index](../../indexes/querying/query-index)
+
+### Document Store
+
+- [What is a Document Store](../../client-api/what-is-a-document-store)
---
+++ Documentation/5.4/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/LoadingEntities.cs
@@ -0,0 +1,441 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Session;
+using Raven.Client.Documents.Session.Loaders;
+using Raven.Client.Util;
+using Raven.Documentation.Samples.Orders;
+
+namespace Raven.Documentation.Samples.ClientApi.Session
+{
+    using System.Linq;
+
+    public class LoadingEntities
+    {
+        private interface IFoo
+        {
+            #region loading_entities_1_0
+            TResult Load<TResult>(string id);
+            #endregion
+
+            #region loading_entities_1_0_async
+            Task<TResult> LoadAsync<TResult>(string id);
+            #endregion
+
+            #region loading_entities_2_0
+            ILoaderWithInclude<object> Include(string path);
+
+            ILoaderWithInclude<T> Include<T>(Expression<Func<T, string>> path);
+
+            ILoaderWithInclude<T> Include<T>(Expression<Func<T, IEnumerable<string>>> path);
+
+            ILoaderWithInclude<T> Include<T, TInclude>(Expression<Func<T, string>> path);
+
+            ILoaderWithInclude<T> Include<T, TInclude>(Expression<Func<T, IEnumerable<string>>> path);
+            #endregion
+
+            #region loading_entities_3_0
+            Dictionary<string, TResult> Load<TResult>(IEnumerable<string> ids);
+            #endregion
+
+            #region loading_entities_3_0_async
+            Task<Dictionary<string, TResult>> LoadAsync<TResult>(IEnumerable<string> ids);
+            #endregion
+
+            #region loading_entities_4_0
+            T[] LoadStartingWith<T>(
+                string idPrefix,
+                string matches = null,
+                int start = 0,
+                int pageSize = 25,
+                string exclude = null,
+                string startAfter = null);
+
+            void LoadStartingWithIntoStream(
+                string idPrefix,
+                Stream output,
+                string matches = null,
+                int start = 0,
+                int pageSize = 25,
+                string exclude = null,
+                string startAfter = null);
+            #endregion
+
+            #region loading_entities_4_0_async
+            Task<T[]> LoadStartingWithAsync<T>(
+                string idPrefix,
+                string matches = null,
+                int start = 0,
+                int pageSize = 25,
+                string exclude = null,
+                string startAfter = null);
+
+            Task LoadStartingWithIntoStreamAsync(
+                string idPrefix,
+                Stream output,
+                string matches = null,
+                int start = 0,
+                int pageSize = 25,
+                string exclude = null,
+                string startAfter = null);
+            #endregion
+
+            #region loading_entities_5_0
+            IEnumerator<StreamResult<T>> Stream<T>(IQueryable<T> query);
+
+            IEnumerator<StreamResult<T>> Stream<T>(IQueryable<T> query, out StreamQueryStatistics streamQueryStats);
+
+            IEnumerator<StreamResult<T>> Stream<T>(IDocumentQuery<T> query);
+
+            IEnumerator<StreamResult<T>> Stream<T>(IRawDocumentQuery<T> query);
+
+            IEnumerator<StreamResult<T>> Stream<T>(IRawDocumentQuery<T> query, out StreamQueryStatistics streamQueryStats);
+
+            IEnumerator<StreamResult<T>> Stream<T>(IDocumentQuery<T> query, out StreamQueryStatistics streamQueryStats);
+
+            IEnumerator<StreamResult<T>> Stream<T>(string startsWith, string matches = null, int start = 0, int pageSize = int.MaxValue, string startAfter = null);
+            #endregion
+
+            #region loading_entities_5_0_async
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IQueryable<T> query);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IQueryable<T> query, out StreamQueryStatistics streamQueryStats);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IDocumentQuery<T> query);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IRawDocumentQuery<T> query);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IRawDocumentQuery<T> query, out StreamQueryStatistics streamQueryStats);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(IDocumentQuery<T> query, out StreamQueryStatistics streamQueryStats);
+
+            Task<IAsyncEnumerator<StreamResult<T>>> StreamAsync<T>(string startsWith, string matches = null, int start = 0, int pageSize = int.MaxValue, string startAfter = null);
+            #endregion
+
+            #region loading_entities_6_0
+            bool IsLoaded(string id);
+            #endregion
+
+            #region loading_entities_7_0
+            (T Entity, string ChangeVector) ConditionalLoad<T>(string id, string changeVector);
+            #endregion
+
+            #region loading_entities_7_0_async
+            Task<(T Entity, string ChangeVector)> ConditionalLoadAsync<T>(string id, string changeVector);
+            #endregion
+        }
+
+        public async Task LoadingEntitiesXY()
+        {
+            using (var store = new DocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_1_1
+
+                    Employee employee = session.Load<Employee>("employees/1");
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_1_1_async
+
+                    Employee employee = await asyncSession.LoadAsync<Employee>("employees/1");
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_2_1
+
+                    // loading 'products/1'
+                    // including document found in 'Supplier' property
+                    Product product = session
+                        .Include("Supplier")
+                        .Load<Product>("products/1");
+
+                    Supplier supplier = session.Load<Supplier>(product.Supplier); // this will not make server call
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_2_1_async
+
+                    // loading 'products/1'
+                    // including document found in 'Supplier' property
+                    Product product = await asyncSession
+                        .Include("Supplier")
+                        .LoadAsync<Product>("products/1");
+
+                    Supplier supplier = await asyncSession.LoadAsync<Supplier>(product.Supplier); // this will not make server call
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_2_2
+
+                    // loading 'products/1'
+                    // including document found in 'Supplier' property
+                    Product product = session
+                        .Include<Product>(x => x.Supplier)
+                        .Load<Product>("products/1");
+
+                    Supplier supplier = session.Load<Supplier>(product.Supplier); // this will not make server call
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_2_2_async
+
+                    // loading 'products/1'
+                    // including document found in 'Supplier' property
+                    Product product = await asyncSession
+                        .Include<Product>(x => x.Supplier)
+                        .LoadAsync<Product>("products/1");
+
+                    Supplier supplier = await asyncSession.LoadAsync<Supplier>(product.Supplier); // this will not make server call
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_3_1
+
+                    Dictionary<string, Employee> employees = session.Load<Employee>(new[]
+                    {
+                        "employees/1",
+                        "employees/2",
+                        "employees/3"
+                    });
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_3_1_async
+
+                    Dictionary<string, Employee> employees = await asyncSession.LoadAsync<Employee>(new[]
+                    {
+                        "employees/1",
+                        "employees/2",
+                    });
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_4_1
+                    // return up to 128 entities with Id that starts with 'employees'
+                    Employee[] result = session
+                        .Advanced
+                        .LoadStartingWith<Employee>("employees", null, 0, 128);
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_4_1_async
+                    // return up to 128 entities with Id that starts with 'employees'
+                    Employee[] result = (await asyncSession
+                        .Advanced
+                        .LoadStartingWithAsync<Employee>("employees", null, 0, 128))
+                        .ToArray();
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_4_2
+                    // return up to 128 entities with Id that starts with 'employees/' 
+                    // and rest of the key begins with "1" or "2" e.g. employees/10, employees/25
+                    Employee[] result = session
+                        .Advanced
+                        .LoadStartingWith<Employee>("employees/", "1*|2*", 0, 128);
+                    #endregion
+                }
+
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_4_2_async
+                    // return up to 128 entities with Id that starts with 'employees/' 
+                    // and rest of the key begins with "1" or "2" e.g. employees/10, employees/25
+                    Employee[] result = (await asyncSession
+                        .Advanced
+                        .LoadStartingWithAsync<Employee>("employees/", "1*|2*", 0, 128))
+                        .ToArray();
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_5_1
+
+                    IEnumerator<StreamResult<Employee>> enumerator = session
+                        .Advanced
+                        .Stream<Employee>("employees/");
+
+                    while (enumerator.MoveNext())
+                    {
+                        StreamResult<Employee> employee = enumerator.Current;
+                    }
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_5_1_async
+
+                    IAsyncEnumerator<StreamResult<Employee>> enumerator = await asyncSession
+                        .Advanced
+                        .StreamAsync<Employee>("employees/");
+
+                    while (await enumerator.MoveNextAsync())
+                    {
+                        StreamResult<Employee> employee = enumerator.Current;
+                    }
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_5_2
+
+                    using (var outputStream = new MemoryStream())
+                    {
+                        session
+                            .Advanced
+                            .LoadStartingWithIntoStream("employees/", outputStream);
+                    }
+
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_5_2_async
+
+                    using (var outputStream = new MemoryStream())
+                    {
+                        await asyncSession
+                            .Advanced
+                            .LoadStartingWithIntoStreamAsync("employees/", outputStream);
+                    }
+
+                    #endregion
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    #region loading_entities_6_1
+                    bool isLoaded = session.Advanced.IsLoaded("employees/1"); // false
+                    Employee employee = session.Load<Employee>("employees/1");
+                    isLoaded = session.Advanced.IsLoaded("employees/1"); // true
+                    #endregion
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    #region loading_entities_6_1_async
+                    bool isLoaded = asyncSession.Advanced.IsLoaded("employees/1"); // false
+                    Employee employee = await asyncSession.LoadAsync<Employee>("employees/1");
+                    isLoaded = asyncSession.Advanced.IsLoaded("employees/1"); // true
+                    #endregion
+                }
+
+                #region loading_entities_7_1
+                string changeVector;
+                User user = new User { Name = "Bob" };
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(user, "users/1");
+                    session.SaveChanges();
+
+                    changeVector = session.Advanced.GetChangeVectorFor(user);
+                }
+
+                // New session which does not track our User entity
+                using (var session = store.OpenSession())
+                {
+                    // The given change vector matches 
+                    // the server-side change vector
+                    // Does not load the document
+                    var result1 = session.Advanced
+                                   .ConditionalLoad<User>("users/1", changeVector);
+
+                    // Modify the document
+                    user.Name = "Bob Smith";
+                    session.Store(user);
+                    session.SaveChanges();
+
+                    // Change vectors do not match
+                    // Loads the document
+                    var result2 = session.Advanced
+                                   .ConditionalLoad<User>("users/1", changeVector);
+                }
+                #endregion
+            }
+
+            using (var store = new DocumentStore())
+            {
+                #region loading_entities_7_1_async
+                string changeVector;
+                User user = new User { Name = "Bob" };
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(user, "users/1");
+                    await session.SaveChangesAsync();
+
+                    changeVector = session.Advanced.GetChangeVectorFor(user);
+                }
+
+                // New session which does not track our User entity
+                using (var session = store.OpenAsyncSession())
+                {
+                    // The given change vector matches 
+                    // the server-side change vector
+                    // Does not load the document
+                    var result1 = await session.Advanced
+                                   .ConditionalLoadAsync<User>("users/1", changeVector);
+
+                    // Modify the document
+                    user.Name = "Bob Smith";
+                    await session.StoreAsync(user);
+                    await session.SaveChangesAsync();
+
+                    // Change vectors do not match
+                    // Loads the document
+                    var result2 = await session.Advanced
+                                   .ConditionalLoadAsync<User>("users/1", changeVector);
+                }
+                #endregion
+            }
+        }
+    }
+
+    internal class User
+    {
+        public string Name { get; internal set; }
+    }
+}
