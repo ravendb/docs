f87cc0c85d4fc9d106026711e6d059f0804f1d02
danielle9897 <danielle.9897@gmail.com>
2024-02-29 16:52:25 +0200

    RDoc-2634 [Node.js] Client API > Commands > Documents > Put [Replace C# samples]

---
+++ Documentation/5.4/Samples/csharp/Raven.Documentation.Samples/ClientApi/Commands/Documents/Put.cs
@@ -0,0 +1,52 @@
+﻿
+using Raven.Client.Documents;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Session;
+using Raven.Documentation.Samples.Orders;
+using Sparrow.Json;
+
+namespace Raven.Documentation.Samples.ClientApi.Commands.Documents
+{
+    public class PutInterfaces
+    {
+        private class PutDocumentCommand
+        {
+            #region put_interface
+            public PutDocumentCommand(string id, string changeVector, BlittableJsonReaderObject document)
+            #endregion
+            {
+            }
+        }
+    }
+
+    public class PutSamples
+    {
+        public void Foo()
+        {
+            using (var store = new DocumentStore())
+            using (var session = store.OpenSession())
+            {
+                #region put_sample
+                // Create a new document
+                var doc = new Category
+                {
+                    Name = "My category",
+                    Description = "My category description"
+                };
+                // Create metadata on the document
+                var docInfo = new DocumentInfo
+                {
+                    Collection = "Categories"
+                };
+                // Convert your entity to a BlittableJsonReaderObject
+                var blittableDoc = session.Advanced.JsonConverter.ToBlittable(doc, docInfo);
+
+                // The Put command (parameters are document ID, changeVector check is null, the document to store)
+                var command = new PutDocumentCommand("categories/999", null, blittableDoc);
+                // RequestExecutor sends the command to the server
+                session.Advanced.RequestExecutor.Execute(command, session.Advanced.Context);
+                #endregion
+            }
+        }
+    }
+}
