ff68b7076f583b055a8139e8dc70184781cda73a
reebhub <reebreeb@gmail.com>
2024-03-06 18:49:00 +0200

    RDoc-2641 - copied markdown and sample files to version 5.4

---
+++ Documentation/5.4/Samples/csharp/Raven.Documentation.Samples/ClientApi/Session/ClusterTransaction/AtomicGuards.cs
@@ -0,0 +1,97 @@
+﻿using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations.CompareExchange;
+using Raven.Client.Documents.Session;
+
+namespace Raven.Documentation.Samples.ClientApi.Session.ClusterTransactions
+{
+    public class AtomicGuards
+    {
+        public async Task CreateAtomicGuard()
+        {
+            //var (nodes, leader) = await CreateRaftCluster(3);
+            using var store = new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "test"
+            }.Initialize();
+
+            #region atomic-guards-enabled
+
+            using (var session = store.OpenAsyncSession(new SessionOptions
+                   {
+                       // Open a cluster-wide session
+                       TransactionMode = TransactionMode.ClusterWide
+                   }))
+            {
+                await session.StoreAsync(new User(), "users/johndoe");
+                await session.SaveChangesAsync();
+                // An atomic-guard is now automatically created for the new document "users/johndoe".
+            }
+
+            // Open two more cluster-wide sessions
+            using (var session1 = store.OpenAsyncSession(
+                   new SessionOptions 
+                   {TransactionMode = TransactionMode.ClusterWide}))
+            using (var session2 = store.OpenAsyncSession(
+                   new SessionOptions 
+                   {TransactionMode = TransactionMode.ClusterWide}))
+            {
+                // The two sessions will load the same document at the same time
+                var loadedUser1 = await session1.LoadAsync<User>("users/johndoe");
+                loadedUser1.Name = "jindoe";
+
+                var loadedUser2 = await session2.LoadAsync<User>("users/johndoe");
+                loadedUser2.Name = "jandoe";
+
+                // Session1 will save changes first, which triggers a change in the 
+                // version number of the associated atomic-guard.
+                await session1.SaveChangesAsync();
+
+                // session2.saveChanges() will be rejected with ConcurrencyException
+                // since session1 already changed the atomic-guard version,
+                // and session2 saveChanges uses the document version that it had when it loaded the document.
+                await session2.SaveChangesAsync();
+            }
+            #endregion
+
+            var result = await store.Operations.SendAsync(new GetCompareExchangeValuesOperation<User>(""));
+        }
+
+        public async Task DoNotCreateAtomicGuard()
+        {
+            //var (nodes, leader) = await CreateRaftCluster(3);
+            using var store = new DocumentStore
+            {
+                Urls = new[] { "http://127.0.0.1:8080" },
+                Database = "test"
+            }.Initialize();
+
+
+            #region atomic-guards-disabled
+            using (var session = store.OpenAsyncSession(new SessionOptions
+                   {
+                       TransactionMode = TransactionMode.ClusterWide,
+                       // Disable atomic-guards
+                       DisableAtomicDocumentWritesInClusterWideTransaction = true
+                   }))
+            {
+                await session.StoreAsync(new User(), "users/johndoe");
+
+                // No atomic-guard will be created upon saveChanges
+                await session.SaveChangesAsync();
+            }
+            #endregion
+
+            // WaitForUserToContinueTheTest(store);
+
+            var result = await store.Operations.SendAsync(new GetCompareExchangeValuesOperation<User>(""));
+        }
+
+        private class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ Documentation/5.4/Samples/nodejs/client-api/session/ClusterTransaction/atomicGuards.js
@@ -0,0 +1,62 @@
+import { DocumentStore } from "ravendb";
+
+const documentStore = new DocumentStore();
+
+async function examples() {
+    {
+        //region atomic-guards-enabled
+        const user = {
+            firstName: "John",
+            lastName: "Doe"
+        };
+        
+        // Open a cluster-wide session
+        const session = documentStore.openSession({
+            transactionMode: "ClusterWide"
+        });
+
+        await session.store(user, "users/johndoe");
+        await session.saveChanges();
+        // An atomic-guard is now automatically created for the new document "users/johndoe".
+        
+        // Open two more cluster-wide sessions
+        const session1 = documentStore.openSession({
+            transactionMode: "ClusterWide"
+        });        
+        const session2 = documentStore.openSession({
+            transactionMode: "ClusterWide"
+        });
+
+        // The two sessions will load the same document at the same time
+        const loadedUser1 = await session1.load("users/johndoe");
+        loadedUser1.name = "jindoe";
+
+        const loadedUser2 = await session2.load("users/johndoe");
+        loadedUser2.name = "jandoe";
+        
+        // session1 will save changes first, which triggers a change in the 
+        // version number of the associated atomic-guard.
+        await session1.saveChanges();
+        
+        // session2.saveChanges() will be rejected with a ClusterTransactionConcurrencyException
+        // since session1 already changed the atomic-guard version,
+        // and session2 saveChanges uses the document version that it had when it loaded the document.
+        await session2.saveChanges();        
+        //endregion
+
+        //region atomic-guards-disabled
+
+        // Open a cluster-wide session
+        const session = documentStore.openSession({
+            transactionMode: "ClusterWide",
+            // Disable atomic-guards
+            disableAtomicDocumentWritesInClusterWideTransaction: true
+        });
+
+        await session.store(user, "users/johndoe");
+        
+        // No atomic-guard will be created upon saveChanges
+        await session.saveChanges();
+        //endregion
+    }
+}
