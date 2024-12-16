f88151acf8d5eeb64049b427e2551a4f9ba0b051
danielle9897 <danielle.9897@gmail.com>
2024-12-16 20:33:51 +0200

    RDoc-3148 Indexes > What are Indexes [python]

--- Documentation/6.0/Samples/python/Indexes/WhatAreIndexes.py
+++ Documentation/6.0/Samples/python/Indexes/WhatAreIndexes.py
@@ -4,12 +4,21 @@ from examples_base import ExampleBase, Employee
 
 
 # region indexes_1
-class Employees_ByFirstAndLastName(AbstractIndexCreationTask):
+# Define the index:
+# =================
+
+class Employees_ByNameAndCountry(AbstractIndexCreationTask):
     def __init__(self):
         super().__init__()
-        self.map = "from employee in docs.Employees select new {first_name = employee.FirstName, last_name = employee.LastName}"
-
-
+        
+        self.map = """
+        from employee in docs.Employees 
+        select new {
+            LastName = employee.LastName, 
+            FullName = employee.FirstName + " " + employee.LastName,
+            Country = employee.Address.country
+        }
+        """
 # endregion
 
 
@@ -21,15 +30,22 @@ class WhatAreIndexes(ExampleBase):
         with self.embedded_server.get_document_store("WhatAreIndexes") as store:
             with store.open_session() as session:
                 # region indexes_2
-                # save index on server
-                Employees_ByFirstAndLastName().execute(store)
+                # Deploy the index to the server:
+                # ===============================
+                
+                Employees_ByNameAndCountry().execute(store)
                 # endregion
 
                 with store.open_session() as session:
                     # region indexes_3
-                    results = list(
-                        session.query_index_type(Employees_ByFirstAndLastName, Employee).where_equals(
-                            "first_name", "Robert"
-                        )
+                    # Query the database using the index:
+                    # ===================================
+                    
+                    employeesFromUK = list(
+                        session.query_index_type(Employees_ByNameAndCountry, Employee)
+                         # Here we query for all Employee documents that are from the UK
+                         # and have 'King' in their LastName field:
+                        .where_equals("Country", "UK")
+                        .where_equals("LastName", "King")
                     )
                     # endregion
