c21f0c529aa436812e63a30a9bbfbc03ff1262ea
Michał Leśniak <michal.lesniak@ravendb.net>
2026-03-05 11:35:02 +0100

    RDoc-3661 Added Cloudfront redirects

--- scripts/redirect_root_to_version.js
+++ scripts/redirect_root_to_version.js
@@ -1,33 +1,56 @@
-function handler(event) {
-    var request = event.request;
-    var uri = request.uri;
+import cf from 'cloudfront';
+
+const kvsHandle = cf.kvs();
+
+const defaultVersion = "7.2";
+
+const staticAssetRegex =
+    /\.(html|css|js|jpg|jpeg|png|gif|webp|svg|ico|ttf|otf|woff|woff2|eot|mp4|mp3|webm|avi|mov|pdf|txt|xml)$/i;
+
+const versionRegex = /^\/(\d+\.\d+)(\/.*)?/;
+
+async function handler(event) {
+    const request = event.request;
+    const uri = request.uri;
 
     if (uri.startsWith("/cloud") || uri.startsWith("/guides")) {
         return request;
     }
 
-    var defaultVersion = "7.2";
-
-    var staticAssetRegex =
-        /\.(html|css|js|jpg|jpeg|png|gif|webp|svg|ico|ttf|otf|woff|woff2|eot|mp4|mp3|webm|avi|mov|pdf|txt|xml)$/i;
-
     if (staticAssetRegex.test(uri)) {
         return request;
     }
 
-    var versionRegex = /^\/\d+\.\d+/;
+    const versionMatch = uri.match(versionRegex);
 
-    if (!versionRegex.test(uri)) {
-        var newUri = `/${defaultVersion}${uri}`;
+    let version, versionlessUri;
+    if (versionMatch) {
+        version = versionMatch[1];
+        versionlessUri = versionMatch[2];
+    }
+    else {
+        version = defaultVersion;
+        versionlessUri = uri;
+    }
+
+    try {
+        const redirectPath = await kvsHandle.get(versionlessUri);
+
+        if (redirectPath === null) {
+            return request;
+        }
+
+        const newUri = `/${version}${redirectPath}`;
 
         return {
             statusCode: 301,
             statusDescription: "Moved Permanently",
             headers: {
-                location: { value: newUri },
-            },
+                location: { value: newUri }
+            }
         };
     }
-
-    return request;
+    catch (_) {
+        return request;
+    }
 }
