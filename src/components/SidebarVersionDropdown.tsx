53e5519e91d702f56a4f3bb5eceb2487b2173dff
Gracjan Sadowicz <gracjan@ravendb.net>
2026-08-04 14:12:41 +0200

    RDoc-3573 Preserve query and anchor on version switch, save preferred version

--- src/components/SidebarVersionDropdown.tsx
+++ src/components/SidebarVersionDropdown.tsx
@@ -1,15 +1,27 @@
 import React, { useState, useRef, useEffect } from "react";
 import Link from "@docusaurus/Link";
-import { useVersions, useLatestVersion, useActiveDocContext } from "@docusaurus/plugin-content-docs/client";
+import {
+    useVersions,
+    useLatestVersion,
+    useActiveDocContext,
+    useDocsPreferredVersion,
+    type GlobalVersion,
+} from "@docusaurus/plugin-content-docs/client";
+import { useHistorySelector } from "@docusaurus/theme-common";
 
 export default function CustomVersionDropdown() {
     const pluginId = "default";
     const versions = useVersions(pluginId);
     const latestVersion = useLatestVersion(pluginId);
     const { activeVersion, alternateDocVersions } = useActiveDocContext(pluginId);
+    const { savePreferredVersionName } = useDocsPreferredVersion(pluginId);
+    const queryString = useHistorySelector((history) => history.location.search);
+    const anchor = useHistorySelector((history) => history.location.hash);
 
-    const getVersionTargetPath = (version: (typeof versions)[number]) =>
-        alternateDocVersions[version.name]?.path ?? version.path;
+    const getVersionMainDoc = (version: GlobalVersion) => version.docs.find((doc) => doc.id === version.mainDocId)!;
+
+    const getVersionTargetPath = (version: GlobalVersion) =>
+        `${(alternateDocVersions[version.name] ?? getVersionMainDoc(version)).path}${queryString}${anchor}`;
 
     const [open, setOpen] = useState(false);
     const wrapperRef = useRef<HTMLDivElement>(null);
@@ -75,7 +87,10 @@ export default function CustomVersionDropdown() {
                             <Link
                                 to={getVersionTargetPath(version)}
                                 className="menu__link"
-                                onClick={() => setOpen(false)}
+                                onClick={() => {
+                                    savePreferredVersionName(version.name);
+                                    setOpen(false);
+                                }}
                             >
                                 {version.label}.x
                             </Link>
